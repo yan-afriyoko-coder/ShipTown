7ed89a6bdede89f4161d9d36c08f85e6d271ba1c
CMHayden <haydencallum4@gmail.com>
2019-06-20 11:59:04 +0200

    modify the sns controller to work as a parent controller for other SNS related controllers

--- app/Http/Controllers/snsController.php
+++ app/Http/Controllers/snsController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use AWS;
 
 class snsController extends Controller
 {
-    //
+    protected $topicName = "";
+
+    function store(Request $request){
+        $message = $request->getContent();
+        $this->sendTo($this->getTopicName(), $message);
+    }
+
+    function sendTo($topicName, $message){
+
+        $aws = AWS::createClient('sns');
+
+        $aws->publish([
+            'Message'   => $message,
+            'TargetArn' => $topicName
+        ]);
+
+
+    }
+
+    function getTopicName(){
+        return "arn:aws:sns:eu-west-1:310005059065:".auth('api')->user()->id."_".$this->topicName;
+
+    }
 }
