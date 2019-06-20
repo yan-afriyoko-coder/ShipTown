1efa195e845006d5c5daafbc4d5717448fc7f089
CMHayden <haydencallum4@gmail.com>
2019-06-20 11:58:32 +0200

    modify the product controller to set the topic name and to validate

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class productController extends Controller
+class productController extends snsController
 {
-    //
+    //protected $topicName='product';
+    protected $topicName = "snsProductsPostTopic";
+
+    function validation() {
+        return true;
+    }
 }
