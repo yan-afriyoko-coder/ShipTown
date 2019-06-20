779e62f903cc549f5fd0ea1416064a3b28ed8a05
CMHayden <haydencallum4@gmail.com>
2019-06-20 11:57:06 +0200

    modify routes for posting to product

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user()->id;
 });
 
-Route::post('/product', 'productController@handleRequest');
+Route::post('/products', 'productController@store');
