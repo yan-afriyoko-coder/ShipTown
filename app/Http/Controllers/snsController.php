4f382c54438b8b292dcbe0dc099a427b7b8eaced
CMHayden <haydencallum4@gmail.com>
2019-06-21 14:05:14 +0200

    slightly modify logic for response

--- app/Http/Controllers/snsController.php
+++ app/Http/Controllers/snsController.php
@@ -11,38 +11,40 @@ class snsController extends Controller
     protected $topicName = "";
 
     function store(Request $request){
+
         $message = $request->getContent();
 
         if(!$this->validation($message)){
             return response()->json("Error 422: Invalid data", 422);
         }
 
-        $this->sendTo($this->getTopicName(), $message);
+        if ($this->sendTo($this->getTopicName(), $message)) {
+            return response()->json("ok", 200);
+        };
+
+
     }
 
     function sendTo($topicName, $message){
 
         $aws = AWS::createClient('sns');
 
-        $aws->publish([
+        $result = $aws->publish([
             'Message'   => $message,
             'TargetArn' => $topicName
         ]);
+
+        return $result['MessageId'];
     }
 
     function getTopicName(){
 
         $userID = auth('api')->user()->id;
 
-        if(!isset($userID)){
-            return response()->json("Error 403: Forbidden. Please log in", 403);
-
-        }
-
         return "arn:aws:sns:".env('AWS_REGION').":".env('AWS_USER_CODE').":snsTopic_".$this->topicName."_User".$userID;
     }
 
     function validation($message){
-        //
+        return true;
     }
 }
