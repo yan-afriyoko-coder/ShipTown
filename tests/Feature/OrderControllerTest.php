197e8cbdf37933e9563d17c5d05966d6b8ce9573
CMHayden <haydencallum4@gmail.com>
2019-06-26 12:58:58 +0200

    create test for valid orders route

--- tests/Feature/OrderControllerTest.php
+++ tests/Feature/OrderControllerTest.php
@@ -8,15 +8,25 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class OrderControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    public function test_orders_route () {
+
+        $data =
+            '{
+                "orderID": "001241",
+                "sku": "123456",
+                "quantity": 9,
+                "pricePerUnit": 78,
+                "priceTotal": 702
+            }';
+
+
+        $response = $this->withHeaders([
+            'Authorization'=>env('TEST_AUTH'),
+            ])->json('POST', 'api/orders',[
+                $data
+            ]);
 
         $response->assertStatus(200);
+
     }
 }
