3515e7b853223afceb1989d82b1760bfdccd67e1
CMHayden <haydencallum4@gmail.com>
2019-06-26 12:59:34 +0200

    create test for not allowing unauthenticated user

--- tests/Feature/OrderControllerTest.php
+++ tests/Feature/OrderControllerTest.php
@@ -29,4 +29,22 @@ class OrderControllerTest extends TestCase
         $response->assertStatus(200);
 
     }
+
+    public function test_if_unauthenticated_user_is_not_allowed () {
+
+        $data =
+            '{
+                "orderID": "001241",
+                "sku": "123456",
+                "quantity": 9,
+                "pricePerUnit": 78,
+                "priceTotal": 702
+            }';
+
+        $response = $this->json('POST', 'api/orders',[
+                $data
+            ]);
+
+        $response->assertStatus(401);
+    }
 }
