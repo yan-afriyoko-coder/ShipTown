529fd46be2133ac856e5732f3a8872fa8ff4aefc
CMHayden <haydencallum4@gmail.com>
2019-07-19 16:48:20 +0200

    modify controllers which extend from sns base controller

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class OrdersController extends snsController
+class OrdersController extends SnsBaseController
 {
     protected $topicName = "Orders";
 
--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ProductsController extends snsController
+class ProductsController extends SnsBaseController
 {
     protected $topicName = "Products";
 
