2b855e51419c9ee7b8a750094389711f818f3c7b
CMHayden <haydencallum4@gmail.com>
2019-07-19 16:46:07 +0200

    rename products controller

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class productController extends snsController
+class ProductsController extends snsController
 {
     protected $topicName = "Products";
 
