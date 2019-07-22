3d23c6646a335d25e3cbe9480d84561fea769bcb
CMHayden <haydencallum4@gmail.com>
2019-07-22 16:27:22 +0200

    update routes

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user()->id;
 });
 
-Route::middleware('auth:api')->post('/products', 'productController@store');
+Route::middleware('auth:api')->post('/products', 'ProductsController@store');
 
 Route::middleware('auth:api')->post('/orders', 'OrdersController@store');
