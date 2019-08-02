c1f9ed1966b4a48d856de669bc8adf78c5470e19
CMHayden <haydencallum4@gmail.com>
2019-08-02 14:33:28 +0200

    create frontend of component

---
+++ resources/js/components/SubscribeTopic.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div>
+            <div class="card card-default">
+                <div class="card-header">
+                    <div style="display: flex; justify-content: space-between; align-items: center;">
+                        <span>
+                            Subscribe To Topic
+                        </span>
+                    </div>
+                </div>
+
+                <div class="card-body">
+
+                    <table class="table table-borderless mb-0">
+                        <thead>
+                            <tr>
+                                <th>
+                                    <div class="form-group">
+                                        <label for="URL">URL</label>
+                                        <input type="text" class="form-control" id="URL" placeholder="Enter the URL">
+                                    </div>
+                                </th>
+                            </tr>
+                            <tr>
+                                <th><div class="text-center"><button id="product-btn" class="btn-primary centre" v-on:click="todo()">Subscribe</button></div></th>
+
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
