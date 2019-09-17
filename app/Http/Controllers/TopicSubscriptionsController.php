878d5fab3567e513d225454ebe47c391411a3ee4
Artur Hanusek <arthur@youritsolutions.ie>
2019-09-17 18:52:38 +0200

    pass topic subscriptions test

--- app/Http/Controllers/TopicSubscriptionsController.php
+++ app/Http/Controllers/TopicSubscriptionsController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class TopicSubscriptionsController extends Controller
 {
-    //
+    public function store(Request $request){
+        $this->respond_OK_200();
+    }
 }
