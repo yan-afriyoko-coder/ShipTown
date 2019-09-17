6d9612dd77be3f65cb55f485ea416fe548e488f1
Artur Hanusek <arthur@youritsolutions.ie>
2019-09-17 15:56:09 +0200

    move store method to controller

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,14 +8,12 @@ class ProductsController extends SnsBaseController
 {
     protected $topicNamePrefix = "Products";
 
-    function store(Request $request){
+    public function store(Request $request){
 
         $message = $request->getContent();
 
         if ($this->publishMessage($message)) {
-
             return response()->json("ok", 200);
-
         };
 
     }
