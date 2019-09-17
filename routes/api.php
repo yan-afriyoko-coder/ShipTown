a16ec6e70f8a8b2696867dd5107777691a0c7a1f
Artur Hanusek <arthur@youritsolutions.ie>
2019-09-17 13:31:20 +0200

    moved routes to group@

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user()->id;
 });
 
-Route::middleware('auth:api')->post('/products', 'ProductsController@store');
+Route::middleware('auth:api')->group(function() {
 
-Route::middleware('auth:api')->post('/orders', 'OrdersController@store');
+    Route::post('products', 'ProductsController@store');
+    Route::post('orders', 'OrdersController@store');
+    Route::post('createtopic', 'SnsBaseController@create');
+    Route::post('subscribetopic', 'SnsBaseController@subscribe');
 
-Route::post('/createtopic', 'SnsBaseController@create');
+});
 
-Route::post('/subscribetopic', 'SnsBaseController@subscribe');
+
+
+
