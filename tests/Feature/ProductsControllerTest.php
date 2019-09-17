534c13dcc66f14f84b784f83a137ccba5d80cb8d
Artur Hanusek <arthur@youritsolutions.ie>
2019-09-17 18:24:49 +0200

    update subscription url

--- tests/Feature/ProductsControllerTest.php
+++ tests/Feature/ProductsControllerTest.php
@@ -14,11 +14,8 @@ class ProductsControllerTest extends TestCase
     public function test_products_route_authenticated_user () {
 
         $data = [
-            'ProductName'   => '001241',
-            'ProductModel'  => '123456',
-            'Description'   => 9,
-            'SKU'           => 78,
-            'Price'         => 702,
+            "sku"   => "001241",
+            "price"  => 25,
         ];
 
         Passport::actingAs(
@@ -33,11 +30,8 @@ class ProductsControllerTest extends TestCase
     public function test_products_route_unauthenticated_user () {
 
         $data = [
-            'ProductName'   => '001241',
-            'ProductModel'  => '123456',
-            'Description'   => 9,
-            'SKU'           => 78,
-            'Price'         => 702,
+            "sku"   => "001241",
+            "price"  => 25,
         ];
 
         $this->json('POST', 'api/products', [$data])
