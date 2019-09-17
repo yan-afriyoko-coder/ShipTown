3c16a79d2807c5e9c2b949be51820824580c42f6
Artur Hanusek <arthur@youritsolutions.ie>
2019-09-17 13:08:12 +0200

    create AWS Access test

--- tests/Feature/AwsAccessTest.php
+++ tests/Feature/AwsAccessTest.php
@@ -19,4 +19,13 @@ class AwsAccessTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_if_aws_credentials_are_set()
+    {
+        $this->assertNotEmpty(env('AWS_ACCESS_KEY_ID'), 'AWS_ACCESS_KEY_ID is not set');
+        $this->assertNotEmpty(env('AWS_SECRET_ACCESS_KEY'), 'AWS_SECRET_ACCESS_KEY is not set');
+        $this->assertNotEmpty(env('AWS_REGION'), 'AWS_REGION is not set');
+        $this->assertNotEmpty(env('AWS_USER_CODE'), 'AWS_USER_CODE is not set');
+        $this->assertNotEmpty(env('AWS_BUCKET'), 'AWS_BUCKET is not set');
+    }
 }
