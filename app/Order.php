8d42e006af1b5bd886ced9edd35c0922394ee917
Artur Hanusek <arthur@youritsolutions.ie>
2019-09-30 10:57:06 +0200

    add order_number & order_json to mass assignment

--- app/Order.php
+++ app/Order.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'order_number', 'order_json'
+    ];
+
     protected $casts = [
         'order_json' => 'array'
     ];
