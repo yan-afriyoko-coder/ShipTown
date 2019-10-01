e52ad3145db09af9751f5a5da42518906f961034
Artur Hanusek <arthur@youritsolutions.ie>
2019-10-01 12:02:35 +0200

    create test for missing order_number

--- tests/Feature/OrdersControllerTest.php
+++ tests/Feature/OrdersControllerTest.php
@@ -66,4 +66,33 @@ class OrdersControllerTest extends TestCase
             ->assertStatus(401);
 
     }
+
+    public function test_if_missing_order_number_not_allowed() {
+
+        $data = [
+            //'order_number'      => '001241',
+            "products" => [
+                [
+                    'sku' => '123',
+                    'quantity'     => 2,
+                    'price'        => 4,
+                ],
+
+                [
+                    'sku' => '456',
+                    'quantity'     => 5,
+                    'price'        => 10,
+                ],
+            ]
+        ];
+
+
+        Passport::actingAs(
+            factory(User::class)->create()
+        );
+
+        $this->json('POST', 'api/orders', $data)
+            ->assertStatus(200);
+
+    }
 }
