52a9413079925fe072593b89213d40ec73e266c3
Artur Hanusek <arthur@youritsolutions.ie>
2019-10-02 11:26:49 +0200

    add casts & fillable fields to Product model

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+
+    ];
+
+    protected $casts = [
+        'quantity_reserved_details' => 'array'
+    ];
 }
