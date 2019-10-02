4fb3b66658922815b360314d78307889c21bfcd3
Artur Hanusek <arthur@youritsolutions.ie>
2019-10-02 12:46:06 +0200

    add reserve method

--- app/Product.php
+++ app/Product.php
@@ -13,4 +13,30 @@ class Product extends Model
     protected $casts = [
         'quantity_reserved_details' => 'array'
     ];
+
+    public function reserve($quantity, $comment) {
+
+        $this->increment('quantity_reserved', $quantity);
+
+        $this->addReservedComment($quantity, $comment);
+
+        $this->save();
+
+    }
+
+    /**
+     * @param $quantity
+     * @param $comment
+     */
+    public function addReservedComment($quantity, $comment): void
+    {
+        $quantity_reserved_details = $this->quantity_reserved_details;
+
+        $quantity_reserved_details[] = [
+            "quantity" => $quantity,
+            "comment" => $comment
+        ];
+
+        $this->quantity_reserved_details = $quantity_reserved_details;
+    }
 }
