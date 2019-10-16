6f3ecab127027fcbe231dd21adf68608f7a2b00b
Artur Hanusek <arthur@youritsolutions.ie>
2019-10-16 15:26:53 +0200

    update table name

--- database/migrations/2019_10_16_123958_create_new_orders_table.php
+++ database/migrations/2019_10_16_123958_create_new_orders_table.php
@@ -35,6 +35,6 @@ class CreateNewOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('new_orders');
+        Schema::dropIfExists('orders');
     }
 }
