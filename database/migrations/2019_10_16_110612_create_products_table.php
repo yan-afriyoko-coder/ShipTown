b30d2b84d2aec72d8c4c64aac60c45043982806e
Artur Hanusek <arthur@youritsolutions.ie>
2019-10-16 14:31:15 +0200

    create migration for products table

---
+++ database/migrations/2019_10_16_110612_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTableNew extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::enableForeignKeyConstraints();
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('sku', 20);
+            $table->string('name', 100);
+            $table->decimal('quantity')->default(0);
+            $table->decimal('quantity_reserved')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
