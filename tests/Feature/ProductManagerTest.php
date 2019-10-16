b2933f9808de2fd383c810ff60088ebd946c8024
Artur Hanusek <arthur@youritsolutions.ie>
2019-10-16 18:35:57 +0200

    add test to check release quantity method

--- tests/Feature/ProductManagerTest.php
+++ tests/Feature/ProductManagerTest.php
@@ -50,4 +50,34 @@ class ProductManagerTest extends TestCase
 
         $this->assertEquals($quantity_to_reserve, $quantity_reserved_diff);
     }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_if_releases_quantity_correctly()
+    {
+        $quantity_to_release = 10;
+
+        $product_before = $this->json('POST', 'api/products', ModelSample::PRODUCT)
+            ->assertStatus(200)
+            ->getContent();
+
+        $product_before = json_decode($product_before, true);
+
+
+        ProductManager::release(ModelSample::PRODUCT['sku'], $quantity_to_release, 'Reservation test');
+
+
+        $product_after = $this->json('POST', 'api/products', ModelSample::PRODUCT)
+            ->assertStatus(200)
+            ->getContent();
+
+        $product_after = json_decode($product_after, true);
+
+        $quantity_reserved_diff = $product_after['quantity_reserved'] - $product_before['quantity_reserved'];
+
+        $this->assertEquals($quantity_to_release, $quantity_reserved_diff);
+    }
 }
