cfa0dd6a4c2249aa55d38b151fac53e7603e5e4e
Artur Hanusek <arthur@youritsolutions.ie>
2019-10-16 14:46:12 +0200

    create migration for orders table

---
+++ database/migrations/2019_10_16_123958_create_new_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('order_number');
+            $table->json('order_as_json');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('new_orders');
+    }
+}
