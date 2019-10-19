919e27e80618f6458a813c5d14d8cf39613e1d32
vagrant <vagrant@homestead>
2019-10-19 20:54:10 +0000

    create test for orders import from api2cart

---
+++ tests/Feature/imports/orders/Api2CartTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\imports\orders;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class api2CartTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_get_route()
+    {
+        $response = $this->get('api/import/orders/api2cart');
+
+        $response->assertStatus(200);
+    }
+}
