e8f8e632d70fe16d751ca485e23842fa2eb6b362
Artur Hanusek <artur@coding.cab>
2019-10-20 13:09:04 +0000

    use AuthorizedUserTestCase in Api2CartTest

--- tests/Feature/import/orders/Api2CartTest.php
+++ tests/Feature/import/orders/Api2CartTest.php
@@ -2,12 +2,19 @@
 
 namespace Tests\Feature\import\orders;
 
+use App\User;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Passport\Passport;
+use League\OAuth2\Server\Entities\Traits\AuthCodeTrait;
+use Tests\Feature\AuthorizedUserTestCase;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class api2CartTest extends TestCase
 {
+    use AuthorizedUserTestCase;
+
     /**
      * A basic feature test example.
      *
