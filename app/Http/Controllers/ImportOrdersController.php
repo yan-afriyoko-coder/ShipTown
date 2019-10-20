a6c44f2c75c48c0f099bc6548a3ea3f3a204bc77
Artur Hanusek <artur@coding.cab>
2019-10-20 14:08:11 +0000

    dispatch job when url invoked

--- app/Http/Controllers/ImportOrdersController.php
+++ app/Http/Controllers/ImportOrdersController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\JobImportOrderApi2Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ImportOrdersController extends Controller
 {
     public function fromApi2Cart()
     {
+        JobImportOrderApi2Cart::dispatch();
+
+        Log::info('Import Order from api2cart dispatched');
+
         $responseText = [
             "message" => "",
             "error_id" => 0,
