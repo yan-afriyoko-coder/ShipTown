cf4490ba4beb8023c20dbf1a38483e8d942ffc68
Artur Hanusek <artur@coding.cab>
2019-10-24 11:43:48 +0000

    add structure test

--- tests/Feature/api/ProductGetRouteTest.php
+++ tests/Feature/api/ProductGetRouteTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\api;
 
 use Tests\Feature\AuthorizedUserTestCase;
+use Tests\ModelSample;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,6 +11,35 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ProductGetRouteTest extends TestCase
 {
     use AuthorizedUserTestCase;
+
+    public function test_products_response_structure()
+    {
+        // We will send item to make sure
+        // at lease one item exists
+        $this->json("POST", '/api/products', ModelSample::PRODUCT);
+
+        $response = $this->get('/api/products');
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            "current_page",
+            "data" => [
+                "*" => [
+                    "sku",
+                    "name",
+                    "price",
+                    "sale_price",
+                    "sale_price_start_date",
+                    "sale_price_end_date",
+                    "quantity",
+                    "quantity_reserved",
+                    "quantity_available"
+                ]
+            ],
+            "total",
+        ]);
+    }
 
     /**
      * @return void
