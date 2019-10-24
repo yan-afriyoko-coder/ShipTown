00d89408cb64e1fe7936244cebe70a9e4ef874b0
Artur Hanusek <artur@coding.cab>
2019-10-24 10:41:31 +0000

    add products api route

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::middleware('auth:api')->group(function() {
     Route::post('products', 'ProductsController@store');
+    Route::get('products', 'ProductsController@index');
 
     Route::get('orders', 'OrdersController@index');
     Route::post('orders', 'OrdersController@store');
