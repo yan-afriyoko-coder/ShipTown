a3cb5b265c2f9db3aa40fbef1d27312e1bf295ea
Artur Hanusek <artur@coding.cab>
2019-12-19 14:18:52 +0000

    index all products on api/inventory route

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Scopes\AuthenticatedUserScope;
 use Illuminate\Http\Request;
 
 class InventoryController extends Controller
 {
     public function index() {
-
+        return Product::all();
     }
 }
