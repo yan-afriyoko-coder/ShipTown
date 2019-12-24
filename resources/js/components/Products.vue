cf248298c42e18cd672f8bf1e49844419fe59576
Artur Hanusek <artur@coding.cab>
2019-12-24 11:14:53 +0000

    extract data from response and assign to handsontable data@

--- resources/js/components/Products.vue
+++ resources/js/components/Products.vue
@@ -11,40 +11,33 @@
         },
 
         methods: {
-            feedData: function() {
-                axios({
-                    method:'get',
-                    url: '/api/inventory',
-                    mode: 'no-cors',
-                })
-                .then(function(response) {
-                    return response['data'];
-                })
-            },
             loadProductList: function() {
-                this.data = this.feedData();
+                axios.get('/api/inventory')
+                    .then(response => {
+
+                        const resultArray = [];
+
+                        response.data.forEach(function(record) {
+                            const result = [
+                                record["sku"], // sku
+                                record["name"], // product name
+                                record["category"], // category
+                                0, // TODO: quantity available web
+                                record["quantity_reserved"], // quantity reserved (on web orders)
+                                0, // TODO: quantity required from warehouse
+                            ];
+
+                            resultArray.push(result);
+                        });
+
+                        this.data = resultArray;
+                    });
             },
         },
 
         data: function() {
             return {
-                api_url: "https://live.bellababy.hq.rmsapi.products.management/api/",
-                api_limit: 20,
-                data: [
-                    ["164965","15cm stainless Steel milk pan","HARDWARE",1,7,6],
-                    ["133423","Doorstopper white 5pcs","HARDWARE",2,3,1],
-                    ["120850","Teapot 100oz","HARDWARE",3,5,2],
-                    ["146502","18cm Non Stick Sauce Pan & Gla","HARDWARE",5,9,4],
-                    ["110943","BUTTERFLY CAN OPEN","HARDWARE",2,12,10],
-                    ["110942","ICECREAM SCOOP CHROME PLATED","HARDWARE",1,6,5],
-                    ["126020","DESSERT FORK 4pk","HARDWARE",1,2,1],
-                    ["126373","Safety pins 120pcs","HARDWARE",4,6,2],
-                    ["110939","S/S Gravy Boat","HARDWARE",1,12,11],
-                    ["132652","HWARE DOG LEASH 3M J19E","HARDWARE",1,8,7],
-                    ["132023","FARMHOUSE RAMEKIN 9.5 X 5CM /","HARDWARE",2,8,6],
-                    ["116805","DESSERT KNIFE 4pk","HARDWARE",1,8,7],
-                    ["117088","Kettle Coloured 3.5L","HARDWARE",6,8,2]
-                ],
+                data: null,
                 settings: {
                     licenseKey: 'non-commercial-and-evaluation',
                     colHeaders: ["SKU", "Name", "Category", "Qty Avail (Web)", "Qty Ord (Web)", "Qty Req (WH)"],
