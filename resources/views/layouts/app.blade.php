ae45d415ea87f16c95213ffd04cd8ba5805acb78
Artur Hanusek <artur@coding.cab>
2019-12-24 11:45:05 +0000

    add menu link to settings route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,15 +57,20 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('settings') }}">
+                                        {{ __('Settings') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
+
+
+
                                 </div>
                             </li>
                         @endguest
