00b8c7db877dacfc1d2dc1b2af1c292c5f48585e
Artur Hanusek <artur@coding.cab>
2020-01-04 21:01:21 +0000

    add quantity_available column

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Inventory extends Model
 {
     //
+
+    protected $appends = [
+        "quantity_available"
+    ];
+
+    public function getQuantityAvailableAttribute()
+    {
+        $quantity_available = $this->quantity - $this->quantity_reserved;
+
+        if ($quantity_available<0) {
+            return 0;
+        }
+
+        return $quantity_available;
+    }
+
 }
