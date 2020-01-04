57f980e2a27dba019f9045413dfb5c1669123cac
Artur Hanusek <artur@coding.cab>
2020-01-04 22:29:43 +0000

    create product

--- tests/Feature/PostInventoryUpdateTest.php
+++ tests/Feature/PostInventoryUpdateTest.php
@@ -2,7 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Inventory;
+use App\Models\Product;
 use App\User;
+use Faker\Generator;
+use Illuminate\Support\Facades\Event;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -14,9 +18,13 @@ class PostInventoryUpdateTest extends TestCase
     public function test_if_post_is_succesfull() {
 
         // assign
+        Event::fake();
+
         Passport::actingAs(
             factory(User::class)->create()
         );
+
+        $product = factory(Product::class)->create();
 
         // act
         $response = $this->post('api/inventory');
