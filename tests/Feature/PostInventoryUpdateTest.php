f0ab03643261ae70bed30fef7950dbd255f8fadf
Artur Hanusek <artur@coding.cab>
2020-01-04 21:49:31 +0000

    create authenticated post to inventory route

--- tests/Feature/PostInventoryUpdateTest.php
+++ tests/Feature/PostInventoryUpdateTest.php
@@ -2,12 +2,29 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PostInventoryUpdateTest extends TestCase
 {
+
+    public function test_if_post_is_succesfull() {
+
+        // assign
+        Passport::actingAs(
+            factory(User::class)->create()
+        );
+
+        // act
+        $response = $this->post('api/inventory');
+
+        // assert
+        $response->assertStatus(200);
+    }
+
     /**
      * A basic feature test example.
      *
