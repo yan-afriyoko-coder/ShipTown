ddf949c0060ab99457d0a347c096d93714248e3e
Artur Hanusek <arthur@youritsolutions.ie>
2020-02-07 14:01:03 +0000

    Fix columns and calculations

--- resources/js/components/Products.vue
+++ resources/js/components/Products.vue
@@ -17,16 +17,28 @@
                         var data = response.data;
 
                         data = data.map(record => {
+
+                            let quantity_on_hand_rms_web = record.inventory['100'] ? record.inventory['100'].quantity: 0;
+                            let quantity_reserved_web = record.inventory['999'] ? record.inventory['999'].quantity_reserved: 0;
+                            let quantity_available_rms_web = record.inventory['100'] ? record.inventory['100'].quantity_available: 0;
+                            let quantity_required = quantity_reserved_web > quantity_available_rms_web ? quantity_reserved_web - quantity_available_rms_web: 0;
+                            let quantity_available_warehouse = record.inventory['99'] ? record.inventory['99'].quantity_available: 0;
+
                             return [
-                                record.sku,// sku
-                                record.name, // product name
-                                record.category, // category
-                                record.inventory['100'] ? record.inventory['100'].quantity: 0, // quantity on hand in web
-                                record.inventory['999'] ? record.inventory['999'].quantity_reserved: 0, // quantity reserved in web
-                                record.inventory['100'] ? record.inventory['100'].quantity_available: 0, // quantity available in web
-                                record.inventory['100'] ? record.inventory['100'].quantity - record.inventory['999'].quantity_reserved:'', // quantity required from warehouse
-                                record.inventory['99'] ? record.inventory['99'].quantity_available:'', // quantity available in warehous
+                                record.sku,
+                                record.name,
+                                record.category,
+                                quantity_on_hand_rms_web,
+                                quantity_reserved_web,
+                                quantity_available_rms_web,
+                                quantity_required,
+                                quantity_available_warehouse,
                             ];
+
+                        });
+
+                        data = data.filter(record => {
+                            return (record[6] > 0) && (record[7] > 0);
                         });
 
                         this.data = data;
