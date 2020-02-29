61c9ae48f4a7aa6563b0b29e5c5b31ff345ab117
Artur Hanusek <arthur@youritsolutions.ie>
2020-02-29 17:39:40 +0000

    move products get json structure test

--- tests/Feature/ProductGetRouteTest.php
+++ tests/Feature/ProductGetRouteTest.php
@@ -12,35 +12,6 @@ class ProductGetRouteTest extends TestCase
 {
     use AuthorizedUserTestCase;
 
-    public function test_products_response_structure()
-    {
-        // We will send item to make sure
-        // at lease one item exists
-        $this->json("POST", '/api/products', ModelSample::PRODUCT);
-
-        $response = $this->get('/api/products');
-
-        $response->assertStatus(200);
-
-        $response->assertJsonStructure([
-            "current_page",
-            "data" => [
-                "*" => [
-                    "sku",
-                    "name",
-                    "price",
-                    "sale_price",
-                    "sale_price_start_date",
-                    "sale_price_end_date",
-                    "quantity",
-                    "quantity_reserved",
-                    "quantity_available"
-                ]
-            ],
-            "total",
-        ]);
-    }
-
     /**
      * @return void
      */
--- tests/Feature/ProductsRoutesTest.php
+++ tests/Feature/ProductsRoutesTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use App\User;
 use Laravel\Passport\Passport;
 use Tests\Feature\AuthorizedUserTestCase;
@@ -30,5 +31,39 @@ class ProductsRoutesTest extends TestCase
         $response = $this->get('/products');
 
         $response->assertStatus(302);
+    }
+
+
+    public function test_get_products_response_structure()
+    {
+        Passport::actingAs(
+            factory(User::class)->make()
+        );
+
+        $product = factory(Product::class)->make();
+
+        $this->json("POST", '/api/products', $product->toArray());
+
+        $response = $this->get('/api/products');
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            "current_page",
+            "data" => [
+                "*" => [
+                    "sku",
+                    "name",
+                    "price",
+                    "sale_price",
+                    "sale_price_start_date",
+                    "sale_price_end_date",
+                    "quantity",
+                    "quantity_reserved",
+                    "quantity_available"
+                ]
+            ],
+            "total",
+        ]);
     }
 }
