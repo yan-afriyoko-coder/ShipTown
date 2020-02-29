ef4a1049863b9bb70abce19f8c26b4a0b7b51964
Artur Hanusek <arthur@youritsolutions.ie>
2020-02-29 17:23:16 +0000

    rename ProductsRouteTest

--- tests/Feature/ProductsRoutesTest.php
+++ tests/Feature/ProductsRoutesTest.php
@@ -7,7 +7,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ProductsRouteTest extends TestCase
+class ProductsRoutesTest extends TestCase
 {
     use AuthorizedUserTestCase;
 
