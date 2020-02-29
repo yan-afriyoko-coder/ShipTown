89d17a6e5bb47ee7143029946044bd6842634f2f
Artur Hanusek <arthur@youritsolutions.ie>
2020-02-29 14:39:29 +0000

    move tests to InventoryRoutesTest

--- tests/Feature/InventoryRoutesTest.php
+++ tests/Feature/InventoryRoutesTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Inventory;
+use App\Models\Product;
 use App\User;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
@@ -19,9 +21,44 @@ class InventoryRoutesTest extends TestCase
 
     public function test_get_route_authorized()
     {
-        Passport::actingAs(factory(User::class)->make());
+        Passport::actingAs(
+            factory(User::class)->create()
+        );
 
         $response = $this->get('/api/inventory');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_if_cant_post_without_data()
+    {
+        Passport::actingAs(
+            factory(User::class)->create()
+        );
+
+        $response = $this->postJson('/api/inventory', []);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_quantity_update()
+    {
+        Passport::actingAs(
+            factory(User::class)->create()
+        );
+
+        $inventory = factory(Inventory::class)->make();
+
+        $product = factory(Product::class)->create();
+
+        $update = [
+            'sku' => $product->sku,
+            'location_id' => 0,
+            'quantity' => $inventory->quantity,
+            'quantity_reserved' => $inventory->quantity_reserved
+        ];
+
+        $response = $this->postJson('/api/inventory', $update);
 
         $response->assertStatus(200);
     }
