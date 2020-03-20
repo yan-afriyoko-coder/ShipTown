515bf975b1de504d50a3e4d87f0c95dc6e1334ac
Artur Hanusek <arthur@youritsolutions.ie>
2020-03-20 10:59:36 +0000

    create CompanyController

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    //
+    public function storeConfiguration(Request $request)
+    {
+        return $this->respond_OK_200();
+    }
 }
