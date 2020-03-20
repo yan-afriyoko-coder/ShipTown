d40d186835b9d43adb29cbdc35732ff68e0e8ca7
Artur Hanusek <arthur@youritsolutions.ie>
2020-03-20 09:54:10 +0000

    Add api2cart store_key form to GUI

---
+++ resources/js/components/Api2CartConfiguration.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <div>
+            <div class="card card-default">
+                <div class="card-header">
+                    <div style="display: flex; justify-content: space-between; align-items: center;">
+                        <span>
+                            API2CART Configuration
+                        </span>
+                    </div>
+                </div>
+
+                <div class="card-body">
+
+                    <table class="table table-borderless mb-0">
+                        <thead>
+                            <tr>
+                                <th>
+                                    <div class="form-group">
+                                        <label for="storeKey">Store Key</label>
+                                        <input type="text" class="form-control" id="storeKey" v-model="storeKey" placeholder="Store Key">
+                                    </div>
+                                </th>
+                            </tr>
+                            <tr>
+                                <th><div class="text-center"><button id="product-btn" class="btn-primary centre" v-on:click="saveStoreKey">Save</button></div></th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                storeKey: null
+            };
+        },
+
+        methods: {
+            saveStoreKey : function() {
+
+                console.log(this.data);
+
+                axios.post('/api/configuration', this.data)
+                    .then(response => {
+                        alert("Configuration saved");
+                    })
+                    .catch(error => {
+                        alert("Something went wrong. Please try again");
+                    });
+            }
+        }
+    }
+
+</script>
