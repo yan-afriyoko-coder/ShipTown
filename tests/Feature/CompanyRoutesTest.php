6597cd3c236555eb9e86c026f287bc0b051874d9
Artur Hanusek <arthur@youritsolutions.ie>
2020-03-20 11:04:48 +0000

    add test for authenticated company post route

--- tests/Feature/CompanyRoutesTest.php
+++ tests/Feature/CompanyRoutesTest.php
@@ -2,12 +2,27 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CompanyRoutesTest extends TestCase
 {
+    public function test_if_post_route_works_when_authenticated()
+    {
+        Passport::actingAs(
+            factory(User::class)->create()
+        );
+
+        $data = [];
+
+        $response = $this->postJson('api/company/configuration', $data);
+
+        $response->assertOk();
+    }
+
     public function test_if_post_route_requires_authentication()
     {
         $data = [
