e015d428c6b24f8af8bc6b47e64c5d954c29c41d
Artur Hanusek <arthur@youritsolutions.ie>
2020-03-26 18:52:07 +0000

    create ConfigurationApi2cart model

---
+++ app/Models/ConfigurationApi2cart.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ConfigurationApi2cart extends Model
+{
+    //
+    protected $fillable = [
+        "bridge_api_key",
+        "last_synced_modified_at"
+    ];
+}
