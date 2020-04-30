714e0fd9ee9adc5c5cd4c209880ab030282a3939
Jose Consador <joseconsador@gmail.com>
2020-04-30 14:50:54 +0800

    allow searching of product sku and name

--- resources/js/components/Products.vue
+++ resources/js/components/Products.vue
@@ -1,34 +1,46 @@
 <template>
     <div class="container">
-        <div v-if="productsData.total > productsData.per_page" class="row">
-            <ul class="pagination">
-                <li :class="{
-                    'page-item': true,
-                    'disabled': productsData.current_page == 1
-                }">
-                    <a class="page-link" href="#" aria-label="Previous" @click.prevent="prev">
-                        <span aria-hidden="true">&laquo;</span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Next">
-                        {{ productsData.from }} - {{ productsData.to }} of {{ productsData.total }}
-                    </a>
-                </li>
-                <li :class="{
-                    'page-item': true,
-                    'disabled': productsData.current_page == productsData.last_page
-                }">
-                    <a class="page-link" href="#" aria-label="Next" @click.prevent="next">
-                        <span aria-hidden="true">&raquo;</span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
         <div class="row">
+            <div class="col">
+                <input class="form-control" @keyup.enter="handleSearchEnter" @blur="loadProductList(1)" v-model="query" placeholder="Search for products..." />
+            </div>
+            <div class="col">
+                <ul v-if="productsData.total > productsData.per_page" class="pagination">
+                    <li :class="{
+                        'page-item': true,
+                        'disabled': productsData.current_page == 1
+                    }">
+                        <a class="page-link" href="#" aria-label="Previous" @click.prevent="prev">
+                            <span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            {{ productsData.from }} - {{ productsData.to }} of {{ productsData.total }}
+                        </a>
+                    </li>
+                    <li :class="{
+                        'page-item': true,
+                        'disabled': productsData.current_page == productsData.last_page
+                    }">
+                        <a class="page-link" href="#" aria-label="Next" @click.prevent="next">
+                            <span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div v-if="productsData.total > 0" class="row">
             <hot-table :data="tableData" :settings="settings"></hot-table>
+        </div>
+        <div class="row" v-else>
+            <div class="col">
+                <div class="alert alert-info" role="alert">
+                    No products found.
+                </div>            
+            </div>
         </div>
         <div class="row" style="margin-top: 10px">
             <pagination :data="productsData" @pagination-change-page="loadProductList"></pagination>
@@ -55,26 +67,38 @@
         methods: {
             loadProductList: function(page) {
                 this.showLoading();
-                axios.get(`/api/products?page=${page}`).then(({ data }) => {
+                axios.get('/api/products', {
+                    params: {
+                        page: page,
+                        q: this.query,
+                    }
+                }).then(({ data }) => {
                     this.productsData = data;
                 }).then(this.hideLoading);
             },
 
             next() {
+                console.log(this.productsData.current_page);
                 this.loadProductList(this.productsData.current_page + 1);
             },
 
             prev() {
                 this.loadProductList(this.productsData.current_page -1);
+            },
+
+            handleSearchEnter(event) {
+                event.target.blur();
             }
         },
 
         data: function() {
             return {
+                query: null,
                 productsData: {
                     data: [],
                     total: 0,
                     per_page: 100,
+                    current_page: 1,
                 },
                 settings: {
                     licenseKey: 'non-commercial-and-evaluation',
