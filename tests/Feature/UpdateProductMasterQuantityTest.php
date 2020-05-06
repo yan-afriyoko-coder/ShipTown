9dffa1ad0b142530481325c164572bd39855b681
Artur Hanusek <arthur@youritsolutions.ie>
2020-05-06 10:51:58 +0000

    make inventory deleted test

--- tests/Feature/UpdateProductMasterQuantityTest.php
+++ tests/Feature/UpdateProductMasterQuantityTest.php
@@ -28,4 +28,26 @@ class UpdateProductMasterQuantityTest extends TestCase
 
        $this->assertEquals($quantity_expected, $product->quantity);
     }
+
+    /**
+     *
+     */
+    public function test_if_quantity_updated_on_inventory_delete()
+    {
+        $product = factory(Product::class)->create();
+
+        $inventory = factory(Inventory::class)->create([
+            "product_id" => $product->id
+        ]);
+
+        $product = $product->fresh();
+
+        $quantity_expected = $product->quantity - $inventory->quantity;
+
+        $inventory->delete();
+
+        $product = $product->fresh();
+
+        $this->assertEquals($quantity_expected, $product->quantity);
+    }
 }
