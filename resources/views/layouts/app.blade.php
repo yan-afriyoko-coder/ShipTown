3421f33215cafa09cfc95b8754d660d563ee7ca3
Jose Consador <joseconsador@gmail.com>
2020-05-11 14:59:19 +0800

    add order and apt widget, set /dashboard as default route, add laravel-widgets component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,9 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
+                            <a class="nav-link" href="{{ route('dashboard') }}">{{ __('Dashboard') }}</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" href="{{ route('products') }}">{{ __('Products') }}</a>
                         </li>
                         <li class="nav-item">
---
+++ resources/views/widgets/apt.blade.php
@@ -0,0 +1,9 @@
+<div class="card text-center">
+  <div class="card-header">
+    30 DAY APT
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">NOT "Processing"</h5>
+    <p class="card-text">98h 53m</p>
+  </div>
+</div>
