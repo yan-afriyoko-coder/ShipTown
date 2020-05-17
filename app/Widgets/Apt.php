8b86f83c4dbc7b9c323d6bd318a254d771b4956f
Artur Hanusek <arthur@youritsolutions.ie>
2020-05-17 18:44:47 +0000

    calculate apt

--- app/Widgets/Apt.php
+++ app/Widgets/Apt.php
@@ -2,6 +2,7 @@
 
 namespace App\Widgets;
 
+use App\Models\Order;
 use Arrilot\Widgets\AbstractWidget;
 
 class Apt extends AbstractWidget
@@ -19,10 +20,36 @@ class Apt extends AbstractWidget
      */
     public function run()
     {
-        //
+        $apt_seconds  = Order::query()
+            ->selectRaw("AVG(TIME_TO_SEC(TIMEDIFF(order_closed_at, order_placed_at))) as apt")
+            ->whereRaw('order_closed_at > order_placed_at')
+            ->whereRaw('order_closed_at BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE() ')
+            ->value('apt');
 
         return view('widgets.apt', [
             'config' => $this->config,
+            'apt_string' => $this->timeDiffForPrez($apt_seconds)
         ]);
+    }
+
+    function timeDiffForPrez(int $diffInSeconds)
+    {
+        $result = '';
+
+        $data = array(
+            'd' => 86400,
+            'h' => 3600,
+            'm' => 60
+        );
+
+        foreach ($data as $k => $v) {
+            if ($diffInSeconds >= $v) {
+                $diff = floor($diffInSeconds / $v);
+                $result .= " $diff" . ($diff > 1 ? $k : substr($k, 0, -1));
+                $diffInSeconds -= $v * $diff;
+            }
+        }
+
+        return $result;
     }
 }
--- resources/views/widgets/apt.blade.php
+++ resources/views/widgets/apt.blade.php
@@ -1,7 +1,7 @@
-<div class="card text-center">  
+<div class="card text-center">
   <div class="card-body">
     <h4 class="card-title">30 DAY APT</h5>
     <h5 class="card-title">NOT "Processing"</h5>
-    <h2 class="card-text"><strong>98h 53m</strong></h2>
+    <h2 class="card-text"><strong>{{  $apt_string }}m</strong></h2>
   </div>
 </div>
