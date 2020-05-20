b7701490d81e47b6cb6a7619ed2908cebdd3f0b8
Jose Consador <joseconsador@gmail.com>
2020-05-20 13:44:16 +0800

    fix 30 day order widget styling, add help-tip css

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,9 +2,11 @@
 
 @section('content')
 <div class="container">
-    <div class="row row-cols-sm-2">
+    <div class="row row-cols-sm-2 mb-2">
         <div class="col">@widget('ordersProcessing')</div>
         <div class="col">@widget('apt')</div>
+    </div>
+    <div class="row">
         <div class="col">@widget('StatusOrderCount')</div>
     </div>
 </div>
--- resources/views/widgets/status_order_count.blade.php
+++ resources/views/widgets/status_order_count.blade.php
@@ -1,17 +1,28 @@
-<div class="card text-center">
+<div class="help-tip">
+    <p>This is the inline help tip! It can contain all kinds of HTML. Style it as you please.</p>
+</div>
+<div class="card">
     <div class="card-body">
-        <h4 class="card-title">LAST 30 DAY ORDERS</h5>
-        <table>
-            <tr>
-                <th>Status</th>
-                <th>Count</th>
-            </tr>
-            @foreach ($status_order_counts as $status)
-                <tr>
-                    <td>{{ $status->status_code }}</td>
-                    <td>{{ $status->order_count }}</td>
-                </tr>
-            @endforeach
-        </table>
+        <h4 class="card-title text-center">LAST 30 DAY ORDERS</h5>
+        <div class="row">
+            <div class="col-8 offset-2">
+                <table class="table table-borderless">
+                    <thead>
+                        <tr>
+                            <th scope="col">Status</th>
+                            <th scope="col" class="text-right">Count</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($status_order_counts as $status)
+                        <tr>
+                            <td>{{ $status->status_code }}</td>
+                            <td class="text-right">{{ $status->order_count }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
