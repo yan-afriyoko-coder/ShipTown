32c5e7f5ce4226c8de6a6050776e1834b5aef4f7
Artur Hanusek <arthur@youritsolutions.ie>
2020-05-20 14:56:48 +0000

    update order factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -6,8 +6,23 @@ use App\Models\Order;
 use Faker\Generator as Faker;
 
 $factory->define(Order::class, function (Faker $faker) {
+
+    $status_code = $faker->randomElement(['complete','processing','cancelled']);
+
+    $order_placed_at = $faker->dateTimeBetween('-1 year');
+
+    $order_closed_at = null;
+
+    if($status_code !== 'processing') {
+        $order_closed_at = $faker->dateTimeBetween($order_placed_at);
+    }
+
     return [
         'order_number' => (string) (10000000 + $faker->unique()->randomNumber(7)),
-        'status_code' => $faker->randomElement(['complete','processing','cancelled'])
+        'order_placed_at' => $order_placed_at,
+        'order_closed_at' => $order_closed_at,
+        'products_count' => $faker->randomNumber(2),
+        'status_code' => $status_code,
     ];
+
 });
