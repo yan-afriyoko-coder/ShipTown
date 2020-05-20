dc1a5490c2951f61e9d3cd28982a2e4bc95814b3
Artur Hanusek <arthur@youritsolutions.ie>
2020-05-20 15:00:05 +0000

    update typos in widget tips

--- resources/views/widgets/orders_processing.blade.php
+++ resources/views/widgets/orders_processing.blade.php
@@ -1,10 +1,13 @@
 <div class="help-tip">
-    <p>Number of order with "processing" status code</p>
+    <p>
+        Total number of orders with following status code:<br>
+        - processing
+    </p>
 </div>
     <div class="card text-center">
   <div class="card-body">
-    <h4 class="card-title">ORDERS</h5>
-    <h5 class="card-title">"Processing"</h5>
+    <h4 class="card-title">PENDING ORDERS</h5>
+    <h5 class="card-title"></h5>
     <h2 class="card-text"><strong>{{ $count }}</strong></h2>
   </div>
 </div>
