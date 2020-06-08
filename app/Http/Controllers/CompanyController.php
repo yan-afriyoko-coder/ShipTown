39fcd23f5d2388bcd554e635c8afabbb3b59f470
Artur Hanusek <arthur@youritsolutions.ie>
2020-06-08 14:30:34 +0000

    remove old bridge_api_key company_configuration references

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -10,10 +10,6 @@ class CompanyController extends Controller
 {
     public function storeConfiguration(Request $request)
     {
-        CompanyConfiguration::query()->updateOrCreate([], [
-            "bridge_api_key" => $request->get('bridge_api_key')
-        ]);
-
         return $this->respond_OK_200();
     }
 }
--- tests/Feature/CompanyRoutesTest.php
+++ tests/Feature/CompanyRoutesTest.php
@@ -11,32 +11,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CompanyRoutesTest extends TestCase
 {
-    public function test_if_saves_bridge_api_key()
-    {
-        Passport::actingAs(
-            factory(User::class)->create()
-        );
-
-        $data = [
-            "bridge_api_key" => "12456"
-        ];
-
-        $response = $this->postJson('api/company/configuration', $data);
-
-        $configuration = CompanyConfiguration::query()->first('bridge_api_key');
-
-        $this->assertEquals($data['bridge_api_key'], $configuration['bridge_api_key']);
-    }
-
     public function test_if_post_route_works_when_authenticated()
     {
         Passport::actingAs(
             factory(User::class)->create()
         );
 
-        $data = [
-            "bridge_api_key" => "12456"
-        ];
+        $data = [];
 
         $response = $this->postJson('api/company/configuration', $data);
 
