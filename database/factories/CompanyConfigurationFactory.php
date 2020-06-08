d56baacd01c73281b05aa36d392693458397a642
Artur Hanusek <arthur@youritsolutions.ie>
2020-06-08 17:43:15 +0000

    remove bridge_api_key from factory

--- database/factories/CompanyConfigurationFactory.php
+++ database/factories/CompanyConfigurationFactory.php
@@ -7,6 +7,5 @@ use Faker\Generator as Faker;
 
 $factory->define(CompanyConfiguration::class, function (Faker $faker) {
     return [
-        'bridge_api_key' =>'ed58a22dfecb405a50ea3ea56979360d'
     ];
 });
