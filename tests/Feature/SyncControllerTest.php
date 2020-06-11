68726edb9ea2a070d22faee2777300668a452687
Artur Hanusek <arthur@youritsolutions.ie>
2020-06-11 16:40:42 +0000

    add sync routes

---
+++ tests/Feature/SyncControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Controllers\SyncController;
+use App\Jobs\Api2cart\ImportOrdersJob;
+use App\Jobs\Rmsapi\ImportProductsJob;
+use App\User;
+use Illuminate\Support\Facades\Bus;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SyncControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_if_dispatches_jobs()
+    {
+        Bus::fake();
+
+        Passport::actingAs(
+            factory(User::class)->create()
+        );
+
+        $response = $this->get('/api/sync');
+
+        Bus::assertDispatched(ImportOrdersJob::class);
+        Bus::assertDispatched(ImportProductsJob::class);
+    }
+}
