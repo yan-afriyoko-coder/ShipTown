507d5a7ecd0cd658fadeebb5ebf84968c5512db8
Artur Hanusek <arthur@youritsolutions.ie>
2020-06-18 09:31:49 +0000

    refactor

--- app/Jobs/Rmsapi/ImportProductsJob.php
+++ app/Jobs/Rmsapi/ImportProductsJob.php
@@ -37,9 +37,9 @@ class ImportProductsJob implements ShouldQueue
     public function handle()
     {
         $params = [
-            'min:db_change_stamp' => $this->rmsapiConnection->products_last_timestamp,
             'per_page' => config('rmsapi.import.products.per_page'),
             'order_by'=> 'db_change_stamp:asc',
+            'min:db_change_stamp' => $this->rmsapiConnection->products_last_timestamp,
         ];
 
         $products = RmsapiClient::GET($this->rmsapiConnection, 'api/products', $params);
