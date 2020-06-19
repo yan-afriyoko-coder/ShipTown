5aa8e94beab4180c62bfa8e33f8f5df67de0f3a8
Artur Hanusek <arthur@youritsolutions.ie>
2020-06-19 11:25:47 +0000

    remove duplicate test and rename other

--- tests/External/Api2cart/ImportOrdersJobTest.php
+++ tests/External/Api2cart/ImportOrdersJobTest.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
-class ImportOrdersFromApi2cartJobTest extends TestCase
+class ImportOrdersJobTest extends TestCase
 {
     public function test_if_job_runs_without_exceptions()
     {
