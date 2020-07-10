3429152e9302f3cdff7a0acfd11c019aace88c13
Artur Hanusek <arthur@youritsolutions.ie>
2020-07-10 19:19:20 +0000

    add product label on picklist view

--- resources/js/components/PicklistItem.vue
+++ resources/js/components/PicklistItem.vue
@@ -9,6 +9,7 @@
                             <div class="col-md-8">
                                 <div class="text-primary h4">{{ name }}</div>
                                 <div class="text-secondary h5">sku: <span class="font-weight-bold"> {{ sku }}</span></div>
+                                <div class="text-secondary h5">product: <span class="font-weight-bold"> ------ </span></div>
                             </div>
                             <div class="col-md-4">
                                 <div class="row pt-1 mt-1 text-center border-top">
