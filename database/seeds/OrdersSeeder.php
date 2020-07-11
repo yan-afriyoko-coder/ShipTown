e8bc8fd7c4faadc30028e5cee1f781554948113b
Artur Hanusek <arthur@youritsolutions.ie>
2020-07-11 12:18:10 +0000

    add order factory with SKU not in db

--- database/seeds/OrdersSeeder.php
+++ database/seeds/OrdersSeeder.php
@@ -3,6 +3,7 @@
 use App\Models\Order;
 use App\Models\OrderProduct;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class OrdersSeeder extends Seeder
 {
@@ -13,11 +14,32 @@ class OrdersSeeder extends Seeder
      */
     public function run()
     {
-        factory(Order::class,50)
+        factory(Order::class,10)
             ->create()->each(function ($order) {
                 $orderProducts = factory(OrderProduct::class, rand(1,20))->make();
 
                 $order->orderProducts()->saveMany($orderProducts);
             });
+
+        // we fabricate few orders with SKU not present in database
+        factory(Order::class,5)
+            ->create()->each(function ($order) {
+
+                $orderProducts = collect(factory(OrderProduct::class, rand(1,20))->make())
+                    ->map(function ($orderProduct) {
+
+                        $suffix = Arr::random(['-blue', '-red', '-green', '-xl', '-small-orange']);
+
+                        $orderProduct['product_id'] = 0;
+                        $orderProduct['sku_ordered'] = $orderProduct['sku_ordered'] . $suffix;
+                        $orderProduct['name_ordered'] = $orderProduct['name_ordered'] . $suffix;
+
+                        return $orderProduct;
+                    });
+
+                $order->orderProducts()->saveMany($orderProducts);
+
+            });
+
     }
 }
