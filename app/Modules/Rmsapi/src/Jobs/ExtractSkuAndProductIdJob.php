225cd5fef09ae303063b7b71d0fd9dc9a543b397
Artur Hanusek <arthur@youritsolutions.ie>
2020-07-12 15:45:25 +0000

    increase batch size

--- app/Modules/Rmsapi/src/Jobs/ExtractSkuAndProductIdJob.php
+++ app/Modules/Rmsapi/src/Jobs/ExtractSkuAndProductIdJob.php
@@ -34,7 +34,7 @@ class ExtractSkuAndProductIdJob implements ShouldQueue
         $productImports = RmsapiProductImport::query()
             ->whereNotNull('when_processed')
             ->whereNull('sku')
-            ->limit(10)
+            ->limit(50)
             ->get();
 
         foreach ($productImports as $importedProduct ) {
