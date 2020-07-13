012ac9ba489d156c5aed5fa3ee355cb2f3aee526
Artur Hanusek <arthur@youritsolutions.ie>
2020-07-13 22:35:14 +0000

    change products view layout

--- resources/js/components/Products.vue
+++ resources/js/components/Products.vue
@@ -14,57 +14,35 @@
                 </div>
             </div>
             <template v-else class="row">
-                <div class="row mb-3">
-                    <div class="col">
-                        <div class="row">
-                            <div class="col-3">
-                                <div class="row header-row text-left">
-                                    <div class="col">SKU</div>
-                                </div>
-                            </div>
-                            <div class="col-3">
-                                <div class="row header-row text-left">
-                                    <div class="col">NAME</div>
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="row header-row">
-                                    <div class="col-3 text-left">Location</div>
-                                    <div class="col-3 text-left">In Stock</div>
-                                    <div class="col-2 text-left">Reserved</div>
-                                    <div class="col-2 text-left">Available</div>
-                                    <div class="col-2 text-left">Shelf</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <template v-for="product in products">
-                    <div class="row mb-3">
-                        <div class="col">
-                            <div class="row">
-                                <div class="col-3 text-left">
+
+                    <div class="row mb-3 ml-1 mr-1">
+                        <div class="col p-2 pl-3">
+                            <div class="row text-left">
+                                <div class="col-md-6">
+                                    <div class="text-primary h4">{{ product.name }}</div>
+                                    <div class="text-secondary h5">sku: <span class="font-weight-bold"> {{ product.sku }} </span></div>
+                                </div>
+                                <div class="col-md-6">
                                     <div class="row">
-                                        <div>{{ product.sku }}</div>
+                                        <div class="col-3 font-weight-bold">Location</div>
+                                        <div class="col-3 font-weight-bold">In Stock</div>
+                                        <div class="col-2 font-weight-bold">Reserved</div>
+                                        <div class="col-2 font-weight-bold">Available</div>
+                                        <div class="col-2 font-weight-bold">Shelf</div>
                                     </div>
-                                </div>
-                                <div class="col-3 text-left">
-                                    <div class="row text-left">
-                                        <div class="row text-left">{{ product.name }}</div>
+                                    <div class="row" v-for="warehouse_inventory in product.inventory">
+                                        <div class="col-3">{{ warehouse_inventory.location_id }}</div>
+                                        <div class="col-3">{{ warehouse_inventory.quantity }}</div>
+                                        <div class="col-2">{{ warehouse_inventory.quantity_reserved }}</div>
+                                        <div class="col-2">{{ warehouse_inventory.quantity - warehouse_inventory.quantity_reserved }}</div>
+                                        <div class="col-2">{{ warehouse_inventory.shelve_location }}</div>
                                     </div>
-                                </div>
-                                <div class="col-6 text-right">
-                                        <div class="row" v-for="warehouse_inventory in product.inventory">
-                                            <div class="col-3">{{ warehouse_inventory.location_id }}</div>
-                                            <div class="col-3">{{ warehouse_inventory.quantity }}</div>
-                                            <div class="col-2">{{ warehouse_inventory.quantity_reserved }}</div>
-                                            <div class="col-2">{{ warehouse_inventory.quantity - warehouse_inventory.quantity_reserved }}</div>
-                                            <div class="col-2">{{ warehouse_inventory.shelve_location }}</div>
-                                        </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </template>
             </template>
         </div>
