9a6e6b233c40675b20bc84a2138ec935f0da5fe1
Artur Hanusek <arthur@youritsolutions.ie>
2020-07-17 11:07:11 +0000

    add basic test to check if event is dispatched

---
+++ tests/Unit/Jobs/Api2cart/ProcessImportedOrderJobTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit\Jobs\Api2cart;
+
+use App\Events\OrderCreatedEvent;
+use App\Jobs\Api2cart\ProcessImportedOrderJob;
+use App\Models\Api2cartOrderImports;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProcessImportedOrderJobTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        Event::fake();
+
+        $importedOrder = factory(Api2cartOrderImports::class)->create();
+
+        ProcessImportedOrderJob::dispatch($importedOrder);
+
+        Event::assertDispatched(OrderCreatedEvent::class);
+    }
+}
