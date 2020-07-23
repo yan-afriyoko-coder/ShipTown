e61f7aad3673ceeaaa771a6e507594f280058db0
Artur Hanusek <arthur@youritsolutions.ie>
2020-07-23 08:10:55 +0000

    make test

--- tests/Unit/Jobs/Api2cart/ProcessImportedOrderJobTest.php
+++ tests/Unit/Jobs/Api2cart/ProcessImportedOrderJobTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit\Jobs\Api2cart;
 
 use App\Events\OrderCreatedEvent;
 use App\Jobs\Api2cart\ProcessApi2cartImportedOrderJob;
+use App\Models\Order;
+use App\Models\OrderProduct;
 use App\Modules\Api2cart\src\Models\Api2cartOrderImports;
 use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
@@ -19,12 +21,25 @@ class ProcessImportedOrderJobTest extends TestCase
      */
     public function testExample()
     {
+        // prepare
         Event::fake();
+
+        Order::query()->forceDelete();
+        OrderProduct::query()->forceDelete();
+        Api2cartOrderImports::query()->forceDelete();
 
         $importedOrder = factory(Api2cartOrderImports::class)->create();
 
         ProcessApi2cartImportedOrderJob::dispatch($importedOrder);
 
-        Event::assertDispatched(OrderCreatedEvent::class);
+        $order = Order::query()
+            ->where([
+                'order_number' => $importedOrder['raw_import']['id']
+            ])
+            ->first();
+
+        $this->assertNotNull($order, 'Order does not exist in database');
+        $this->assertNotNull($order->status_code, 'Status code missing');
+
     }
 }
