5c602ec3ad13fc836a4660d3c116f5371a99cabf
Artur Hanusek <arthur@youritsolutions.ie>
2020-08-01 17:02:38 +0000

    add ProductAliasSeeder to database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -31,6 +31,7 @@ class DatabaseSeeder extends Seeder
             RmsapiConnectionSeeder::class,
             RmsapiProductImportSeeder::class,
             PicklistSeeder::class,
+            ProductAliasSeeder::class,
         ]);
 
     }
