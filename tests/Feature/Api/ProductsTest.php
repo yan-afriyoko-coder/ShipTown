f3d9a10efa085b99aa25afa10298d521ed46311d
Artur Hanusek <arthur@youritsolutions.ie>
2020-08-06 18:23:07 +0000

    move ProductTest

--- tests/Feature/Api/ProductsTest.php
+++ tests/Feature/Api/ProductsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Api;
 
 use App\Models\Product;
 use App\User;
@@ -10,7 +10,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ProductsRoutesTest extends TestCase
+class ProductsTest extends TestCase
 {
     public function test_if_product_found_using_alias()
     {
