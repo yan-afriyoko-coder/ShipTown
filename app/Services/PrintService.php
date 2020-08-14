06ad8e90edc8f0a83ebdfafd2612aeb9f9e598bb
Artur Hanusek <arthur@youritsolutions.ie>
2020-08-14 11:08:25 +0000

    small fixes

--- app/Services/PrintService.php
+++ app/Services/PrintService.php
@@ -2,14 +2,32 @@
 
 namespace App\Services;
 
+use App\Models\Configuration;
+use Exception;
 use PrintNode\Credentials;
 use PrintNode\PrintJob;
 use PrintNode\Request;
 use PrintNode\Response;
+use PrintNode\ApiKey;
 
 class PrintService
 {
     private $credentials = null;
+
+    public function __construct()
+    {
+        try {
+            $configuration = Configuration::where('key', env('PRINTNODE_CONFIG_KEY_NAME'))->first();
+        } catch (Exception $exception) {
+            $configuration = null;
+        };
+
+        if ($configuration) {
+            $this->setApiKey($configuration->value);
+        } else {
+            $this->setApiKey(null);
+        }
+    }
 
     public function setApiKey($key)
     {
@@ -17,7 +35,7 @@ class PrintService
     }
 
     /**
-     * @return Array
+     * @return \PrintNode\Printer[]
      */
     public function getPrinters()
     {
@@ -26,13 +44,13 @@ class PrintService
 
     /**
      * Sends a new print job request to the service from a PDF source.
-     * 
+     *
      * @param int|string $printerId The id of the printer you wish to print to.
      * @param string $title A title to give the print job. This is the name which will appear in the operating system's print queue.
      * @param string $content The path to the pdf file, a pdf string
-     * 
+     *
      * @return Response
-     */    
+     */
     public function newPdfPrintJob($printerId, $title, $content)
     {
         if (@is_file($content)) { // if file exists and is a file and not a directory, suppress errors
@@ -44,12 +62,12 @@ class PrintService
 
     /**
      * Sends a new print job request to the service.
-     * 
+     *
      * @param int|string $printerId The id of the printer you wish to print to.
      * @param string $title A title to give the print job. This is the name which will appear in the operating system's print queue.
      * @param string $content If contentType is pdf_uri or raw_uri, this should be the URI from which the document you wish to print can be downloaded.
      *                        If contentType is pdf_base64 or raw_base64, this should be the base64-encoding of the document you wish to print.
-     * 
+     *
      * @return Response
      */
     public function newPrintJob($printerId, $title, $content, $contentType = 'pdf_base64')
