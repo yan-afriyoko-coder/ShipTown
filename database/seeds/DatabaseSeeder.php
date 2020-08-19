f2f53b256e8a3d615e18215ee5762b9c51e7e608
Artur Hanusek <arthur@youritsolutions.ie>
2020-08-19 22:30:55 +0000

    add SingleLineOrderPicklistSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -31,6 +31,7 @@ class DatabaseSeeder extends Seeder
             PicklistSeeder::class,
             PacklistSeeder::class,
             ProductAliasSeeder::class,
+            SingleLineOrderPicklistSeeder::class,
         ]);
 
     }
