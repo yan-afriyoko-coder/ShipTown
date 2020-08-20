1dc2485b50984d7e5f3676cd2f8d0d7a2bc7b042
Artur Hanusek <arthur@youritsolutions.ie>
2020-08-20 13:22:02 +0000

    add pick_counts widget

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,10 @@
         <div class="col">@widget('ordersProcessing')</div>
         <div class="col">@widget('apt')</div>
     </div>
+    <div class="row row-cols-sm-2 mb-2">
+        <div class="col">@widget('PickCounts')</div>
+        <div class="col"></div>
+    </div>
     <div class="row">
         <div class="col">@widget('StatusOrderCount')</div>
     </div>
