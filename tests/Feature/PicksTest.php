a6644f545e526d2bab0bf28257bb0d0c39600f9f
Artur Hanusek <arthur@youritsolutions.ie>
2020-08-28 18:41:36 +0000

    create test

---
+++ tests/Feature/PicksTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PicksTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_get()
+    {
+        $response = $this->get('/api/picks');
+
+        $response->assertStatus(200);
+    }
+}
