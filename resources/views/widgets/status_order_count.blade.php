f2a6b872eb0399c196009c697cd99f3abe428d37
Artur Hanusek <arthur@youritsolutions.ie>
2020-09-01 22:46:34 +0000

    update widgets

--- resources/views/widgets/status_order_count.blade.php
+++ resources/views/widgets/status_order_count.blade.php
@@ -8,26 +8,56 @@
 </div>
 <div class="card">
     <div class="card-body">
-        <h4 class="card-title text-center">LAST 30 DAY ORDERS</h4>
-        <div class="row">
-            <div class="col-8 offset-2">
-                <table class="table table-borderless">
-                    <thead>
+        <div>
+            <h4 class="card-title text-center">ACTIVE ORDERS</h4>
+            <div class="row">
+                <div class="col-8 offset-2">
+                    <table class="table table-borderless">
+                        <thead>
                         <tr>
                             <th scope="col">Status</th>
                             <th scope="col" class="text-right">Count</th>
                         </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($status_order_counts as $status)
-                        <tr>
-                            <td>{{ $status->status_code }}</td>
-                            <td class="text-right">{{ $status->order_count }}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach ($status_order_counts as $status)
+                            @if (in_array($status->status_code, ['picking','processing','holded']))
+                                <tr>
+                                    <td>{{ $status->status_code }}</td>
+                                    <td class="text-right">{{ $status->order_count }}</td>
+                                </tr>
+                            @endif
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
+        <div>
+            <h4 class="card-title text-center">LAST 30 DAY COMPLETED ORDERS</h4>
+            <div class="row">
+                <div class="col-8 offset-2">
+                    <table class="table table-borderless">
+                        <thead>
+                        <tr>
+                            <th scope="col">Status</th>
+                            <th scope="col" class="text-right">Count</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($status_order_counts as $status)
+                            @if (! in_array($status->status_code, ['picking','processing','holded']))
+                                <tr>
+                                    <td>{{ $status->status_code }}</td>
+                                    <td class="text-right">{{ $status->order_count }}</td>
+                                </tr>
+                            @endif
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
