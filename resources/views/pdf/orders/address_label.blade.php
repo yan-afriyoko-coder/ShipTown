6dd6ec07273fa17a283115d83731e9112f7a6e90
Artur Hanusek <arthur@youritsolutions.ie>
2020-09-18 10:51:11 +0000

    update address label to include state_name

--- resources/views/pdf/orders/address_label.blade.php
+++ resources/views/pdf/orders/address_label.blade.php
@@ -1,13 +1,12 @@
 @extends('pdf.template')
 @section('content')
     <div>{{ $shipping_address['company'] }}</div>
-    <br>
     <div>{{ $shipping_address['first_name'] }} {{ $shipping_address['last_name'] }}</div>
     <div>{{ $shipping_address['address1'] }}</div>
     <div>{{ $shipping_address['address2'] }}</div>
     <div>{{ $shipping_address['city'] }} {{ $shipping_address['postcode'] }}</div>
+    <div>{{ $shipping_address['state_name'] }}</div>
     <div style="font-weight: bold">{{ $shipping_address['country_name'] }}</div>
-    <br>
     <div style="font-size: xx-small">T: {{ $shipping_address['phone'] }}</div>
     <br>
     <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($order_number, 'C39', 1,15) }}" alt="barcode" />
