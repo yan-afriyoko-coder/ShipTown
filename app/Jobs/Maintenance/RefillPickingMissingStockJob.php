38788b69b6dd406c4349bcaae728f051bf17db4f
Artur Hanusek <arthur@youritsolutions.ie>
2020-10-27 13:15:06 +0000

    create RefillPicking_WarehouseStockJob

---
+++ app/Jobs/Maintenance/RefillPickingMissingStockJob.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Jobs\Maintenance;
+
+use App\Models\Order;
+use App\Services\AutoPilot;
+use App\Services\OrderService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class RefillPickingMissingStockJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var int
+     */
+    private $maxDailyAllowed;
+
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->maxDailyAllowed = AutoPilot::getBatchSize();
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $currentOrdersInProcessCount = Order::whereIn('status_code', ['picking', 'packing_web'])->count();
+
+        logger('Refilling "picking" status (warehouse stock)', [
+            'currently_in_process' => $currentOrdersInProcessCount,
+            'max_daily_allowed' => $this->maxDailyAllowed,
+        ]);
+
+        if ($currentOrdersInProcessCount > $this->maxDailyAllowed) {
+            return;
+        }
+
+        Order::whereStatusCode('paid')
+            ->orderBy('created_at')
+            ->limit($this->maxDailyAllowed - $currentOrdersInProcessCount)
+            ->get()
+            ->each(function ($order) use ($currentOrdersInProcessCount) {
+                if (OrderService::canNotFulfill($order, 100)) {
+                    $order->update(['status_code' => 'picking']);
+                    info('RefillWebPickingStatusListJob: updated status to picking', ['order_number' => $order->order_number]);
+                    if ($currentOrdersInProcessCount > $this->maxDailyAllowed) {
+                        return;
+                    }
+                }
+            });
+    }
+}
