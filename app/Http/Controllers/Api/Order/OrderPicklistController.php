364f935bc2a2f5b72b11fca0ccedc0e972e51413
Artur Hanusek <arthur@youritsolutions.ie>
2020-11-09 19:41:44 +0000

    add initial grouping

--- app/Http/Controllers/Api/Order/OrderPicklistController.php
+++ app/Http/Controllers/Api/Order/OrderPicklistController.php
@@ -6,12 +6,25 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\OrderPicklistResource;
 use App\Models\OrderProduct;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderPicklistController extends Controller
 {
     public function index(Request $request)
     {
-        $query = OrderProduct::getSpatieQueryBuilder();
+        $query = OrderProduct::getSpatieQueryBuilder()
+            ->select([
+                'name_ordered',
+                'sku_ordered',
+                'inventory_source_shelf_location',
+                'product_id',
+            ])
+            ->groupBy([
+                'name_ordered',
+                'sku_ordered',
+                'inventory_source_shelf_location',
+                'product_id',
+            ]);
 
         return OrderPicklistResource::collection($this->getPerPageAndPaginate($request, $query, 10));
     }
