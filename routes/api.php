c0a9b48d8593072a93253a3f234169228889a6a0
Artur Hanusek <arthur@youritsolutions.ie>
2020-11-17 09:02:59 +0000

    move RmsapiConnectionController

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,7 @@ Route::middleware('auth:api')->group(function () {
 
     Route::apiResource('settings/printers', 'Api\PrintersController')->only(['index']);
     Route::apiResource('settings/widgets', 'Api\WidgetsController');
-    Route::apiResource('settings/modules/rms_api/connections', "Api\RmsapiConnectionController");
+    Route::apiResource('settings/modules/rms_api/connections', "Api\Settings\Module\Rmsapi\RmsapiConnectionController");
     Route::apiResource('settings/modules/api2cart/connections', "Api\Api2cartConnectionController");
 
     Route::put('print/order/{order_number}/{view}', 'Api\PrintOrderController@store');
@@ -49,7 +49,7 @@ Route::middleware('auth:api')->group(function () {
     Route::apiResource('picks', 'Api\PickController')->except('store');
 
     Route::resource('widgets', 'Api\WidgetsController');
-    Route::resource("rms_api_configuration", "Api\RmsapiConnectionController");
+    Route::resource("rms_api_configuration", "Api\Settings\Module\Rmsapi\RmsapiConnectionController");
     Route::resource("api2cart_configuration", "Api\Api2cartConnectionController");
 
     // Routes for users with the admin role only
