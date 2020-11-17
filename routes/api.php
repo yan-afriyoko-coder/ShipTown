1774a17cf5c08193adcde3da0d9e0f077f399e58
Artur Hanusek <arthur@youritsolutions.ie>
2020-11-17 09:42:58 +0000

    move UsersController

--- routes/api.php
+++ routes/api.php
@@ -53,7 +53,7 @@ Route::middleware('auth:api')->group(function () {
     Route::group(['middleware' => ['role:admin']], function () {
         Route::post('invites', 'InvitesController@store');
         Route::get('roles', 'Api\RolesController@index')->middleware('can:list roles');
-        Route::resource('users', 'Api\UsersController')->middleware('can:manage users');
+        Route::resource('users', 'Api\Settings\UsersController')->middleware('can:manage users');
         Route::resource('configuration', 'Api\Settings\ConfigurationController');
     });
 
