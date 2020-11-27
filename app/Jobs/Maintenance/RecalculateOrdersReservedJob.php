a70ab0b263d923941d7ed1af9082463aa0bef64e
Artur Hanusek <arthur@youritsolutions.ie>
2020-11-27 17:07:46 +0000

    refactor RecalculateOrdersreservedJob

--- app/Jobs/Maintenance/RecalculateOrdersReservedJob.php
+++ app/Jobs/Maintenance/RecalculateOrdersReservedJob.php
@@ -8,20 +8,21 @@ use App\Models\OrderStatus;
 use App\Models\Product;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
-use Spatie\Activitylog\Models\Activity;
 
 class RecalculateOrdersReservedJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function withPrefix($tableName)
-    {
-        return  config('database.connections.mysql.prefix').$tableName;
-    }
+    /**
+     * @var int
+     */
+    private $locationId;
+
     /**
      * Create a new job instance.
      *
@@ -29,7 +30,7 @@ class RecalculateOrdersReservedJob implements ShouldQueue
      */
     public function __construct()
     {
-        //
+        $this->locationId = 999;
     }
 
     /**
@@ -39,45 +40,65 @@ class RecalculateOrdersReservedJob implements ShouldQueue
      */
     public function handle()
     {
-        $locationId = 999;
+        $this->getQuantityReservedErrorsQuery()
+            // for performance purposes limit to 1000 products per job
+            ->limit(1000)
+            ->each(function ($errorRecord) {
+                activity()->on($errorRecord)->log('Incorrect quantity reserved detected');
+                Inventory::query()->updateOrCreate([
+                    'product_id' => $errorRecord->product_id,
+                    'location_id' => $this->locationId,
+                ], [
+                    'quantity_reserved' => $errorRecord->quantity_reserved_expected ?? 0
+                ]);
+            });
+    }
 
-        $quantityToShipTotals = OrderProduct::whereStatusCodeIn(OrderStatus::getOpenStatuses())
-            ->addSelect([
-                'order_products.product_id',
-                DB::raw('sum(quantity_to_ship) as total_quantity_to_ship'),
-            ])
-            ->groupBy(['order_products.product_id']);
-
-        $productQuantityReservedDifferences = Product::query()
+    /**
+     * @return Product|\Illuminate\Database\Eloquent\Builder|Builder
+     */
+    private function getQuantityReservedErrorsQuery()
+    {
+        // select all products
+        // left join specified $locationId inventory to get actual quantity reserved
+        // left join expected quantities reserved sub query
+        // select only where expected not matching actual
+        return Product::query()
             ->select([
-                'products.id',
-                DB::raw($locationId.' as location_id'),
-                DB::raw('ceil('.DB::getTablePrefix().'inventory.quantity_reserved) as quantity_reserved_actual'),
+                'products.id as product_id',
+                DB::raw($this->locationId . ' as location_id'),
+                DB::raw('ceil(' . DB::getTablePrefix() . 'inventory.quantity_reserved) as quantity_reserved_actual'),
                 'product_reserved_totals.total_quantity_to_ship as quantity_reserved_expected',
             ])
-            ->leftJoin('inventory', function ($join) use ($locationId) {
+            ->leftJoin('inventory', function ($join) {
                 $join->on('inventory.product_id', '=', 'products.id');
-                $join->on('inventory.location_id', '=', DB::raw($locationId));
+                $join->on('inventory.location_id', '=', DB::raw($this->locationId));
             })
             ->leftJoinSub(
-                $quantityToShipTotals,
+                $this->getSumQuantityToShipByProductIdQuery(),
                 'product_reserved_totals',
                 'product_reserved_totals.product_id',
                 '=',
                 'products.id'
             )
-            ->where(DB::raw('IFNULL('.DB::getTablePrefix().'inventory.quantity_reserved, 0)'), '!=', DB::raw('IFNULL(`'.DB::getTablePrefix().'product_reserved_totals`.`total_quantity_to_ship`, 0)'))
-            ->limit(1000)
-            ->get();
+            ->where(
+                DB::raw('IFNULL(' . DB::getTablePrefix() . 'inventory.quantity_reserved, 0)'),
+                '!=',
+                DB::raw('IFNULL(`' . DB::getTablePrefix() . 'product_reserved_totals`.`total_quantity_to_ship`, 0)')
+            );
+    }
 
-        foreach ($productQuantityReservedDifferences as $product) {
-            activity()->on($product)->log('Incorrect quantity reserved detected');
-            Inventory::query()->updateOrCreate([
-                'product_id' => $product->id,
-                'location_id' => $locationId,
-            ], [
-                'quantity_reserved' => $product->quantity_reserved_expected ?? 0
-            ]);
-        }
+    /**
+     * @return OrderProduct|\Illuminate\Database\Eloquent\Builder|Builder
+     */
+    private function getSumQuantityToShipByProductIdQuery()
+    {
+        return OrderProduct::query()
+            ->whereStatusCodeIn(OrderStatus::getOpenStatuses())
+            ->addSelect([
+                'product_id',
+                DB::raw('sum(quantity_to_ship) as total_quantity_to_ship'),
+            ])
+            ->groupBy(['product_id']);
     }
 }
