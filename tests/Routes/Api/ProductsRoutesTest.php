4f89ea04bf4f6c491b3e08a518eb2ca0c50d9451
Artur Hanusek <arthur@youritsolutions.ie>
2020-12-03 16:37:24 +0000

    configure Product to be taggable

--- tests/Routes/Api/ProductsRoutesTest.php
+++ tests/Routes/Api/ProductsRoutesTest.php
@@ -13,8 +13,43 @@ class ProductsRoutesTest extends AuthenticatedRoutesTestCase
      */
     public function testGet()
     {
-        $response = $this->get('/api/products');
+        $response = $this->get('/api/products?' . 'include=inventory,aliases,tags');
 
         $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'current_page',
+            'data' => [
+                '*' => [
+                    'id',
+                    'sku',
+                    'name',
+                    'price',
+                    'sale_price',
+                    'sale_price_start_date',
+                    'sale_price_end_date',
+                    'quantity',
+                    'quantity_reserved',
+                    'quantity_available',
+                    'deleted_at',
+                    'created_at',
+                    'updated_at',
+                    'inventory_source_shelf_location',
+                    'inventory_source_quantity',
+                    'inventory_source_product_id',
+                    'inventory_source_location_id',
+                    'inventory' => [
+                        '*' => []
+                    ],
+                    'aliases' => [
+                        '*' => []
+                    ],
+                    'tags' => [
+                        '*' => []
+                    ],
+                ]
+            ],
+            'total',
+        ]);
     }
 }
