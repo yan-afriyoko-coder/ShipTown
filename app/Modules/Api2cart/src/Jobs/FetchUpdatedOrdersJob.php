46ce36d880bc1327dfad8c7779b558816a69758b
Artur Hanusek <arthur@youritsolutions.ie>
2020-12-03 23:42:56 +0000

    fix

--- app/Modules/Api2cart/src/Jobs/FetchUpdatedOrdersJob.php
+++ app/Modules/Api2cart/src/Jobs/FetchUpdatedOrdersJob.php
@@ -70,7 +70,7 @@ class FetchUpdatedOrdersJob implements ShouldQueue
         );
 
         $connection->update([
-            'last_synced_modified_at' => $lastTimeStamp
+            'last_synced_modified_at' => $lastTimeStamp->addSecond()
         ]);
     }
 
