ce95cec350ae5ca0e59ccc4dbab47c21fd66c079
Artur Hanusek <arthur@youritsolutions.ie>
2020-12-09 13:15:09 +0000

    refactor api.php

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,9 @@ Route::middleware('auth:api')->get('/user/me', function (Request $request) {
 
 Route::middleware('auth:api')->group(function () {
 
+    Route::put('print/order/{order_number}/{view}', 'Api\PrintOrderController@store');
+
+    Route::apiResource('run/sync', 'Api\Run\SyncController')->only('index');
     Route::apiResource('run/hourly/jobs', 'Api\Run\HourlyJobsController')->only('index');
     Route::apiResource('run/daily/jobs', 'Api\Run\DailyJobsController')->only('index');
 
@@ -41,6 +44,7 @@ Route::middleware('auth:api')->group(function () {
 
     Route::apiResource('picklist', 'Api\PicklistController')->only(['index']);
     Route::apiResource('picklist/picks', 'Api\Picklist\PicklistPickController')->only(['store']);
+    Route::apiResource('picks', 'Api\PickController')->except('store');
 
     Route::apiResource('packlist/order', 'Api\PacklistOrderController')->only(['index']);
 
@@ -49,12 +53,8 @@ Route::middleware('auth:api')->group(function () {
     Route::apiResource('settings/widgets', 'Api\WidgetsController');
     Route::apiResource('settings/modules/rms_api/connections', "Api\Settings\Module\Rmsapi\RmsapiConnectionController");
     Route::apiResource('settings/modules/api2cart/connections', "Api\Settings\Module\Api2cart\Api2cartConnectionController");
+    Route::apiResource('settings/widgets', 'Api\Settings\WidgetsController');
 
-    Route::put('print/order/{order_number}/{view}', 'Api\PrintOrderController@store');
-
-    Route::apiResource('picks', 'Api\PickController')->except('store');
-
-    Route::resource('widgets', 'Api\WidgetsController');
     // Routes for users with the admin role only
     Route::group(['middleware' => ['role:admin']], function () {
         Route::post('invites', 'InvitesController@store');
@@ -63,18 +63,17 @@ Route::middleware('auth:api')->group(function () {
         Route::resource('configuration', 'Api\Settings\ConfigurationController');
     });
 
-
-    Route::get('sync', "Api\SyncController@index");
-    Route::get('run/maintenance', function () {
-        \App\Jobs\RunMaintenanceJobs::dispatch();
-        return 'Maintenance jobs dispatched';
-    });
-
     // to remove
+    Route::get('sync', "Api\Run\SyncController@index");
     Route::resource("rms_api_configuration", "Api\Settings\Module\Rmsapi\RmsapiConnectionController");
     Route::resource("api2cart_configuration", "Api\Settings\Module\Api2cart\Api2cartConnectionController");
     Route::apiResource('users/me', 'Api\Settings\User\UserMeController')->only(['index']);
     Route::apiResource('inventory', 'Api\Product\ProductInventoryController')->only(['index','store']);
     Route::get('products/{sku}/sync', 'Api\ProductsController@publish');
     Route::put('printers/use/{printerId}', 'Api\Settings\PrinterController@use');
+    Route::apiResource('widgets', 'Api\Settings\WidgetsController');
+    Route::get('run/maintenance', function () {
+        \App\Jobs\RunMaintenanceJobs::dispatch();
+        return 'Maintenance jobs dispatched';
+    });
 });
