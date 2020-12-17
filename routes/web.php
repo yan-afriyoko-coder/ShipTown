94a491288d5aa1c31d48583c2c269196e30c5349
Artur Hanusek <arthur@youritsolutions.ie>
2020-12-17 15:12:15 +0000

    rename packlist to packsheet

--- routes/web.php
+++ routes/web.php
@@ -28,23 +28,23 @@ Route::post('invites/{token}', 'Api\Admin\UserInvitesController@process');
 
 // Routes for authenticated users only
 Route::middleware('auth')->group(function () {
-    Route::redirect('/', 'dashboard');
-    Route::view('/dashboard', 'dashboard')->name('dashboard');
-    Route::view('/products', 'products')->name('products');
-    Route::view('/orders', 'orders')->name('orders');
-    Route::view('/picklist', 'picklist')->name('picklist');
-    Route::view('/packlist', 'packlist')->name('packlist');
-    Route::view('/reports/picks', 'reports/picks_report')->name('picks_report');
-    Route::view('/settings', 'settings')->name('settings');
+    Route::redirect('', 'dashboard');
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('products', 'products')->name('products');
+    Route::view('picklist', 'picklist')->name('picklist');
+    Route::view('orders', 'orders')->name('orders');
+    Route::view('order/packsheet', 'packsheet')->name('order.packsheet');
+    Route::view('reports/picks', 'reports/picks_report')->name('picks_report');
+    Route::view('settings', 'settings')->name('settings');
 
-    Route::get('/pdf/orders/{order_number}/{template}', 'PdfOrderController@show');
-    Route::get('/csv/ready_order_shipments', 'Csv\ReadyOrderShipmentController@index')->name('ready_order_shipments_as_csv');
-    Route::get('/csv/order_shipments', 'Csv\PartialOrderShipmentController@index')->name('partial_order_shipments_as_csv');
-    Route::get('/csv/products/picked', 'Csv\ProductsPickedInWarehouse@index')->name('warehouse_picks.csv');
-    Route::get('/csv/products/shipped', 'Csv\ProductsShippedFromWarehouseController@index')->name('warehouse_shipped.csv');
+    Route::get('pdf/orders/{order_number}/{template}', 'PdfOrderController@show');
+    Route::get('csv/ready_order_shipments', 'Csv\ReadyOrderShipmentController@index')->name('ready_order_shipments_as_csv');
+    Route::get('csv/order_shipments', 'Csv\PartialOrderShipmentController@index')->name('partial_order_shipments_as_csv');
+    Route::get('csv/products/picked', 'Csv\ProductsPickedInWarehouse@index')->name('warehouse_picks.csv');
+    Route::get('csv/products/shipped', 'Csv\ProductsShippedFromWarehouseController@index')->name('warehouse_shipped.csv');
 
     // Admin only routes
     Route::middleware('role:admin')->group(function () {
-        Route::view('/users', 'users')->name('users');
+        Route::view('users', 'users')->name('users');
     });
 });
