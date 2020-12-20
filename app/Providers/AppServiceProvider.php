7f1da23d43355d64595453d9690c356ca51be3ea
Artur Hanusek <arthur@youritsolutions.ie>
2020-12-20 21:34:33 +0000

    move module observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,25 +5,14 @@ namespace App\Providers;
 use App\Models\Inventory;
 use App\Models\Order;
 use App\Models\OrderProduct;
-use App\Models\Packlist;
-use App\Models\Pick;
-use App\Models\Picklist;
-use App\Models\PickRequest;
 use App\Models\Product;
 use App\Modules\Api2cart\src\Models\Api2cartOrderImports;
 use App\Observers\InventoryObserver;
-use App\Observers\Modules\Api2cart\Api2cartOrderImportsObserver;
+use App\Modules\Api2cart\src\Observers\Api2cartOrderImportsObserver;
 use App\Observers\OrderObserver;
 use App\Observers\OrderProductObserver;
-use App\Observers\PacklistsObserver;
-use App\Observers\PicklistsObserver;
-use App\Observers\PickObserver;
-use App\Observers\PickRequestObserver;
 use App\Observers\ProductObserver;
-use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Queue\Events\JobProcessed;
-use Illuminate\Queue\Events\JobProcessing;
 
 class AppServiceProvider extends ServiceProvider
 {
