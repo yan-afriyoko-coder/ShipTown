19218486cd4818d21b3a130b0ac8b3e0253bbd8c
Artur Hanusek <arthur@youritsolutions.ie>
2020-12-20 23:47:33 +0000

    fix duplicated code

--- app/Listeners/Inventory/InventoryUpdatedEventListener.php
+++ app/Listeners/Inventory/InventoryUpdatedEventListener.php
@@ -24,55 +24,7 @@ class InventoryUpdatedEventListener
      */
     public function handle(InventoryUpdatedEvent $event)
     {
-        $this->updateProductTotalQuantity($event);
-        $this->updateProductQuantityReserved($event);
         $this->updateProductInventoryTotals($event);
-    }
-
-    /**
-     * Handle the event.
-     *
-     * @param InventoryUpdatedEvent $event
-     * @return void
-     */
-    public function updateProductTotalQuantity(InventoryUpdatedEvent $event)
-    {
-        $event->getInventory()
-            ->product()
-            ->decrement(
-                'quantity',
-                $event->getInventory()->getOriginal('quantity')
-            );
-
-        $event->getInventory()
-            ->product()
-            ->increment(
-                'quantity',
-                $event->getInventory()->quantity
-            );
-    }
-
-    /**
-     * Handle the event.
-     *
-     * @param InventoryUpdatedEvent $event
-     * @return void
-     */
-    public function updateProductQuantityReserved(InventoryUpdatedEvent $event)
-    {
-        $event->getInventory()
-            ->product()
-            ->decrement(
-                'quantity_reserved',
-                $event->getInventory()->getOriginal('quantity_reserved')
-            );
-
-        $event->getInventory()
-            ->product()
-            ->increment(
-                'quantity_reserved',
-                $event->getInventory()->quantity
-            );
     }
 
     /**
