aaccfd1bcc9794776c2bd9812e3c514d7e5b8d38
Artur Hanusek <arthur@youritsolutions.ie>
2020-12-20 22:17:57 +0000

    merge Inventory listeners

--- app/Listeners/Inventory/InventoryUpdatedEventListener.php
+++ app/Listeners/Inventory/InventoryUpdatedEventListener.php
@@ -2,8 +2,7 @@
 
 namespace App\Listeners\Inventory;
 
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Events\Inventory\InventoryUpdatedEvent;
 
 class InventoryUpdatedEventListener
 {
@@ -20,11 +19,58 @@ class InventoryUpdatedEventListener
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param InventoryUpdatedEvent $event
      * @return void
      */
-    public function handle($event)
+    public function handle(InventoryUpdatedEvent $event)
     {
-        //
+        $this->updateProductTotalQuantity($event);
+        $this->updateProductQuantityReserved($event);
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param InventoryUpdatedEvent $event
+     * @return void
+     */
+    public function updateProductTotalQuantity(InventoryUpdatedEvent $event)
+    {
+        $event->getInventory()
+            ->product()
+            ->decrement(
+                'quantity',
+                $event->getInventory()->getOriginal('quantity')
+            );
+
+        $event->getInventory()
+            ->product()
+            ->increment(
+                'quantity',
+                $event->getInventory()->quantity
+            );
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param InventoryUpdatedEvent $event
+     * @return void
+     */
+    public function updateProductQuantityReserved(InventoryUpdatedEvent $event)
+    {
+        $event->getInventory()
+            ->product()
+            ->decrement(
+                'quantity_reserved',
+                $event->getInventory()->getOriginal('quantity_reserved')
+            );
+
+        $event->getInventory()
+            ->product()
+            ->increment(
+                'quantity_reserved',
+                $event->getInventory()->quantity
+            );
     }
 }
