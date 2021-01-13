5e73868366c82a2ecec0d4dce80ee82cc7147269
Artur Hanusek <arthur@youritsolutions.ie>
2021-01-13 11:10:45 +0000

    24h kick

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::middleware('auth')->group(function () {
     Route::view('settings', 'settings')->name('settings');
 
     Route::get('pdf/orders/{order_number}/{template}', 'PdfOrderController@show');
+    Route::get('products/24h/kick', 'Products24hKickController@index');
     Route::get('products/{sku}/kick', 'ProductKickController@index');
     Route::get('csv/ready_order_shipments', 'Csv\ReadyOrderShipmentController@index')->name('ready_order_shipments_as_csv');
     Route::get('csv/order_shipments', 'Csv\PartialOrderShipmentController@index')->name('partial_order_shipments_as_csv');
