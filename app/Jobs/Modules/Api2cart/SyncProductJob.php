2808aa34338ffc48d83379322e6bf7e1207b6b09
Artur Hanusek <arthur@youritsolutions.ie>
2021-01-28 12:46:16 +0000

    reformat logs

--- app/Jobs/Modules/Api2cart/SyncProductJob.php
+++ app/Jobs/Modules/Api2cart/SyncProductJob.php
@@ -55,8 +55,8 @@ class SyncProductJob implements ShouldQueue
                 VerifyProductSyncJob::dispatch($connection->bridge_api_key, $product_data);
 
                 info('Api2cart: Product synced', [
-                    'product_data' => $product_data,
                     'response' => $response ? $response->asArray() : null,
+                    'product_data' => $product_data,
                 ]);
             } catch (Exception $exception) {
 
@@ -65,11 +65,11 @@ class SyncProductJob implements ShouldQueue
                 }, 100);
 
                 Log::warning('Api2cart: Product NOT SYNCED', [
-                    'product_data' => $product_data,
                     'response' => [
                         'code' => $exception->getCode(),
                         'message' => $exception->getMessage(),
                     ],
+                    'product_data' => $product_data,
                 ]);
             }
         });
