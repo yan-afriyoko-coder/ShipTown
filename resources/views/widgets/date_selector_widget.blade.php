75feb328898258db72f872f3e22cb6cf16eb8ead
Artur Hanusek <arthur@youritsolutions.ie>
2021-01-30 22:07:03 +0000

    add select filter box

--- resources/views/widgets/date_selector_widget.blade.php
+++ resources/views/widgets/date_selector_widget.blade.php
@@ -3,7 +3,15 @@
     </thead>
     <tbody>
     <tr>
-        <td scope="col" class="text-right small">from <b>{{ $config['starting_date'] }}</b> to <b>{{ $config['ending_date'] }}</b></td>
+        <td scope="col" class="text-right small">from <b>{{ $config['starting_date'] }}</b> to <b>{{ $config['ending_date'] }}</b>
+            <select id="selectbox" name="" onchange="javascript:location.href = this.value;">
+                <option value="" selected></option>
+                <option value="?between_dates=today,now">Today</option>
+                <option value="?between_dates=yesterday">Yesterday</option>
+                <option value="?between_dates=last monday,now">This week</option>
+                <option value="?between_dates=last week monday,last monday">Last Week</option>
+            </select>
+        </td>
     </tr>
     </tbody>
 </table>
