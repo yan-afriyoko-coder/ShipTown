231ebe7b6e5d80ed60f1154f6743846273bb19c9
Artur Hanusek <arthur@youritsolutions.ie>
2021-01-30 23:12:04 +0000

    fix wording

--- resources/views/widgets/products_picked_counts_widget.blade.php
+++ resources/views/widgets/products_picked_counts_widget.blade.php
@@ -1,7 +1,7 @@
 <table class="table table-borderless">
     <thead>
     <tr>
-        <th scope="col">Products - Picked Today</th>
+        <th scope="col">Products Picked</th>
         <th scope="col" class="text-right">{{ $total_count }}</th>
     </tr>
     </thead>
