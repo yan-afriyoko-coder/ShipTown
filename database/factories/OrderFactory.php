2fcf1c3eefcd8ac657bb30886f49048f869dd89e
Artur Hanusek <arthur@youritsolutions.ie>
2021-01-30 14:16:35 +0000

    fix order factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -1,28 +1,33 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 
 use App\Models\Order;
 use App\Models\OrderAddress;
 use App\Models\OrderStatus;
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
 $factory->define(Order::class, function (Faker $faker) {
-    $order = [
+    $shippingAddress = factory(OrderAddress::class)->create();
+
+    $newOrder = [
         'order_number' => (string)(10000000 + $faker->unique()->randomNumber(7)),
-        'shipping_address_id' => factory(OrderAddress::class)->create()->id,
+        'shipping_address_id' => $shippingAddress->getKey(),
         'order_placed_at' => $faker->dateTimeBetween('-1 months', now()),
         'status_code' => $faker->randomElement([
             'complete','processing','cancelled','on_hold','paid','picking','packing'
         ])
     ];
 
-    if (OrderStatus::isComplete($order['status_code'])) {
-        $order['order_closed_at'] = $faker->dateTimeBetween($order['order_placed_at'], now());
-        $order['packer_user_id'] = User::query()->inRandomOrder()->first('id')->getKey();
-        $order['packed_at'] = $order['order_closed_at'];
+    if (OrderStatus::isComplete($newOrder['status_code'])) {
+        $user = User::query()->inRandomOrder()->first('id') ?? factory(User::class)->create();
+
+        $newOrder['order_closed_at'] = $faker->dateTimeBetween($newOrder['order_placed_at'], now());
+        $newOrder['packer_user_id'] = $user->getKey();
+        $newOrder['packed_at'] = $newOrder['order_closed_at'];
     }
 
-    return $order;
+    return $newOrder;
 });
