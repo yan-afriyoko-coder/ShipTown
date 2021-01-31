e7704ec71a2d89d405be0e878a9c8798940109b3
Artur Hanusek <arthur@youritsolutions.ie>
2021-01-31 17:57:28 +0000

    refactor factory

--- database/factories/OrderShipmentFactory.php
+++ database/factories/OrderShipmentFactory.php
@@ -1,15 +1,22 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+/** @var Factory $factory */
 
 use App\Models\Order;
 use App\Models\OrderShipment;
+use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
 $factory->define(OrderShipment::class, function (Faker $faker) {
+    $shipping_number = $faker->toUpper( $faker->randomLetter . $faker->randomLetter . '100' . $faker->randomNumber(8));
+
+    $order = Order::query()->inRandomOrder()->first() ?? factory(Order::class)->create();
+    $user = User::query()->inRandomOrder()->first() ?? factory(User::class)->create();
+
     return [
-        'order_id' => Order::query()->inRandomOrder()->first()->getKey(),
-        'shipping_number' => $faker->toUpper($faker->randomLetter . $faker->randomLetter . '100' .$faker->randomNumber(8)),
-        'user_id' => \App\User::query()->inRandomOrder()->first()->getKey()
+        'order_id' => $order->getKey(),
+        'shipping_number' => $shipping_number,
+        'user_id' => $user->getKey()
     ];
 });
