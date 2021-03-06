54f799ec58dcb5ca803bd4f8f662b149650743e6
Artur Hanusek <artur@arturhanusek.com>
2021-03-06 23:44:31 +0100

    test incorrect postcode@

--- tests/External/DpdIreland/NormalOvernightTest.php
+++ tests/External/DpdIreland/NormalOvernightTest.php
@@ -83,7 +83,6 @@ class NormalOvernightTest extends TestCase
         $this->assertEquals($consignment->toArray()['RecordID'], $preAdvice->getAttribute('RecordID'));
     }
 
-
     /**
      * @test
      */
@@ -100,6 +99,44 @@ class NormalOvernightTest extends TestCase
                 'AddressLine2' => 'Unit 2B Midland Gateway Bus',
                 'AddressLine3' => 'Kilbeggan',
                 'AddressLine4' => 'Westmeath',
+                'CountryCode' =>  'IE',
+            ],
+            'CollectionAddress' => [
+                'Contact' => 'John Smith',
+                'ContactTelephone' => '12345678901',
+                'ContactEmail' => 'john.smith@ie.ie',
+                'BusinessName' => 'JS Business',
+                'AddressLine1' => 'DPD Ireland, Westmeath',
+                'AddressLine2' => 'Unit 2B Midland Gateway Bus',
+                'AddressLine3' => 'Kilbeggan',
+                'AddressLine4' => 'Westmeath',
+                'PostCode'     => 'XYZ1234',
+                'CountryCode' =>  'IE',
+            ],
+        ]);
+
+        $preAdvice = Dpd::getPreAdvice($consignment);
+
+        $this->assertEquals($consignment->toArray()['RecordID'], $preAdvice->getAttribute('RecordID'));
+    }
+
+    /**
+     * @test
+     */
+    public function if_succeeds_with_wrong_postcode()
+    {
+        $consignment = new Consignment([
+            'TotalParcels' => rand(11,20),
+            'DeliveryAddress' => [
+                'Contact' => 'John Smith',
+                'ContactTelephone' => '12345678901',
+                'ContactEmail' => 'john.smith@ie.ie',
+                'BusinessName' => 'JS Business',
+                'AddressLine1' => 'DPD Ireland, Westmeath',
+                'AddressLine2' => 'Unit 2B Midland Gateway Bus',
+                'AddressLine3' => 'Kilbeggan',
+                'AddressLine4' => 'Westmeath',
+                'PostCode'     => 'XYZ12345678901234',
                 'CountryCode' =>  'IE',
             ],
             'CollectionAddress' => [
