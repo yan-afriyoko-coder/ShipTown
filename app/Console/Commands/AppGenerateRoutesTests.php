33f8811e96e98072c9ee0404dc17b09c500f410b
Artur Hanusek <artur@arturhanusek.com>
2021-04-12 23:05:01 +0200

    add comment with sample

---
+++ app/Console/Commands/AppGenerateRoutesTests.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
+
+class AppGenerateRoutesTests extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-api-routes-tests';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generates incomplete tests for all API routes not covered by tests';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        Artisan::call('route:list --json --path=api --env=production');
+
+        $routes = collect(json_decode(Artisan::output()));
+
+        $routes->each(function ($route) {
+            $testName = $this->getTestName($route);
+
+            $this->generateTest($testName);
+        });
+
+        return 0;
+    }
+
+    /**
+     * @param $testName
+     */
+    public function generateTest($testName): void
+    {
+        Artisan::call('generate:test ' . $testName . ' --stub=test_controller --force');
+    }
+
+    /**
+     * @param $route
+     * @return array|string|string[]
+     */
+    public function getTestName($route)
+    {
+        // $sample_action = 'App\\Http\\Controllers\\Api\\Settings\\UserMeController@index'
+        $controllerName = Str::before($route->action, '@');
+        $methodName = Str::after($route->action, '@');
+
+        $testDirectory = Str::after($controllerName, 'App\\') ;
+        $testName = $testDirectory . '\\' . Str::ucfirst($methodName) . 'Test';
+
+        return str_replace('\\', '/', $testName);
+    }
+}
