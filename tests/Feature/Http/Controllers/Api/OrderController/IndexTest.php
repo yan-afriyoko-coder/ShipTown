59b2cc48c695323b8ac0fbe02f91df1a9df4aaec
Artur Hanusek <artur@arturhanusek.com>
2021-04-12 23:59:27 +0200

    refactor api order tests

--- tests/Feature/Http/Controllers/Api/OrderController/IndexTest.php
+++ tests/Feature/Http/Controllers/Api/OrderController/IndexTest.php
@@ -2,15 +2,82 @@
 
 namespace Tests\Feature\Http\Controllers\Api\OrderController;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Order;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Passport\Passport;
+use Spatie\Tags\Tag;
+use Tests\TestCase;
 
 class IndexTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /**
+     * @return void
+     */
+    public function test_has_tags_filter_exists()
+    {
+        Passport::actingAs(
+            factory(User::class)->create()
+        );
+
+        Order::query()->forceDelete();
+        Tag::query()->forceDelete();
+
+        $order = factory(Order::class)->create();
+
+        $order->attachTag('Test');
+
+        $response = $this->get('api/orders?filter[has_tags]=Test');
+
+        $this->assertEquals(1, $response->json()['meta']['total']);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_has_tags_filter_missing()
+    {
+        Passport::actingAs(
+            factory(User::class)->create()
+        );
+
+        Order::query()->forceDelete();
+        Tag::query()->forceDelete();
+
+        $order = factory(Order::class)->create();
+
+        $response = $this->get('api/orders?filter[has_tags]=Test');
+
+        $this->assertEquals(0, $response->json()['meta']['total']);
+    }
+
     /** @test */
     public function test_index_call_returns_ok()
     {
-        $this->markTestIncomplete();
+        Order::query()->forceDelete();
+        factory(Order::class)->create();
+
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user, 'api')->getJson(route('orders.index'));
+
+        $response->assertOk();
+
+        $this->assertNotEquals(0, $response->json('meta.total'));
+
+        $response->assertJsonStructure([
+            'meta',
+            'links',
+            'data' => [
+                '*' => [
+                    'id',
+                    'status_code',
+                ]
+            ]
+        ]);
     }
 }
--- tests/Feature/Http/Controllers/Api/OrderController/UpdateTest.php
+++ tests/Feature/Http/Controllers/Api/OrderController/UpdateTest.php
@@ -2,15 +2,31 @@
 
 namespace Tests\Feature\Http\Controllers\Api\OrderController;
 
+use App\Models\Order;
+use App\User;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UpdateTest extends TestCase
 {
     /** @test */
     public function test_update_call_returns_ok()
     {
-        $this->markTestIncomplete();
+        $order = factory(Order::class)->create();
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user, 'api')->putJson(route('orders.update', [$order]), [
+            'status_code' => "test_status_code",
+            'packer_user_id' => $user->getKey(),
+        ]);
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+//            'meta',
+//            'links',
+            'data' => [
+                '*' => [
+                ]
+            ]
+        ]);
     }
 }
