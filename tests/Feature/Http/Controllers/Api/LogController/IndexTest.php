19bbdf44bee2be576e6eee74b383b076f1f30775
Artur Hanusek <artur@arturhanusek.com>
2021-04-13 00:00:28 +0200

    refactor api log controller tests

--- tests/Feature/Http/Controllers/Api/LogController/IndexTest.php
+++ tests/Feature/Http/Controllers/Api/LogController/IndexTest.php
@@ -2,15 +2,47 @@
 
 namespace Tests\Feature\Http\Controllers\Api\LogController;
 
+use App\Models\Product;
+use App\User;
+use Spatie\Activitylog\Models\Activity;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class IndexTest extends TestCase
 {
     /** @test */
     public function test_index_call_returns_ok()
     {
-        $this->markTestIncomplete();
+        Activity::query()->forceDelete();
+
+        $user = factory(User::class)->create();
+
+        // product creation will generate some logs
+        factory(Product::class)->create();
+
+        $response = $this->actingAs($user, 'api')->getJson(route('logs.index'));
+
+        $response->assertOk();
+
+        $this->assertNotEquals(0, $response->json('meta.total'));
+
+        $response->assertJsonStructure([
+            'meta',
+            'links',
+            'data' => [
+                '*' => [
+                    'created_at',
+                    'id',
+                    'description',
+                    'subject_id',
+                    'subject_type',
+                    'causer_id',
+                    'causer_type',
+                    'properties' => [
+                        '*' => []
+                    ],
+                    'changes' => []
+                ]
+            ]
+        ]);
     }
 }
