223cfdfca4bb35d7eb9ac84aff533c30af3d08d1
Artur Hanusek <artur@arturhanusek.com>
2021-04-12 23:45:52 +0200

    refactor admin tests

--- tests/Feature/Http/Controllers/Api/Admin/UserRoleController/IndexTest.php
+++ tests/Feature/Http/Controllers/Api/Admin/UserRoleController/IndexTest.php
@@ -2,15 +2,28 @@
 
 namespace Tests\Feature\Http\Controllers\Api\Admin\UserRoleController;
 
+use App\User;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class IndexTest extends TestCase
 {
     /** @test */
     public function test_index_call_returns_ok()
     {
-        $this->markTestIncomplete();
+        $user = factory(User::class)->create()->assignRole('admin');
+
+        $response = $this->actingAs($user, 'api')->getJson(route('admin.users.roles.index'));
+
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'name',
+                    'guard_name'
+                ]
+            ]
+        ]);
     }
 }
