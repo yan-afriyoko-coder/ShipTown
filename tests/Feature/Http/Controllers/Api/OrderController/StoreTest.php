d283fe46b602795e024bad5c12a25dfd08c6965d
Artur Hanusek <artur@arturhanusek.com>
2021-04-16 17:48:09 +0200

    refactor test

--- tests/Feature/Http/Controllers/Api/OrderController/StoreTest.php
+++ tests/Feature/Http/Controllers/Api/OrderController/StoreTest.php
@@ -2,15 +2,46 @@
 
 namespace Tests\Feature\Http\Controllers\Api\OrderController;
 
+use App\User;
+use Illuminate\Support\Facades\Event;
+use Laravel\Passport\Passport;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class StoreTest extends TestCase
 {
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $admin = factory(User::class)->create()->assignRole('admin');
+        $this->actingAs($admin, 'api');
+    }
+
     /** @test */
     public function test_store_call_returns_ok()
     {
-        $this->markTestIncomplete();
+        $data = [
+            'order_number'      => '0123456789',
+            'products' => [
+                [
+                    'sku' => '123',
+                    'quantity'     => 2,
+                    'price'        => 4,
+                ],
+
+                [
+                    'sku' => '456',
+                    'quantity'     => 5,
+                    'price'        => 10,
+                ],
+            ],
+        ];
+
+        $this->postJson('api/orders', $data)->assertStatus(200);
+
+        $this->assertDatabaseHas('orders', ['order_number' => $data['order_number']]);
     }
 }
