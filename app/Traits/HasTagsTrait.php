9bd089ef77c9c06a2e748f77ec456268831d6711
Artur Hanusek <artur@arturhanusek.com>
2021-05-07 10:14:19 +0200

    verify if tag attached before detaching

--- app/Traits/HasTagsTrait.php
+++ app/Traits/HasTagsTrait.php
@@ -2,11 +2,9 @@
 
 namespace App\Traits;
 
-use App\Events\Product\TagAttachedEvent;
 use ArrayAccess;
 use Exception;
 use Illuminate\Database\Eloquent\Builder;
-use phpDocumentor\Reflection\Types\Mixed_;
 use Spatie\Tags\HasTags;
 use Spatie\Tags\Tag;
 
@@ -55,9 +53,10 @@ trait HasTagsTrait
     public function detachTag($tag, string $type = null)
     {
         try {
-            $this->traitHasTagsDetachTag($tag, $type);
-//            $this->onTagAttached($tag);
-            $this->log('"'.$tag.'" tag detached');
+            if ($this->hasTags([$tag])) {
+                $this->traitHasTagsDetachTag($tag, $type);
+                $this->log('"'.$tag.'" tag detached');
+            }
         } catch (Exception $exception) {
             $this->log("Could not detach '{$tag}'' tag");
         }
