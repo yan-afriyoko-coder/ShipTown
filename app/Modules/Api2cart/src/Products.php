1d93793f9e8870e05292ec028a0c66d5b8deb04d
Artur Hanusek <artur@arturhanusek.com>
2021-05-18 11:18:26 +0200

    fix warehouse inventory retrieval

--- app/Modules/Api2cart/src/Products.php
+++ app/Modules/Api2cart/src/Products.php
@@ -484,10 +484,19 @@ class Products extends Entity
             return $product["quantity"];
         }
 
+        $inventories = collect($product['inventory'])
+            ->filter(function ($warehouse_inventory) use ($warehouse_id) {
+                return $warehouse_inventory['warehouse_id'] === $warehouse_id;
+            });
+
+        if ($inventories->isEmpty()) {
+            return $product["quantity"];
+        }
+
         if (! key_exists($warehouse_id, $product['inventory'])) {
             return 0;
         }
 
-        return $product["inventory"][$warehouse_id]['quantity'];
+        return $inventories->first()['quantity'];
     }
 }
