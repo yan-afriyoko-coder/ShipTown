64e2aeabb71f2d9f022f3fb137402f7bf8f462c3
Artur Hanusek <artur@arturhanusek.com>
2021-05-26 21:32:13 +0200

    refactor job and install queue monitor

--- app/Modules/Maintenance/src/Jobs/Products/FixQuantityAvailableJob.php
+++ app/Modules/Maintenance/src/Jobs/Products/FixQuantityAvailableJob.php
@@ -10,10 +10,11 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use romanzipp\QueueMonitor\Traits\IsMonitored;
 
 class FixQuantityAvailableJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, IsMonitored;
 
     /**
      * Execute the job.
