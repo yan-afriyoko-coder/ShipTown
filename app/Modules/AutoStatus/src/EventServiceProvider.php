78cc6e3018295cb942711675bedb1febe52171b0
Artur Hanusek <artur@arturhanusek.com>
2021-05-30 20:06:19 +0200

    fix

--- app/Modules/AutoStatus/src/EventServiceProvider.php
+++ app/Modules/AutoStatus/src/EventServiceProvider.php
@@ -16,7 +16,7 @@ class EventServiceProvider extends ServiceProvider
         HourlyEvent::class => [
             Listeners\HourlyEvent\RefillStatusPackingWarehouseListener::class,
             Listeners\HourlyEvent\RefillStatusSingleLineOrdersListener::class,
-            Listeners\HourlyEvent\Re::class,
+            Listeners\HourlyEvent\RefillStatusPickingListener::class,
         ],
 
         OrderUpdatedEvent::class => [
