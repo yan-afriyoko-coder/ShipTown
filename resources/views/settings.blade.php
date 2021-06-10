0687b74f34b24e98e6e22eb735bca1ac7ff8a38c
Artur Hanusek <artur@arturhanusek.com>
2021-06-10 10:27:24 +0200

    rearrange settings page

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -6,24 +6,53 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-                    <printer-configuration></printer-configuration>
-                    <user-courier-integration-select></user-courier-integration-select>
-                    @role('admin')
-                        <maintenance-section></maintenance-section>
-                        <auto-pilot-tuning-section></auto-pilot-tuning-section>
-                        <passport-clients></passport-clients>
-                        <passport-authorized-clients></passport-authorized-clients>
-                        <passport-personal-access-tokens></passport-personal-access-tokens>
-                        <api2cart-configuration></api2cart-configuration>
-                        <rmsapi-configuration></rmsapi-configuration>
-                        <printnode-configuration></printnode-configuration>
-                    @endrole
+            <div class="card">
+                <a href="{{ route('settings.general') }}">
+                    <div class="title">General</div>
+                    <div class="swiper-button-black">View and update general application settings</div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="{{ route('settings.printnode') }}">
+                    <div class="title">PrintNode</div>
+                    <div class="swiper-button-black">View and update PrintNode integration  settings</div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="{{ route('settings.rmsapi') }}">
+                    <div class="title">Microsoft Dynamic RMS 2.0 API</div>
+                    <div class="swiper-button-black">View and update RMS API integration settings</div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="{{ route('settings.dpd-ireland') }}">
+                    <div class="title">DPD Ireland API</div>
+                    <div class="swiper-button-black">View and update DPD Ireland integration settings</div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="{{ route('settings.api2cart') }}">
+                    <div class="title">Api2cart</div>
+                    <div class="swiper-button-black">View and update Api2cart integration settings</div>
+                </a>
+            </div>
+
+            <div class="card">
+                <a href="{{ route('settings.api') }}">
+                    <div class="title">API</div>
+                    <div class="swiper-button-black">View and update application API settings and tokens</div>
+                </a>
+            </div>
     </div>
 </div>
 @endsection
