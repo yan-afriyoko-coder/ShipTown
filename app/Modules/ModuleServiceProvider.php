828607dc1bdeb18761bb0c90896b8fb3574a251d
Artur Hanusek <artur@arturhanusek.com>
2021-06-24 16:45:05 +0200

    add autoEnabled feature to modules service providor

--- app/Modules/ModuleServiceProvider.php
+++ app/Modules/ModuleServiceProvider.php
@@ -6,10 +6,23 @@ namespace App\Modules;
 use App;
 use App\Module;
 use Exception;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider;
 
+/**
+ * Class ModuleServiceProvider
+ * @package App\Modules
+ */
 class ModuleServiceProvider extends EventServiceProvider
 {
+    /**
+     * Should we automatically enable it
+     * When module first registered
+     *
+     * @var bool
+     */
+    public bool $autoEnable = true;
+
     /**
      * Register any events for your application.
      *
@@ -28,19 +41,29 @@ class ModuleServiceProvider extends EventServiceProvider
     public function isEnabled(): bool
     {
         try {
-            $module = Module::firstOrCreate(['service_provider_class' => get_called_class()], ['enabled' => true]);
+            $module = Module::firstOrCreate([
+                    'service_provider_class' => get_called_class()
+                ], [
+                    'enabled' => $this->autoEnable
+                ]);
             return $module->enabled;
         } catch (Exception $exception) {
             return false;
         }
     }
 
+    /**
+     *
+     */
     public static function enableModule()
     {
         Module::updateOrCreate(['service_provider_class' => get_called_class()], ['enabled' => true]);
         App::register(get_called_class());
     }
 
+    /**
+     * @return Module|Model
+     */
     public static function disableModule()
     {
         return Module::updateOrCreate(['service_provider_class' => get_called_class()], ['enabled' => false]);
