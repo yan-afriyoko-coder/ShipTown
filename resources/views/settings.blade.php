64c8cc468fdec59c2932f9d1d44f72170de5e6ea
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-06-24 21:46:13 +0700

    add setting style

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -13,115 +13,141 @@
             @endif
 
             <div class="list-group">
-                <a href="{{ route('settings.general') }}" class="list-group-item list-group-item-action">
-                    <div class="media">
-                        <div class="p-3 rounded mr-3 bg-light">
-                            <font-awesome-icon icon="cog" class="fa-lg align-self-center"></font-awesome-icon>
-                        </div>
-                        <div class="media-body">
-                            <div class="h5 text-primary font-weight-bolder">General</div>
-                            <div>View and update general application settings</div>
-                        </div>
+                <a href="{{ route('settings.general') }}" class="setting-list">
+                    <div class="setting-icon">
+                        <font-awesome-icon icon="cog" class="fa-lg"></font-awesome-icon>
+                    </div>
+                    <div class="setting-body">
+                        <div class="setting-title">General</div>
+                        <div>View and setting-desc application settings</div>
                     </div>
                 </a>
 
-                <a href="{{ route('users') }}" class="list-group-item list-group-item-action">
-                    <div class="media">
-                        <div class="p-3 rounded mr-3 bg-light">
-                            <font-awesome-icon icon="user-edit" class="fa-lg align-self-center"></font-awesome-icon>
-                        </div>
-                        <div class="media-body">
-                            <div class="h5 text-primary font-weight-bolder">Users</div>
-                            <div class="text-secondary">Manage Users</div>
-                        </div>
+                <a href="{{ route('users') }}" class="setting-list">
+                    <div class="setting-icon">
+                        <font-awesome-icon icon="user-edit" class="fa-lg"></font-awesome-icon>
+                    </div>
+                    <div class="setting-body">
+                        <div class="setting-title">Users</div>
+                        <div class="setting-desc">Manage Users</div>
                     </div>
                 </a>
 
-                <a href="{{ route('settings.printnode') }}" class="list-group-item list-group-item-action">
-                    <div class="media">
-                        <div class="p-3 rounded mr-3 bg-light">
-                            <font-awesome-icon icon="print" class="fa-lg align-self-center"></font-awesome-icon>
-                        </div>
-                        <div class="media-body">
-                            <div class="h5 text-primary font-weight-bolder">PrintNode</div>
-                            <div class="text-secondary">View and update PrintNode integration  settings</div>
-                        </div>
+                <a href="{{ route('settings.printnode') }}" class="setting-list">
+                    <div class="setting-icon">
+                        <font-awesome-icon icon="print" class="fa-lg"></font-awesome-icon>
+                    </div>
+                    <div class="setting-body">
+                        <div class="setting-title">PrintNode</div>
+                        <div class="setting-desc">View and update PrintNode integration  settings</div>
                     </div>
                 </a>
 
-                <a href="{{ route('settings.rmsapi') }}" class="list-group-item list-group-item-action">
-                    <div class="media">
-                        <div class="p-3 rounded mr-3 bg-light">
-                            <font-awesome-icon icon="code" class="fa-lg align-self-center"></font-awesome-icon>
-                        </div>
-                        <div class="media-body">
-                            <div class="h5 text-primary font-weight-bolder">Microsoft Dynamic RMS 2.0 API</div>
-                            <div class="text-secondary">View and update RMS API integration settings</div>
-                        </div>
+                <a href="{{ route('settings.rmsapi') }}" class="setting-list">
+                    <div class="setting-icon">
+                        <font-awesome-icon icon="code" class="fa-lg"></font-awesome-icon>
+                    </div>
+                    <div class="setting-body">
+                        <div class="setting-title">Microsoft Dynamic RMS 2.0 API</div>
+                        <div class="setting-desc">View and update RMS API integration settings</div>
                     </div>
                 </a>
 
-                <a href="{{ route('settings.dpd-ireland') }}" class="list-group-item list-group-item-action">
-                    <div class="media">
-                        <div class="p-3 rounded mr-3 bg-light">
-                            <font-awesome-icon icon="truck" class="fa-lg align-self-center"></font-awesome-icon>
-                        </div>
-                        <div class="media-body">
-                            <div class="h5 text-primary font-weight-bolder">DPD Ireland API</div>
-                            <div class="text-secondary">View and update DPD Ireland integration settings</div>
-                        </div>
+                <a href="{{ route('settings.dpd-ireland') }}" class="setting-list">
+                    <div class="setting-icon">
+                        <font-awesome-icon icon="truck" class="fa-lg"></font-awesome-icon>
+                    </div>
+                    <div class="setting-body">
+                        <div class="setting-title">DPD Ireland API</div>
+                        <div class="setting-desc">View and update DPD Ireland integration settings</div>
                     </div>
                 </a>
 
-                <a href="{{ route('settings.api2cart') }}" class="list-group-item list-group-item-action">
-                    <div class="media">
-                        <div class="p-3 rounded mr-3 bg-light">
-                            <font-awesome-icon icon="shopping-cart" class="fa-lg align-self-center"></font-awesome-icon>
-                        </div>
-                        <div class="media-body">
-                            <div class="h5 text-primary font-weight-bolder">Api2cart</div>
-                            <div class="text-secondary">View and update Api2cart integration settings</div>
-                        </div>
+                <a href="{{ route('settings.api2cart') }}" class="setting-list">
+                    <div class="setting-icon">
+                        <font-awesome-icon icon="shopping-cart" class="fa-lg"></font-awesome-icon>
+                    </div>
+                    <div class="setting-body">
+                        <div class="setting-title">Api2cart</div>
+                        <div class="setting-desc">View and update Api2cart integration settings</div>
                     </div>
                 </a>
 
-                <a href="{{ route('settings.api') }}" class="list-group-item list-group-item-action">
-                    <div class="media">
-                        <div class="p-3 rounded mr-3 bg-light">
-                            <font-awesome-icon icon="key" class="fa-lg align-self-center"></font-awesome-icon>
-                        </div>
-                        <div class="media-body">
-                            <div class="h5 text-primary font-weight-bolder">API</div>
-                            <div class="text-secondary">View and update application API settings and tokens</div>
-                        </div>
+                <a href="{{ route('settings.api') }}" class="setting-list">
+                    <div class="setting-icon">
+                        <font-awesome-icon icon="key" class="fa-lg"></font-awesome-icon>
+                    </div>
+                    <div class="setting-body">
+                        <div class="setting-title">API</div>
+                        <div class="setting-desc">View and update application API settings and tokens</div>
                     </div>
                 </a>
 
-                <a href="{{ url('admin/tools/queue-monitor') }}" target="_blank" class="list-group-item list-group-item-action">
-                    <div class="media">
-                        <div class="p-3 rounded mr-3 bg-light">
-                            <font-awesome-icon icon="desktop" class="fa-lg align-self-center"></font-awesome-icon>
-                        </div>
-                        <div class="media-body">
-                            <div class="h5 text-primary font-weight-bolder">Queue Monitor</div>
-                            <div class="text-secondary">Open jobs monitor</div>
-                        </div>
+                <a href="{{ url('admin/tools/queue-monitor') }}" target="_blank" class="setting-list">
+                    <div class="setting-icon">
+                        <font-awesome-icon icon="desktop" class="fa-lg"></font-awesome-icon>
+                    </div>
+                    <div class="setting-body">
+                        <div class="setting-title">Queue Monitor</div>
+                        <div class="setting-desc">Open jobs monitor</div>
                     </div>
                 </a>
 
-                <a href="{{ url('admin/tools/log-viewer') }}" target="_blank" class="list-group-item list-group-item-action">
-                    <div class="media">
-                        <div class="p-3 rounded mr-3 bg-light">
-                            <font-awesome-icon icon="clipboard-list" class="fa-lg align-self-center"></font-awesome-icon>
-                        </div>
-                        <div class="media-body">
-                            <div class="h5 text-primary font-weight-bolder">Log Viewer</div>
-                            <div class="text-secondary">View application logs</div>
-                        </div>
+                <a href="{{ url('admin/tools/log-viewer') }}" target="_blank" class="setting-list">
+                    <div class="setting-icon">
+                        <font-awesome-icon icon="clipboard-list" class="fa-lg"></font-awesome-icon>
+                    </div>
+                    <div class="setting-body">
+                        <div class="setting-title">Log Viewer</div>
+                        <div class="setting-desc">View application logs</div>
                     </div>
                 </a>
             </div>
 
         </div>
+    </div>
 </div>
+@endsection
+
+@section('css')
+    <style>
+        .setting-list{
+            padding: 0.75rem 1.25rem;
+            background-color: #fff;
+            border: 1px solid rgba(0, 0, 0, 0.125);
+            width: 100%;
+            color: #495057;
+            display: flex;
+            align-items: flex-start;
+        }
+
+        .setting-list:hover, .setting-list:focus {
+            color: #495057;
+            text-decoration: none;
+            background-color: #f8f9fa;
+        }
+
+        .setting-icon{
+            padding: 1rem;
+            margin-right: 1rem;
+            background-color: #f8f9fa;
+            border-radius: 0.25rem;
+        }
+
+        .setting-icon:hover{
+            background-color: unset;
+        }
+
+        .setting-title{
+            color: #3490dc;
+            font-weight: bolder;
+            font-size: 1.125rem;
+            line-height: 1.2;
+            margin-bottom: 0.5rem;
+        }
+
+        .setting-desc{
+            color: #6c757d;
+        }
+    </style>
 @endsection
