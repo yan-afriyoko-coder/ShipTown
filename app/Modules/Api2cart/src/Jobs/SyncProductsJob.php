ae6909b0957536258a0eff116c2ce2f85d64faac
Artur Hanusek <artur@arturhanusek.com>
2021-07-06 14:40:51 +0200

    make sure it creates product link record if does not exists

--- app/Modules/Api2cart/src/Jobs/SyncProductsJob.php
+++ app/Modules/Api2cart/src/Jobs/SyncProductsJob.php
@@ -3,6 +3,7 @@
 namespace App\Modules\Api2cart\src\Jobs;
 
 use App\Models\Product;
+use App\Modules\Api2cart\src\Models\Api2cartConnection;
 use App\Modules\Api2cart\src\Models\Api2cartProductLink;
 use App\Modules\MagentoApi\src\Jobs\SyncProductStockJob;
 use Exception;
@@ -11,7 +12,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Collection;
 use romanzipp\QueueMonitor\Traits\IsMonitored;
 
 class SyncProductsJob implements ShouldQueue
@@ -31,29 +31,30 @@ class SyncProductsJob implements ShouldQueue
      */
     public function handle()
     {
+        $connections = Api2cartConnection::all();
+
         $query = Product::withAllTags(['Available Online', 'Not Synced']);
 
         $this->queueData([
             'total_count' => $query->count(),
         ]);
 
-        $totalCount = $query->count();
-
-        $chunkSize = 10;
-
         // we want to sync products with smallest quantities first to avoid oversells
-        $query->orderBy('quantity')
+        $products = $query->orderBy('quantity')
             ->orderBy('updated_at')
-            ->chunk($chunkSize, function (Collection $products) use ($totalCount, $chunkSize) {
-                $this->queueProgressChunk($totalCount, $chunkSize);
+            ->get();
 
-                $products->each(function (Product $product) {
-                    Api2cartProductLink::where(['product_id' => $product->getKey()])
+        $products->each(function (Product $product) use ($connections) {
+            $connections->each(function (Api2cartConnection $api2cartConnection) use ($product) {
+                    Api2cartProductLink::firstOrCreate([
+                            'product_id' => $product->getKey(),
+                            'api2cart_connection_id' => $api2cartConnection->getKey(),
+                        ], [])
                         ->each(function (Api2cartProductLink $product_link) {
                             SyncProduct::dispatch($product_link);
                             SyncProductStockJob::dispatch($product_link->product);
                         });
-                });
             });
+        });
     }
 }
