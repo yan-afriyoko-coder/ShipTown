8206727c4f1542083d87530dfce724f43c8e9ca4
Artur Hanusek <artur@arturhanusek.com>
2021-07-19 18:31:42 +0200

    move layaway status update to job

--- tests/Feature/Modules/AutoStatusLayaway/AutoStatusLayawayServiceProviderTest.php
+++ tests/Feature/Modules/AutoStatusLayaway/AutoStatusLayawayServiceProviderTest.php
@@ -21,12 +21,17 @@ class AutoStatusLayawayServiceProviderTest extends TestCase
     {
         AutoStatusLayawayServiceProvider::enableModule();
 
-        $order = factory(Order::class)->create(['status_code' => 'paid', 'is_active' => true]);
+        /** @var Order $order */
+        $order = factory(Order::class)->make(['status_code' => 'paid', 'is_active' => true]);
+        $order->save();
+
+        //        $order->update(['status_code' => 'paid']);
 
         OrderUpdatedEvent::dispatch($order);
 
         /** @var Order $order */
         $order = $order->refresh();
+
 
         $this->assertEquals('layaway', $order->status_code);
     }
