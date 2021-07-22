937075a3908c1b3c2f4c770c1dd3c5be2b2d62f0
Artur Hanusek <artur@arturhanusek.com>
2021-07-22 18:59:41 +0200

    fix test

--- tests/External/Api2cart/StatusSyncTest.php
+++ tests/External/Api2cart/StatusSyncTest.php
@@ -40,15 +40,18 @@ class StatusSyncTest extends TestCase
 
         $orderStatusList = $response->getResult()['cart_order_statuses'];
 
-        $randomStatus = $orderStatusList[rand(0, count($orderStatusList)-1)];
-
-        \App\Models\OrderStatus::firstOrCreate([
-            'name' => $randomStatus['id'],
-            'code' => $randomStatus['id'],
-            'sync_ecommerce' => true,
-        ]);
-
         $order = Order::first();
+
+        do {
+            $randomStatus = $orderStatusList[rand(0, count($orderStatusList)-1)];
+        } while ($order->status_code === $randomStatus['id']);
+
+        \App\Models\OrderStatus::updateOrCreate([
+            'code' => $randomStatus['id']
+        ], [
+            'name' => $randomStatus['id'],
+            'sync_ecommerce' => false,
+        ]);
 
         $order->status_code = $randomStatus['id'];
         $order->save();
