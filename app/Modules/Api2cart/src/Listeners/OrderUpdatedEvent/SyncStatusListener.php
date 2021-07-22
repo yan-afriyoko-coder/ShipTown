29f4a6a05266d83556e05a249014a98bc7e1442d
Artur Hanusek <artur@arturhanusek.com>
2021-07-22 17:52:50 +0200

    move order sync to job

--- app/Modules/Api2cart/src/Listeners/OrderUpdatedEvent/SyncStatusListener.php
+++ app/Modules/Api2cart/src/Listeners/OrderUpdatedEvent/SyncStatusListener.php
@@ -6,6 +6,7 @@ namespace App\Modules\Api2cart\src\Listeners\OrderUpdatedEvent;
 use App\Events\Order\OrderUpdatedEvent;
 use App\Modules\Api2cart\src\Api\Orders;
 use App\Modules\Api2cart\src\Exceptions\RequestException;
+use App\Modules\Api2cart\src\Jobs\SyncOrderStatus;
 use App\Modules\Api2cart\src\Models\Api2cartConnection;
 use App\Modules\Api2cart\src\Models\Api2cartOrderImports;
 use GuzzleHttp\Exception\GuzzleException;
@@ -26,15 +27,6 @@ class SyncStatusListener
             return;
         }
 
-        $orderImport = Api2cartOrderImports::where(['order_number' => $event->order->order_number])
-            ->latest()
-            ->first();
-
-        $api2cartConnection = Api2cartConnection::find($orderImport->connection_id);
-
-        Orders::update($api2cartConnection->bridge_api_key, [
-            'order_id' => $orderImport->api2cart_order_id,
-            'order_status' => $event->order->status_code
-        ]);
+        SyncOrderStatus::dispatch($event->order);
     }
 }
