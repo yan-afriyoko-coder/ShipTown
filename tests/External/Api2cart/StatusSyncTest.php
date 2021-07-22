fcd4c6f6d8a2c2b0839e6df811968b39b0824636
Artur Hanusek <artur@arturhanusek.com>
2021-07-22 16:27:58 +0200

    add sync_ecommerce column to order_statuses

--- tests/External/Api2cart/StatusSyncTest.php
+++ tests/External/Api2cart/StatusSyncTest.php
@@ -42,6 +42,12 @@ class StatusSyncTest extends TestCase
 
         $randomStatus = $orderStatusList[rand(0, count($orderStatusList)-1)];
 
+        \App\Models\OrderStatus::firstOrCreate([
+            'name' => $randomStatus['id'],
+            'code' => $randomStatus['id'],
+            'sync_ecommerce' => true,
+        ]);
+
         $order = Order::first();
 
         $order->status_code = $randomStatus['id'];
@@ -50,5 +56,47 @@ class StatusSyncTest extends TestCase
         $response = Orders::list($api2cartConnection->bridge_api_key, ['ids' => $order->order_number]);
 
         $this->assertEquals($order->status_code, $response->getResult()['order'][0]['status']['id']);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     * @throws RequestException
+     * @throws GuzzleException
+     */
+    public function test_if_not_syncs_status_when_disabled()
+    {
+        // we set key to api2cart demo store
+        $api2cartConnection = new Api2cartConnection([
+            'location_id'    => '99',
+            'type'           => 'magento',
+            'url'            => 'https://demo.api2cart.com/opencart',
+            'bridge_api_key' => config('api2cart.api2cart_test_store_key'),
+        ]);
+        $api2cartConnection->save();
+
+        DispatchImportOrdersJobs::dispatchNow();
+
+        $response = OrderStatus::list($api2cartConnection->bridge_api_key);
+
+        $orderStatusList = $response->getResult()['cart_order_statuses'];
+
+        $randomStatus = $orderStatusList[rand(0, count($orderStatusList)-1)];
+
+        \App\Models\OrderStatus::firstOrCreate([
+            'name' => $randomStatus['id'],
+            'code' => $randomStatus['id'],
+            'sync_ecommerce' => false,
+        ]);
+
+        $order = Order::first();
+
+        $order->status_code = $randomStatus['id'];
+        $order->save();
+
+        $response = Orders::list($api2cartConnection->bridge_api_key, ['ids' => $order->order_number]);
+
+        $this->assertNotEquals($order->status_code, $response->getResult()['order'][0]['status']['id']);
     }
 }
