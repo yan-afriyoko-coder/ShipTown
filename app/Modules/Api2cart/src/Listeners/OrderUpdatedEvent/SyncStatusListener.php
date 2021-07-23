ced1a9da279a3f0c5dc8b9fff5797a3f18170ed4
Artur Hanusek <artur@arturhanusek.com>
2021-07-23 15:38:57 +0200

    refactoring

--- app/Modules/Api2cart/src/Listeners/OrderUpdatedEvent/SyncStatusListener.php
+++ app/Modules/Api2cart/src/Listeners/OrderUpdatedEvent/SyncStatusListener.php
@@ -4,18 +4,11 @@
 namespace App\Modules\Api2cart\src\Listeners\OrderUpdatedEvent;
 
 use App\Events\Order\OrderUpdatedEvent;
-use App\Modules\Api2cart\src\Api\Orders;
-use App\Modules\Api2cart\src\Exceptions\RequestException;
 use App\Modules\Api2cart\src\Jobs\SyncOrderStatus;
-use App\Modules\Api2cart\src\Models\Api2cartConnection;
-use App\Modules\Api2cart\src\Models\Api2cartOrderImports;
-use GuzzleHttp\Exception\GuzzleException;
 
 class SyncStatusListener
 {
     /**
-     * @throws RequestException
-     * @throws GuzzleException
      */
     public function handle(OrderUpdatedEvent $event)
     {
