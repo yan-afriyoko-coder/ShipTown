81a16edb75a79d34f7bcd1b8a24d461795c33bc0
Artur Hanusek <artur@arturhanusek.com>
2021-07-27 18:10:11 +0200

    disable scurri tests

--- app/Modules/ScurriAnpost/config/scurri.php
+++ app/Modules/ScurriAnpost/config/scurri.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'base_uri' => env('SCURRI_BASE_URI', 'https://sandbox.scurri.co.uk/api/v1/company/'),
+    'base_uri' => env('SCURRI_BASE_URI'),
     'company_slug' => env('SCURRI_COMPANY_SLUG'),
     'username' => env('SCURRI_USERNAME'),
     'password' => env('SCURRI_PASSWORD'),
