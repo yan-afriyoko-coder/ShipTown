21e644ce84f2d24eb4fad99be73a3ab193d9eaaf
Artur Hanusek <artur@arturhanusek.com>
2021-08-03 17:11:56 +0200

    use users location id if present

--- resources/js/components/PicklistPage.vue
+++ resources/js/components/PicklistPage.vue
@@ -73,19 +73,13 @@ export default {
         'picklist-configuration-modal': PicklistConfigurationModalNew   ,
     },
 
-    mounted() {
-        if(this.getUrlParameter('inventory_source_location_id') === null) {
-            this.setUrlParameter('inventory_source_location_id', 100);
-        }
-        this.reloadPicks();
-    },
-
     watch: {
         user: {
             handler() {
                 if(this.user['location_id']) {
                     this.setUrlParameter('inventory_source_location_id', this.user['location_id']);
                 }
+                this.reloadPicks();
             }
         },
 
