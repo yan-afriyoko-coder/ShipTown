807e88b26a060dff741bba23def7dc67bde13d5e
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-08-13 11:08:19 +0700

    PrintOrderController Store Test

--- tests/Feature/Http/Controllers/Api/PrintOrderController/StoreTest.php
+++ tests/Feature/Http/Controllers/Api/PrintOrderController/StoreTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Http\Controllers\Api\PrintOrderController;
 
+use App\Models\Order;
+use App\User;
 use Tests\TestCase;
 
 class StoreTest extends TestCase
@@ -9,6 +11,20 @@ class StoreTest extends TestCase
     /** @test */
     public function test_store_call_returns_ok()
     {
-        $this->markTestSkipped();
+        $order = factory(Order::class)->create();
+        $orderNumber = $order->order_number;
+        $user = factory(User::class)->create()->assignRole('admin');
+
+        $response = $this->actingAs($user, 'api')->put("api/print/order/$orderNumber/address_label");
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [
+                'title',
+                'content_type',
+                'expire_after',
+                'printer_id',
+                'content'
+            ]
+        ]);
     }
 }
