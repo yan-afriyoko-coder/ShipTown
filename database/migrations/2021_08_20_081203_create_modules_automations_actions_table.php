441ba20d8223e3aaf6b658d230bd694bcb12f729
Artur Hanusek <artur@arturhanusek.com>
2021-08-20 12:00:18 +0200

    create migration for automations_executions table

--- database/migrations/2021_08_20_081203_create_modules_automations_actions_table.php
+++ database/migrations/2021_08_20_081203_create_modules_automations_actions_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Modules\Automations\src\Models\Action;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,24 @@ class CreateModulesAutomationsActionsTable extends Migration
      */
     public function up()
     {
+        if (Schema::hasTable('modules_automations_executions')) {
+            Schema::rename('modules_automations_executions', 'modules_automations_actions');
+
+            Schema::table('modules_automations_actions', function (Blueprint $table) {
+                $table->renameColumn('execution_class', 'action_class');
+                $table->renameColumn('execution_value', 'action_value');
+
+            });
+
+            Action::query()->get()->each(function (Action $action) {
+                $action->action_class = Str::replaceFirst('Executors', 'Actions', $action->action_class);
+                $action->action_class = Str::replaceFirst('Executor', 'Action', $action->action_class);
+                $action->save();
+            });
+
+            return;
+        }
+
         if (Schema::hasTable('modules_automations_actions')) {
             return;
         }
