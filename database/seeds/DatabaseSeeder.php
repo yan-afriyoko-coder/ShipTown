144fac8385b52f1bd417e048e0b70571fc2a5c17
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-08-25 17:40:40 +0700

    Warehouse seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             OrderShipmentsSeeder::class,
             NavigationMenuSeeder::class,
             AutomationsSeeder::class,
+            WarehouseSeeder::class,
         ]);
 
         \App\Jobs\RunHourlyJobs::dispatchNow();
