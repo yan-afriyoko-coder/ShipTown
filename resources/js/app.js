9a26ccda134f1455c6d79656207e87a45fd02718
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-08-25 14:41:14 +0700

    Setting warehouse menu

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,7 +14,7 @@ import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/vue-loading.css';
 import { BootstrapVue } from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCog, faQuestionCircle, faUsers, faUserEdit, faBarcode, faUserMinus, faShareAlt, faChevronDown, faChevronUp, faExternalLinkAlt, faPrint, faCode, faTruck, faShoppingCart, faKey, faDesktop, faClipboardList, faPuzzlePiece, faBoxOpen, faEdit, faMinus, faCheckCircle, faTimesCircle, faEnvelopeOpenText, faListUl, faTrash, faMagic } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faQuestionCircle, faUsers, faUserEdit, faBarcode, faUserMinus, faShareAlt, faChevronDown, faChevronUp, faExternalLinkAlt, faPrint, faCode, faTruck, faShoppingCart, faKey, faDesktop, faClipboardList, faPuzzlePiece, faBoxOpen, faEdit, faMinus, faCheckCircle, faTimesCircle, faEnvelopeOpenText, faListUl, faTrash, faMagic, faWarehouse } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import VueTippy, { TippyComponent } from "vue-tippy";
 import Snotify from 'vue-snotify';
@@ -47,6 +47,7 @@ library.add(faEnvelopeOpenText);
 library.add(faListUl);
 library.add(faTrash);
 library.add(faMagic);
+library.add(faWarehouse)
 Vue.config.productionTip = false;
 
 Vue.use(VueCountdownTimer);
