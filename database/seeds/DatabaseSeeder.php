ed757440b511dd22817f337ed7cf7afdffc42f2e
Artur Hanusek <artur@arturhanusek.com>
2021-08-26 10:09:33 +0200

    add configuration seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            ConfigurationSeeder::class,
             UsersSeeder::class,
             ProductsSeeder::class,
             ProductAliasSeeder::class,
