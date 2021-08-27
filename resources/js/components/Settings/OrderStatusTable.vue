2a14a41894ed98746b1109ba8685788ca832a41b
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-08-27 15:54:32 +0700

    Change ui archive

--- resources/js/components/Settings/OrderStatusTable.vue
+++ resources/js/components/Settings/OrderStatusTable.vue
@@ -43,9 +43,6 @@
                                 <a @click.prevent="showEditForm(orderStatus)">
                                     <font-awesome-icon icon="edit"></font-awesome-icon>
                                 </a>
-                                <a @click.prevent="confirmDelete(orderStatus)">
-                                    <font-awesome-icon icon="archive"></font-awesome-icon>
-                                </a>
                             </td>
                         </tr>
                     </tbody>
@@ -58,7 +55,7 @@
         </div>
         <!-- The modals -->
         <create-modal id="createForm" @onCreated="addOrderStatus"></create-modal>
-        <edit-modal :orderStatus="selectedOrderStatus" id="editForm" @onUpdated="updateOrderStatus"></edit-modal>
+        <edit-modal :orderStatus="selectedOrderStatus" id="editForm" @onUpdated="updateOrderStatus" @onDeleted="removeOrderStatus"></edit-modal>
     </div>
 </template>
 
@@ -104,36 +101,10 @@ export default {
         addOrderStatus(orderStatus){
             this.orderStatuses.push(orderStatus)
         },
-        confirmDelete(orderStatus) {
-            const indexOrderStatuses = this.orderStatuses.findIndex(status => status.id == orderStatus.id)
-            this.$snotify.confirm(`wants to archive ${orderStatus.name}`, 'Are you sure?', {
-                position: 'centerCenter',
-                buttons: [
-                    {
-                        text: 'Yes',
-                        action: (toast) => {
-                            this.delete(orderStatus.id, indexOrderStatuses)
-                            this.$snotify.remove(toast.id);
-                        }
-                    },
-                    {text: 'Cancel'},
-                ]
-            });
+        removeOrderStatus(id){
+            const indexOrderStatuses = this.orderStatuses.findIndex(status => status.id == id)
+            Vue.delete(this.orderStatuses, indexOrderStatuses);
         },
-        delete(id, index) {
-            this.apiDeleteOrderStatus(id)
-                .then(() => {
-                    Vue.delete(this.orderStatuses, index);
-                    this.$snotify.success('Automation archived.');
-                })
-                .catch(error => {
-                    if (error.response) {
-                        if (error.response.status === 401) {
-                            this.$snotify.error(error.response.data.message);
-                        }
-                    }
-                });
-        }
     },
 }
 </script>
