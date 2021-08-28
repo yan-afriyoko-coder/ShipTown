c8699500578627b89e79850074dd60eec7254e40
Artur Hanusek <artur@arturhanusek.com>
2021-08-28 16:22:41 +0200

    use RefreshDatabase in each test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use JMac\Testing\Traits\AdditionalAssertions;
 
@@ -9,4 +10,5 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use AdditionalAssertions;
+    use RefreshDatabase;
 }
