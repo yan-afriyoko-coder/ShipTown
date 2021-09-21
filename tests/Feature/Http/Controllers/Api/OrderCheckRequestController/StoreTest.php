a4899faca69a208ad40a3c9da4b4d499421c8312
Artur Hanusek <artur@arturhanusek.com>
2021-09-22 00:06:08 +0200

    add test

---
+++ tests/Feature/Http/Controllers/Api/OrderCheckRequestController/StoreTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api\OrderCheckRequestController;
+
+use App\Models\Order;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class StoreTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $admin = factory(User::class)->create()->assignRole('admin');
+        $this->actingAs($admin, 'api');
+    }
+
+    /** @test */
+    public function test_store_call_returns_ok()
+    {
+        /** @var Order $order */
+        $order = factory(Order::class)->create();
+
+        $response = $this->postJson(route('order-check-request.store'), [
+            'order_id' => $order->getKey()
+        ]);
+
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'data' => [
+                'order_id'
+            ],
+        ]);
+    }
+}
