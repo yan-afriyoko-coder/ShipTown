50fe2e0ece46869328671e863a3dfe7705d638e4
Artur Hanusek <artur@arturhanusek.com>
2021-10-12 23:54:51 +0200

    fix controller namespace

--- tests/Feature/Http/Controllers/Api/Modules/PrintNode/PrintjobController/StoreTest.php
+++ tests/Feature/Http/Controllers/Api/Modules/PrintNode/PrintjobController/StoreTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Http\Controllers\Api\Modules\PrintNode\PrintJobController;
+namespace Tests\Feature\Http\Controllers\Api\Modules\PrintNode\PrintjobController;
 
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
