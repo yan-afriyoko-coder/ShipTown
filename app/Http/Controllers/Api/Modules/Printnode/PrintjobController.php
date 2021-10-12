9165c6057e3f9b12639d2788f91a1d3cb8d4c197
Artur Hanusek <artur@arturhanusek.com>
2021-10-12 23:52:57 +0200

    fix controller namespace

--- app/Http/Controllers/Api/Modules/Printnode/PrintjobController.php
+++ app/Http/Controllers/Api/Modules/Printnode/PrintjobController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\Modules\PrintNode;
+namespace App\Http\Controllers\Api\Modules\Printnode;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePrintJobRequest;
