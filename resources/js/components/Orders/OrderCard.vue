82bcf524acc92f891a779dda62b9b0f40a7b35d4
Artur Hanusek <artur@arturhanusek.com>
2021-10-18 12:25:18 +0200

    improve ui

--- resources/js/components/Orders/OrderCard.vue
+++ resources/js/components/Orders/OrderCard.vue
@@ -119,7 +119,7 @@
                                             <small> ordered </small>
                                             <h4>{{ toNumberOrDash(order_product['quantity_ordered']) }}</h4>
                                         </div>
-                                        <div class="col" v-if="order_product['quantity_split']">
+                                        <div class="col bg-warning" v-if="Number(order_product['quantity_split']) > 0">
                                             <small> split </small>
                                             <h4>{{ toNumberOrDash(order_product['quantity_split']) }}</h4>
                                         </div>
@@ -127,7 +127,7 @@
                                             <small> picked </small>
                                             <h4>{{ toNumberOrDash(order_product['quantity_picked']) }}</h4>
                                         </div>
-                                        <div class="col" v-bind:class="{ 'bg-warning': Number(order_product['quantity_skipped_picking']) > 0 }">
+                                        <div class="col bg-warning" v-if="Number(order_product['quantity_skipped_picking']) > 0">
                                             <small> skipped </small>
                                             <h4>{{ toNumberOrDash(order_product['quantity_skipped_picking']) }}</h4>
                                         </div>
