cf47db1478a9833c130653bc27d0f9991aaa3d40
Artur Hanusek <artur@arturhanusek.com>
2021-11-06 23:36:20 +0100

    move tests

---
+++ tests/External/DpdUk/GenerateLabelDocumentJobTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\External\DpdUk;
+
+use App\Events\OrderShipment\OrderShipmentCreatedEvent;
+use App\Models\OrderAddress;
+use App\Models\OrderShipment;
+use App\Modules\DpdUk\src\DpdUkServiceProvider;
+use App\Modules\DpdUk\src\Jobs\GenerateLabelDocumentJob;
+use App\Modules\DpdUk\src\Models\Connection;
+use App\Modules\DpdUk\src\Services\DpdUkService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Bus;
+use Tests\TestCase;
+
+class GenerateLabelDocumentJobTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_print_new_label()
+    {
+        /** @var OrderAddress $testAddress */
+        $testAddress = factory(OrderAddress::class)->make();
+        $testAddress->first_name      = 'My';
+        $testAddress->last_name       = 'Contact';
+        $testAddress->phone           = '0121 500 2500';
+        $testAddress->company         = "DPD Group Ltd";
+        $testAddress->country_code    = "GB";
+        $testAddress->postcode        = "B66 1BY";
+        $testAddress->address1        = "Roebuck Lane";
+        $testAddress->address2        = "Smethwick";
+        $testAddress->city            = "Birmingham";
+        $testAddress->state_code      = "West Midlands";
+        $testAddress->save();
+
+        /** @var Connection $connection */
+        $connection = factory(Connection::class)->make();
+        $connection->collection_address_id = $testAddress->getKey();
+        $connection->save();
+
+        /** @var OrderShipment $shipment */
+        $shipment = factory(OrderShipment::class)->create();
+        $shipment->order->shippingAddress()->associate($testAddress)->save();
+
+        $this->assertNotNull(DpdUkService::printNewLabel($shipment, $connection));
+    }
+}
--- tests/Feature/Modules/DpdUk/GenerateLabelDocumentJobTest.php
+++ tests/Feature/Modules/DpdUk/GenerateLabelDocumentJobTest.php
@@ -17,34 +17,6 @@ class GenerateLabelDocumentJobTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_print_new_label()
-    {
-        /** @var OrderAddress $testAddress */
-        $testAddress = factory(OrderAddress::class)->make();
-        $testAddress->first_name      = 'My';
-        $testAddress->last_name       = 'Contact';
-        $testAddress->phone           = '0121 500 2500';
-        $testAddress->company         = "DPD Group Ltd";
-        $testAddress->country_code    = "GB";
-        $testAddress->postcode        = "B66 1BY";
-        $testAddress->address1        = "Roebuck Lane";
-        $testAddress->address2        = "Smethwick";
-        $testAddress->city            = "Birmingham";
-        $testAddress->state_code      = "West Midlands";
-        $testAddress->save();
-
-        /** @var Connection $connection */
-        $connection = factory(Connection::class)->make();
-        $connection->collection_address_id = $testAddress->getKey();
-        $connection->save();
-
-        /** @var OrderShipment $shipment */
-        $shipment = factory(OrderShipment::class)->create();
-        $shipment->order->shippingAddress()->associate($testAddress)->save();
-
-        $this->assertNotNull(DpdUkService::printNewLabel($shipment, $connection));
-    }
-
     public function test_if_job_dispatches()
     {
         DpdUkServiceProvider::enableModule();
