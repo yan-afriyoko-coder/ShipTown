a5baacb61a338adf1a4e4dec1f5977b7e1a6e27f
Rizky Puji Raharja <rizky.rsj@gmail.com>
2021-11-13 03:59:29 +0000

    refactor migration

--- database/migrations/2021_11_10_165754_add_2fa_users_table.php
+++ database/migrations/2021_11_10_165754_add_2fa_users_table.php
@@ -14,8 +14,8 @@ class Add2faUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('two_factor_code')->nullable();
-            $table->dateTime('two_factor_expires_at')->nullable();
+            $table->string('two_factor_code')->after('password')->nullable();
+            $table->dateTime('two_factor_expires_at')->after('two_factor_code')->nullable();
         });
     }
 
