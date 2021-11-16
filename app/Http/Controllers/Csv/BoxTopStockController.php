949a1a523062160a8c5e87dd871a063b232c4cab
Artur Hanusek <artur@arturhanusek.com>
2021-11-16 22:33:28 +0100

    update BoxTop csv file download

--- app/Http/Controllers/Csv/BoxTopStockController.php
+++ app/Http/Controllers/Csv/BoxTopStockController.php
@@ -11,6 +11,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use League\Csv\CannotInsertRecord;
+use League\Csv\Writer;
 
 class BoxTopStockController extends Controller
 {
@@ -21,7 +22,11 @@ class BoxTopStockController extends Controller
      */
     public function index(Request $request)
     {
-        $query = DB::select("
+        $filename = 'boxtop_stock.csv';
+
+        BoxTopService::refreshBoxTopWarehouseStock();
+
+        $query = DB::select(DB::raw("
 SELECT
  modules_boxtop_warehouse_stock.SKUNumber AS SKU_BoxTop,
  products.sku as SKU_RMS,
@@ -37,32 +42,31 @@ LEFT JOIN products
   ON products_aliases.product_id = products.id
 
 ORDER BY SKU_RMS, SKUName ASC
-");
+"));
 
-//        dd($query);
-        $vars = collect($query)->map(function ($record) {
-            dd($record);
-            $record['Attributes'] = json_encode($record['Attributes']);
-            return $record;
+        $recordSet = collect($query)->map(function ($record) {
+            return [
+                'SKU_BoxTop' => $record->SKU_BoxTop,
+                'SKU_RMS' => $record->SKU_RMS,
+                'SKUName' => $record->SKUName,
+                'Available' => $record->Available,
+            ];
         });
 
-        dd($vars->toArray());
+        $csv = Writer::createFromFileObject(new \SplTempFileObject());
 
-//        WarehouseStock::query()->delete();
-//        WarehouseStock::query()->insert($vars->toArray());
-//
-//        $query = WarehouseStock::query()->select([
-//            'SKUGroup',
-//            'SKUNumber',
-//            'SKUName',
-//            'Warehouse',
-//            'WarehouseQuantity',
-//            'Allocated',
-//            'Available',
-//        ]);
+        if ($recordSet->isNotEmpty()) {
+            $csv->insertOne(array_keys($recordSet[0]));
 
+            foreach ($recordSet as $record) {
+                $csv->insertOne($record);
+            }
+        }
 
-
-        return $this->toCsvFileResponse(collect($vars), 'boxtop_stock.csv');
+        return response((string) $csv, 200, [
+            'Content-Type'              => 'text/csv',
+            'Content-Transfer-Encoding' => 'binary',
+            'Content-Disposition'       => 'attachment; filename="'.$filename.'"',
+        ]);
     }
 }
