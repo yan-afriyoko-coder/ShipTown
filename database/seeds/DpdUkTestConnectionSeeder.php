c4f1f5ec46784c68c8ce69279fc15aa7a1ec1564
Artur Hanusek <artur@arturhanusek.com>
2021-12-01 10:21:25 +0000

    create order with incorrect UK postcode

--- database/seeds/DpdUkTestConnectionSeeder.php
+++ database/seeds/DpdUkTestConnectionSeeder.php
@@ -19,32 +19,69 @@ class DpdUkTestConnectionSeeder extends Seeder
         if (env('TEST_DPDUK_USERNAME')) {
             DpdUkServiceProvider::enableModule();
 
-            /** @var OrderAddress $testAddress */
-            $testAddress = factory(OrderAddress::class)->make();
-            $testAddress->first_name      = 'My';
-            $testAddress->last_name       = 'Contact';
-            $testAddress->phone           = '0121 500 2500';
-            $testAddress->company         = "DPD Group Ltd";
-            $testAddress->country_code    = "GB";
-            $testAddress->postcode        = "B66 1BY";
-            $testAddress->address1        = "Roebuck Lane";
-            $testAddress->address2        = "Smethwick";
-            $testAddress->city            = "Birmingham";
-            $testAddress->state_code      = "West Midlands";
-            $testAddress->save();
-
-            /** @var Order $order */
-            $order= factory(Order::class)->make();
-            $order->order_number .= '-DPDUK';
-            $order->shippingAddress()->associate($testAddress);
-            $order->save();
-            factory(OrderProduct::class,3)->create(['order_id' => $order->getKey()]);
-
+            $testAddress = $this->createTestOrder();
+            $testAddress = $this->createOrderWithWrongPostCode();
 
             /** @var Connection $connection */
             $connection = factory(Connection::class)->make();
             $connection->collectionAddress()->associate($testAddress);
             $connection->save();
         }
+    }
+
+    /**
+     * @return OrderAddress
+     */
+    private function createTestOrder(): OrderAddress
+    {
+        /** @var OrderAddress $testAddress */
+        $testAddress = factory(OrderAddress::class)->make();
+        $testAddress->first_name = 'My';
+        $testAddress->last_name = 'Contact';
+        $testAddress->phone = '0121 500 2500';
+        $testAddress->company = "DPD Group Ltd";
+        $testAddress->country_code = "GB";
+        $testAddress->postcode = "B66 1BY";
+        $testAddress->address1 = "Roebuck Lane";
+        $testAddress->address2 = "Smethwick";
+        $testAddress->city = "Birmingham";
+        $testAddress->state_code = "West Midlands";
+        $testAddress->save();
+
+        /** @var Order $order */
+        $order = factory(Order::class)->make();
+        $order->order_number .= '-DPDUK';
+        $order->shippingAddress()->associate($testAddress);
+        $order->save();
+        factory(OrderProduct::class, 3)->create(['order_id' => $order->getKey()]);
+        return $testAddress;
+    }
+
+    /**
+     * @return OrderAddress
+     */
+    private function createOrderWithWrongPostCode(): OrderAddress
+    {
+        /** @var OrderAddress $testAddress */
+        $testAddress = factory(OrderAddress::class)->make();
+        $testAddress->first_name = 'My';
+        $testAddress->last_name = 'Contact';
+        $testAddress->phone = '0121 500 2500';
+        $testAddress->company = "DPD Group Ltd";
+        $testAddress->country_code = "GB";
+        $testAddress->postcode = "B66";
+        $testAddress->address1 = "Roebuck Lane";
+        $testAddress->address2 = "Smethwick";
+        $testAddress->city = "Birmingham";
+        $testAddress->state_code = "West Midlands";
+        $testAddress->save();
+
+        /** @var Order $order */
+        $order = factory(Order::class)->make();
+        $order->order_number .= '-DPDUK-WRONG-POSTCODE';
+        $order->shippingAddress()->associate($testAddress);
+        $order->save();
+        factory(OrderProduct::class, 3)->create(['order_id' => $order->getKey()]);
+        return $testAddress;
     }
 }
