6e844f6eef2bacc8c54c97987ea88a4c9f497f4e
Artur Hanusek <artur@arturhanusek.com>
2021-12-15 14:13:55 +0100

    order page UI improvements

--- resources/js/components/Orders/OrderCard.vue
+++ resources/js/components/Orders/OrderCard.vue
@@ -12,19 +12,21 @@
       <div class="swiper-slide">
           <div class="row ml-1 mr-1 card">
               <div class="col p-2 pl-2 rounded">
-                <div class="row">
+                  <div class="row">
+                          <h5 class="text-primary">
+                              <font-awesome-icon icon="copy" class="fa-xs" role="button" @click="copyToClipBoard(order['order_number'])"></font-awesome-icon>
+                              <a :href="'/orders/?search=' + order['order_number']">{{ order['order_number'] }}</a>
+                              <a @click="kickOrder" class="text-white">o</a>
+                          </h5>
+                  </div>
+                <div class="row align-text-top">
 
                     <div class="col-5 col-md-4 col-lg-3 align-text-top">
-                        <h5 class="text-primary">
-                            <font-awesome-icon icon="copy" class="fa-xs" role="button" @click="copyToClipBoard(order['order_number'])"></font-awesome-icon>
-                            <a :href="'/orders/?search=' + order['order_number']">{{ order['order_number'] }}</a>
-                            <a @click="kickOrder" class="text-white">o</a>
-                        </h5>
                         <div class="small"> <b> {{ order['status_code'] }} </b> </div>
                     </div>
 
                     <div class="col text-center" @click="toggleOrderDetails">
-                        <div class="row">
+                        <div class="row ">
                             <div class="col">
                                 <small> age </small>
                                 <h5> {{ order['age_in_days'] }}</h5>
