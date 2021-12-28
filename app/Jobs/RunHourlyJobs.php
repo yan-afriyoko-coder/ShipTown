c0f2f75361cb89ecda13268b042f2c2f712394ca
Artur Hanusek <artur@arturhanusek.com>
2021-12-28 08:57:57 +0100

    refactoring

--- app/Jobs/RunHourlyJobs.php
+++ app/Jobs/RunHourlyJobs.php
@@ -3,7 +3,6 @@
 namespace App\Jobs;
 
 use App\Events\HourlyEvent;
-use App\Models\OrderAddress;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
