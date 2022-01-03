1ce79a3753b043f3fea9ddf9ab5e100a6ba22b2f
Artur Hanusek <artur@arturhanusek.com>
2022-01-03 12:54:49 +0100

    update column types

---
+++ database/migrations/2022_01_03_114244_update_column_types_to_uniform_on_order_products_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateColumnTypesToUniformOnOrderProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_products', function (Blueprint $table) {
+            $table->decimal('quantity_split', 10)->default(0)->change();
+            $table->decimal('quantity_to_ship', 10)->nullable(false)->storedAs('quantity_ordered - quantity_split - quantity_shipped')->default(0)->change();
+        });
+    }
+}
