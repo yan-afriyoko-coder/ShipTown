2ba03f2baf2c9339b03e6aa1f24b50d17fc462d6
Artur Hanusek <artur@arturhanusek.com>
2022-01-04 17:32:27 +0100

    update quantity_to_ship and quantity_split columns types

--- database/migrations/2022_01_03_114244_update_column_types_to_uniform_on_order_products_table.php
+++ database/migrations/2022_01_03_114244_update_column_types_to_uniform_on_order_products_table.php
@@ -15,7 +15,15 @@ class UpdateColumnTypesToUniformOnOrderProductsTable extends Migration
     {
         Schema::table('order_products', function (Blueprint $table) {
             $table->decimal('quantity_split', 10)->default(0)->change();
-            $table->decimal('quantity_to_ship', 10)->nullable(false)->storedAs('quantity_ordered - quantity_split - quantity_shipped')->default(0)->change();
+            $table->dropColumn('quantity_to_ship');
+        });
+
+        Schema::table('order_products', function (Blueprint $table) {
+            $table->decimal('quantity_to_ship', 10)
+                ->nullable(false)
+                ->storedAs('quantity_ordered - quantity_split - quantity_shipped')
+                ->after('quantity_shipped');
         });
     }
 }
+
