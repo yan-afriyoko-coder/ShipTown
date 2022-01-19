f87734a3f8911a00a0ecebc7b4c44474bc6ab492
Artur Hanusek <artur@arturhanusek.com>
2022-01-19 11:58:34 +0100

    add international rules

--- tests/External/DpdIrelandInternational/International_CHE_Test.php
+++ tests/External/DpdIrelandInternational/International_CHE_Test.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\External\DpdIreland\International;
+namespace Tests\External\DpdIrelandInternational;
 
 use App\Models\Order;
 use App\Models\OrderAddress;
@@ -19,36 +19,38 @@ class International_CHE_Test extends TestCase
         $this->markTestIncomplete();
     }
 
-//    public function test_normal_overnight_consignment_single_parcel()
-//    {
-//        $address = factory(OrderAddress::class)->create([
-//            'company'      => 'DPD Test',
-//            'first_name'   => 'DPD',
-//            'last_name'    => 'Depot',
-//            'address1'     => 'Maisbühlstrasse 50',
-//            'address2'     => '',
-//            'phone'        => '0861230000',
-//            'city'         => 'Unterägeri',
-//            'state_name'   => 'ZG',
-//            'postcode'     => '6314',
-//            'country_code' => 'CHE',
-//        ]);
-//
-//        $order = factory(Order::class)->create([
-//            'shipping_address_id' => $address->getKey(),
-//        ]);
-//
-//        try {
-//            Dpd::shipOrder($order);
-//            $success = true;
-//        } catch (Exception $e) {
-//            Log::error($e->getMessage());
-//            $this->fail($e->getMessage());
-//        }
-//
-//        // we just want no exceptions
-//        $this->assertTrue($success);
-//    }
+    public function test_normal_overnight_consignment_single_parcel()
+    {
+        $this->markTestIncomplete();
+
+        $address = factory(OrderAddress::class)->create([
+            'company'      => 'DPD Test',
+            'first_name'   => 'DPD',
+            'last_name'    => 'Depot',
+            'address1'     => 'Maisbühlstrasse 50',
+            'address2'     => '',
+            'phone'        => '0861230000',
+            'city'         => 'Unterägeri',
+            'state_name'   => 'ZG',
+            'postcode'     => '6314',
+            'country_code' => 'CHE',
+        ]);
+
+        $order = factory(Order::class)->create([
+            'shipping_address_id' => $address->getKey(),
+        ]);
+
+        try {
+            Dpd::shipOrder($order);
+            $success = true;
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            $this->fail($e->getMessage());
+        }
+
+        // we just want no exceptions
+        $this->assertTrue($success);
+    }
 //
 //    /**
 //     * @test
