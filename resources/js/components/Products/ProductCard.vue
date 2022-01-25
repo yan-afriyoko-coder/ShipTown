def2603ebe98f67c6d79542f140d9963b2ef54cf
Artur Hanusek <artur@arturhanusek.com>
2022-01-25 23:46:19 +0100

    warehouse.code instead location_id

--- resources/js/components/Products/ProductCard.vue
+++ resources/js/components/Products/ProductCard.vue
@@ -20,7 +20,7 @@
                     </div>
                     <div class="col-md-6" @click="toggle">
                         <div class="row small font-weight-bold text-right">
-                            <div class="col-2 text-left">Warehouse</div>
+                            <div class="col-2 text-left">Location</div>
                             <div class="col-2 d-none d-sm-block ">In Stock</div >
                             <div class="col-3 col-sm-2">Reserved</div>
                             <div class="col-3">Available</div>
