962e829f8fa39e6561d090a4378a67d1fe144a70
Artur Hanusek <artur@arturhanusek.com>
2022-01-27 20:43:40 +0100

    log msg when product shipped

--- app/Observers/OrderProductShipmentObserver.php
+++ app/Observers/OrderProductShipmentObserver.php
@@ -2,8 +2,8 @@
 
 namespace App\Observers;
 
-use App\Models\OrderProductShipment;
 use App\Events\OrderProductShipmentCreatedEvent;
+use App\Models\OrderProductShipment;
 
 class OrderProductShipmentObserver
 {
@@ -17,8 +17,20 @@ class OrderProductShipmentObserver
     public function created(OrderProductShipment $orderProductShipment)
     {
         activity()->on($orderProductShipment->order)
-            ->log('shipped {"sku": "' .
-                $orderProductShipment->sku_shipped. '", "quantity": '.$orderProductShipment->quantity_shipped.'}');
+            ->causedBy($orderProductShipment->user)
+            ->withProperties([
+                'sku' => $orderProductShipment->sku_shipped,
+                'quantity' => $orderProductShipment->quantity_shipped,
+                'warehouse_code' => $orderProductShipment->warehouse->code
+            ]);
+
+        activity()->on($orderProductShipment->product)
+            ->causedBy($orderProductShipment->user)
+            ->withProperties([
+                'order_number' => $orderProductShipment->order->order_number,
+                'quantity' => $orderProductShipment->quantity_shipped,
+                'warehouse_code' => $orderProductShipment->warehouse->code
+            ]);
 
         OrderProductShipmentCreatedEvent::dispatch($orderProductShipment);
     }
