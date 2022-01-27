e2f95ad312cf2c6a5f48ba407863ba8e84ff0a25
Artur Hanusek <artur@arturhanusek.com>
2022-01-27 19:44:03 +0100

    add product_id to resource

--- app/Http/Resources/OrderProductShipmentResource.php
+++ app/Http/Resources/OrderProductShipmentResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderProductShipmentResource extends JsonResource
@@ -9,18 +10,20 @@ class OrderProductShipmentResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      *
      * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
         return [
-            'id'                              => $this->id,
-            'warehouse_id'                    => $this->warehouse_id,
-            'order_product_id'                => $this->order_product_id,
-            'quantity_shipped'                => $this->quantity_shipped,
-            'order_product_shipment_id'       => $this->order_product_shipment_id,
+            'id'                        => $this->id,
+            'product_id'                => $this->warehouse_id,
+            'warehouse_id'              => $this->warehouse_id,
+            'order_id'                  => $this->order_product_id,
+            'order_product_id'          => $this->order_product_id,
+            'quantity_shipped'          => $this->quantity_shipped,
+            'order_product_shipment_id' => $this->order_product_shipment_id,
         ];
     }
 }
