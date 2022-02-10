16a0317320886e3bda97303227ad5b9bf9b485c8
Artur Hanusek <artur@arturhanusek.com>
2022-02-10 10:44:57 +0100

    move out of stock tagging to job and delay by 60s

---
+++ app/Modules/AutoTags/src/Jobs/ToggleOutOfStockTagJob.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Modules\AutoTags\src\Jobs;
+
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+use romanzipp\QueueMonitor\Traits\IsMonitored;
+
+class ToggleOutOfStockTagJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    private int $product_id;
+
+    public function __construct(int $product_id)
+    {
+        $this->product_id = $product_id;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        /** @var Product $product */
+        $product = Product::find($this->product_id, ['id', 'quantity_available']);
+
+        if ($product->quantity_available <= 0) {
+            $product->attachTag('Out Of Stock');
+        } else {
+            $product->detachTag('Out Of Stock');
+        }
+    }
+}
