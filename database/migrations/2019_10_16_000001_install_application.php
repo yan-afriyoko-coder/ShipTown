a17df7ae41af37f965899790c9d50d678ff34778
Artur Hanusek <artur@arturhanusek.com>
2022-02-13 13:56:18 +0100

    merge configurations

---
+++ database/migrations/2019_10_16_000001_install_application.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\Configuration;
+use App\Models\MailTemplate;
+use App\Models\NavigationMenu;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class InstallApplication extends Migration
+{
+    public function up()
+    {
+        //
+    }
+}
