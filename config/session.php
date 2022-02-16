d08c015b78fb9bff396c74815cb1397cf816e16c
Artur Hanusek <artur@arturhanusek.com>
2022-02-16 14:52:40 +0100

    change session driver to database for possible session control

--- config/session.php
+++ config/session.php
@@ -18,7 +18,8 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'file'),
+    'driver' => 'database',
+//    'driver' => env('SESSION_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
