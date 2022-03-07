afdf1087e895d52dd661279830b8f87ef177fc58
Artur Hanusek <artur@arturhanusek.com>
2022-03-07 19:39:02 +0100

    change demo user names

--- database/seeds/demo/UsersSeeder.php
+++ database/seeds/demo/UsersSeeder.php
@@ -14,20 +14,20 @@ class UsersSeeder extends Seeder
     {
         // create admin
         if (User::query()
-            ->where('email', '=', 'demo+admin@products.management')
+            ->where('email', '=', 'admin@products.management')
             ->doesntExist()) {
             $user = factory(User::class, 1)->create([
                 'name' => 'Artur Hanusek',
-                'email' => 'demo+admin@products.management'
+                'email' => 'admin@products.management'
             ]);
             $user->first()->assignRole('admin');
         }
 
         // create user
         if (User::query()
-            ->where('email', '=', 'demo+user@products.management')
+            ->where('email', '=', 'user@products.management')
             ->doesntExist()) {
-            $user = factory(User::class, 1)->create(['email' => 'demo+user@products.management']);
+            $user = factory(User::class, 1)->create(['email' => 'user@products.management']);
             $user->first()->assignRole('user');
         }
     }
