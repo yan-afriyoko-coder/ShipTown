3f75f75d1d8dbd51ce296c7cbbb44186a9f2fa84
Artur Hanusek <artur@arturhanusek.com>
2022-03-18 17:53:37 +0100

    fix test

--- database/migrations/2022_03_18_140950_change_warehouse_code_column_type_on_warehouses.php
+++ database/migrations/2022_03_18_140950_change_warehouse_code_column_type_on_warehouses.php
@@ -23,20 +23,24 @@ class ChangeWarehouseCodeColumnTypeOnWarehouses extends Migration
 
         Schema::table('inventory', function (Blueprint $table) {
             $table->string('warehouse_code', 5)->nullable(false)->change();
-
-            $table->foreign('warehouse_code')
-                ->on('warehouses')
-                ->references('code')
-                ->onDelete('CASCADE');
+//
+//            $table->foreign('warehouse_code')
+//                ->on('warehouses')
+//                ->references('code')
+//                ->onDelete('CASCADE');
         });
 
         Schema::table('products_prices', function (Blueprint $table) {
             $table->string('warehouse_code', 5)->nullable(false)->change();
+//
+//            $table->foreign('warehouse_code')
+//                ->on('warehouses')
+//                ->references('code')
+//                ->onDelete('CASCADE');
+        });
 
-            $table->foreign('warehouse_code')
-                ->on('warehouses')
-                ->references('code')
-                ->onDelete('CASCADE');
+        Schema::table('modules_api2cart_connections', function (Blueprint $table) {
+            $table->string('location_id', 5)->nullable(true)->change();
         });
     }
 
