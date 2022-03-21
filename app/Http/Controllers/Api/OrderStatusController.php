fddbe1a2d7b417405367af0d2e83cc43a0e18809
Artur Hanusek <artur@arturhanusek.com>
2022-03-21 14:12:18 +0100

    add OrderStatusIndexRequest

--- app/Http/Controllers/Api/OrderStatusController.php
+++ app/Http/Controllers/Api/OrderStatusController.php
@@ -3,19 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OrderStatusIndexRequest;
 use App\Http\Resources\OrderStatusResource;
 use App\Models\OrderStatus;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class OrderStatusController extends Controller
 {
-
     /**
      * Display a listing of the resource.
      *
+     * @param OrderStatusIndexRequest $request
      * @return AnonymousResourceCollection
      */
-    public function index() : AnonymousResourceCollection
+    public function index(OrderStatusIndexRequest $request) : AnonymousResourceCollection
     {
         $query = OrderStatus::getSpatieQueryBuilder();
 
