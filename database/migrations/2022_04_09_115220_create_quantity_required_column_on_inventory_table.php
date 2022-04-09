639d4532f351de59387b206e31025592bed428f5
Artur Hanusek <artur@arturhanusek.com>
2022-04-09 13:29:15 +0100

    fix migration

--- database/migrations/2022_04_09_115220_create_quantity_required_column_on_inventory_table.php
+++ database/migrations/2022_04_09_115220_create_quantity_required_column_on_inventory_table.php
@@ -14,10 +14,20 @@ class CreateQuantityRequiredColumnOnInventoryTable extends Migration
     public function up()
     {
         Schema::table('inventory', function (Blueprint $table) {
+            $table->decimal('quantity_available', 10)
+                ->storedAs('quantity - quantity_reserved')
+                ->always()
+                ->comment('quantity - quantity_reserved')
+                ->after('shelve_location')
+                ->change();
+        });
+
+        Schema::table('inventory', function (Blueprint $table) {
             $table->decimal('quantity_required', 10)
                 ->storedAs('CASE WHEN quantity_available < reorder_point ' .
                     'THEN restock_level - quantity_available ' .
                     'ELSE 0 END')
+                ->always()
                 ->comment('CASE WHEN quantity_available < reorder_point ' .
                     'THEN restock_level - quantity_available ' .
                     'ELSE 0 END')
