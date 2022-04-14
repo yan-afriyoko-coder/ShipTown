690f494f35a2433058f196d273491c33fb42a287
Artur Hanusek <artur@arturhanusek.com>
2022-04-14 11:43:30 +0200

    deduct quantity_split from quantity_ordered

---
+++ database/migrations/2022_04_14_094018_update_order_totals_view_quantity_ordered_sum_column.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateOrderTotalsViewQuantityOrderedSumColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("
+            CREATE OR REPLACE VIEW orders_totals_view
+            AS
+            SELECT
+                order_id,
+                COUNT(id) as product_line_count,
+                IFNULL(SUM(quantity_ordered - quantity_split), 0) as quantity_ordered_sum,
+                IFNULL(SUM(quantity_to_ship), 0) as quantity_to_ship_sum,
+                IFNULL(SUM(price * (quantity_ordered - quantity_split)), 0) as total_ordered
+
+            FROM orders_products
+            GROUP BY order_id
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
