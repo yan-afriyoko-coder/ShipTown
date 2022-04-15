afbbd251eee24ea8b6dc7e9c19f95850c0a27a31
Artur Hanusek <artur@arturhanusek.com>
2022-04-16 00:23:13 +0200

    add download feature

--- resources/views/reports/inventory.blade.php
+++ resources/views/reports/inventory.blade.php
@@ -1,20 +1,13 @@
 @extends('layouts.app')
 
-@section('title',__('Restocking Report'))
+@section('title',__('Inventory Report'))
 
 @section('content')
     <div class="container dashboard-widgets">
     <div class="row">
         <div class="col">
             <div class="widget-tools-container">
-                <font-awesome-icon icon="question-circle" name="user-pick-counts-help-icon"></font-awesome-icon>
-                <template>
-                    <tippy to="user-pick-counts-help-icon" arrow>
-                        <p>
-                            {{--                //Total number of picks in last 7 days per user<br>--}}
-                        </p>
-                    </tippy>
-                </template>
+                <a class="btn btn-primary btn-sm mt-2 fa-arrow-alt-circle-down"  href="{{ request()->fullUrlWithQuery(['filename' => 'inventory.csv']) }}">{{ __('Download') }}</a>
             </div>
             <div class="card">
                 <div class="card-body">
