302c371399375872a9d41a3727f3f45d94b8b1d2
Artur Hanusek <artur@arturhanusek.com>
2022-04-24 21:56:30 +0200

    refactor Packlist controller

--- app/Http/Controllers/Api/PacklistOrderController.php
+++ app/Http/Controllers/Api/PacklistOrderController.php
@@ -9,6 +9,7 @@ use App\Http\Resources\OrderResource;
 use App\Http\Resources\PacklistResource;
 use App\Models\Order;
 use App\Models\Packlist;
+use App\Modules\Reports\src\Models\Report;
 use App\Services\OrderService;
 use Illuminate\Http\Request;
 
@@ -24,11 +25,30 @@ class PacklistOrderController extends Controller
      */
     public function index(PacklistOrderIndexRequest $request): OrderResource
     {
-        /** @var Order $order */
-        $order = Order::getSpatieQueryBuilder()
+        $report = new Report();
+        $report->fields = [
+            'id'                                => 'orders.id',
+            'order_number'                      => 'orders.order_number',
+            'status'                            => 'orders.status_code',
+            'status_code'                       => 'orders.status_code',
+            'order_placed_at'                   => 'orders.order_placed_at',
+            'updated_at'                        => 'orders.updated_at',
+            'inventory_source_warehouse_id'     => 'inventory_source.warehouse_id',
+            'inventory_source_warehouse_code'   => 'inventory_source.warehouse_code',
+            'inventory_source_shelf_location'   => 'inventory_source.shelve_location',
+        ];
+
+        $report->baseQuery = Order::query()
+            ->leftJoin('orders_products', 'orders_products.order_id', '=', 'orders.id')
+            ->leftJoin('inventory as inventory_source', function ($join) {
+                $join->on('inventory_source.product_id', '=', 'orders_products.product_id');
+            })
+            ->leftJoin('products', 'products.id', '=', 'orders_products.product_id')
             ->whereNull('packed_at')
-            ->whereNull('packer_user_id')
-            ->firstOrFail();
+            ->whereNull('packer_user_id');
+
+        /** @var Order $order */
+        $order = $report->queryBuilder()->firstOrFail();
 
         $wasReserved = Order::where(['id' => $order->id])
             ->where(['updated_at' => $order->updated_at])
