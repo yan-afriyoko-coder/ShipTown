1fdda91d97fd217d6b0eb20aeca8ea3fc9c7c273
Artur Hanusek <artur@arturhanusek.com>
2022-05-03 17:15:21 +0200

    refactor

--- tests/Feature/Http/Controllers/Api/ActivityController/StoreTest.php
+++ tests/Feature/Http/Controllers/Api/ActivityController/StoreTest.php
@@ -15,8 +15,8 @@ class StoreTest extends TestCase
     public function test_store_call_returns_ok()
     {
         $order = factory(Order::class)->create();
-
         $user = factory(User::class)->create();
+
         $response = $this->actingAs($user, 'api')
             ->postJson(route('activities.store', [
                 'subject_type' => 'order',
