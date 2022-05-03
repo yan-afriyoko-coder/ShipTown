f6bf233ed4e82a1a9ed4f3f6f4c0d2b00fdf2987
Artur Hanusek <artur@arturhanusek.com>
2022-05-03 15:03:21 +0200

    uninstall ShipmentConfirmationEmail module

---
+++ app/Services/ModulesService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Module;
+
+class ModulesService
+{
+    private static array $modules = [
+        // misc modules
+        \App\Modules\Maintenance\src\EventServiceProviderBase::class,
+        \App\Modules\StockControl\src\StockControlServiceProvider::class,
+        \App\Modules\OrderTotals\src\OrderTotalsServiceProvider::class,
+        \App\Modules\OrderStatus\src\OrderStatusServiceProvider::class,
+        \App\Modules\FireActiveOrderCheckEvent\src\ActiveOrderCheckEventServiceProvider::class,
+
+        \App\Modules\InventoryReservations\src\EventServiceProviderBase::class,
+        \App\Modules\Automations\src\AutomationsServiceProvider::class,
+
+        // Automations modules
+        // order MIGHT be important!
+        \App\Modules\AutoPilot\src\AutoPilotServiceProvider::class,
+        \App\Modules\AutoTags\src\EventServiceProviderBase::class,
+        \App\Modules\OversoldProductNotification\src\OversoldProductNotificationServiceProvider::class,
+
+        // AutoStatus modules
+        // order is important!
+        \App\Modules\AutoStatusPicking\src\AutoStatusPickingServiceProvider::class,
+
+        // 3rd party integrations
+        // order SHOULD not be important
+        \App\Modules\Webhooks\src\WebhooksServiceProviderBase::class,
+        \App\Modules\Api2cart\src\Api2cartServiceProvider::class,
+        \App\Modules\Rmsapi\src\RmsapiModuleServiceProvider::class,
+        \App\Modules\MagentoApi\src\EventServiceProviderBase::class,
+        \App\Modules\ScurriAnpost\src\ScurriServiceProvider::class,
+        \App\Modules\DpdUk\src\DpdUkServiceProvider::class,
+        \App\Modules\AddressLabel\src\AddressLabelServiceProvider::class,
+        \App\Modules\DpdIreland\src\DpdIrelandServiceProvider::class,
+    ];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public static function updateModulesTable()
+    {
+        Module::query()->whereNotIn('service_provider_class', self::$modules)->forceDelete();
+
+        collect(self::$modules)->each(function (string $module_class) {
+            Module::firstOrCreate([
+                'service_provider_class' => $module_class
+            ], [
+                'enabled' => $module_class::$autoEnable,
+            ]);
+        });
+    }
+}
--- database/migrations/2022_04_28_113118_reinstall_modules.php
+++ database/migrations/2022_04_28_113118_reinstall_modules.php
@@ -1,46 +1,9 @@
 <?php
 
-use App\Models\Module;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class ReinstallModules extends Migration
 {
-    private array $modules = [
-        // misc modules
-        App\Modules\Maintenance\src\EventServiceProviderBase::class,
-        App\Modules\StockControl\src\StockControlServiceProvider::class,
-        App\Modules\OrderTotals\src\OrderTotalsServiceProvider::class,
-        App\Modules\OrderStatus\src\OrderStatusServiceProvider::class,
-        App\Modules\FireActiveOrderCheckEvent\src\ActiveOrderCheckEventServiceProvider::class,
-
-        App\Modules\InventoryReservations\src\EventServiceProviderBase::class,
-        App\Modules\Automations\src\AutomationsServiceProvider::class,
-
-        // Automations modules
-        // order MIGHT be important!
-        App\Modules\ShipmentConfirmationEmail\src\ServiceProvider::class,
-        App\Modules\AutoPilot\src\AutoPilotServiceProvider::class,
-        App\Modules\AutoTags\src\EventServiceProviderBase::class,
-        App\Modules\OversoldProductNotification\src\OversoldProductNotificationServiceProvider::class,
-
-        // AutoStatus modules
-        // order is important!
-        App\Modules\AutoStatusPicking\src\AutoStatusPickingServiceProvider::class,
-
-        // 3rd party integrations
-        // order SHOULD not be important
-        App\Modules\Webhooks\src\WebhooksServiceProviderBase::class,
-        App\Modules\Api2cart\src\Api2cartServiceProvider::class,
-        App\Modules\Rmsapi\src\RmsapiModuleServiceProvider::class,
-        App\Modules\MagentoApi\src\EventServiceProviderBase::class,
-        App\Modules\ScurriAnpost\src\ScurriServiceProvider::class,
-        App\Modules\DpdUk\src\DpdUkServiceProvider::class,
-        App\Modules\AddressLabel\src\AddressLabelServiceProvider::class,
-        App\Modules\DpdIreland\src\DpdIrelandServiceProvider::class,
-    ];
-
     /**
      * Run the migrations.
      *
@@ -48,14 +11,6 @@ class ReinstallModules extends Migration
      */
     public function up()
     {
-        Module::query()->whereNotIn('service_provider_class', $this->modules)->forceDelete();
-
-        collect($this->modules)->each(function (string $module_class) {
-            Module::firstOrCreate([
-                'service_provider_class' => $module_class
-            ], [
-                'enabled' => $module_class::$autoEnable,
-            ]);
-        });
+        \App\Services\ModulesService::updateModulesTable();
     }
 }
