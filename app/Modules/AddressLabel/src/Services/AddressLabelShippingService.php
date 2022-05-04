56f253e08492e19d85e5b5b7d3e3ce7976b2824f
Artur Hanusek <artur@arturhanusek.com>
2022-05-04 22:27:19 +0200

    create shipping label on address label request

--- app/Modules/AddressLabel/src/Services/AddressLabelShippingService.php
+++ app/Modules/AddressLabel/src/Services/AddressLabelShippingService.php
@@ -4,13 +4,11 @@ namespace App\Modules\AddressLabel\src\Services;
 
 use App\Abstracts\ShippingServiceAbstract;
 use App\Models\Order;
-use App\Modules\PrintNode\src\Models\PrintJob;
+use App\Models\ShippingLabel;
 use App\Modules\PrintNode\src\PrintNode;
-use App\Modules\PrintNode\src\Resources\PrintJobResource;
 use App\Services\OrderService;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Auth;
 
 class AddressLabelShippingService extends ShippingServiceAbstract
 {
@@ -20,13 +18,26 @@ class AddressLabelShippingService extends ShippingServiceAbstract
         $order = Order::findOrFail($order_id);
         $pdfString = OrderService::getOrderPdf($order->order_number, 'address_label');
 
-        $printJob = new PrintJob();
-        $printJob->printer_id = Auth::user()->printer_id;
-        $printJob->title = 'address_label_'.$order->order_number.'_by_'.Auth::id();
-        $printJob->pdf = base64_encode($pdfString);
+        $shippingLabel = new ShippingLabel();
+        $shippingLabel->order()->associate($order);
+        $shippingLabel->carrier = '';
+        $shippingLabel->service = 'address_label';
+        $shippingLabel->shipping_number = $order->order_number;
+        $shippingLabel->base64_pdf_labels = base64_encode($pdfString);
+        $shippingLabel->save();
 
-        PrintNode::print($printJob);
+        $this->printShippingLabel($shippingLabel);
 
-        return PrintJobResource::collection([$printJob]);
+        return JsonResource::collection([$shippingLabel]);
+    }
+
+    /**
+     * @param ShippingLabel $shippingLabel
+     */
+    private function printShippingLabel(ShippingLabel $shippingLabel): void
+    {
+        if (isset(auth()->user()->printer_id)) {
+            PrintNode::printRaw($shippingLabel->base64_pdf_labels, auth()->user()->printer_id);
+        }
     }
 }
