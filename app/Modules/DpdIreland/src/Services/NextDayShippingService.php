bc78bd4ec3678d914a3d562dfa22eb61d72357e3
Artur Hanusek <artur@arturhanusek.com>
2022-05-06 11:05:37 +0200

    rebuild ship method  and add tests DpdIreland

--- app/Modules/DpdIreland/src/Services/NextDayShippingService.php
+++ app/Modules/DpdIreland/src/Services/NextDayShippingService.php
@@ -3,8 +3,8 @@
 namespace App\Modules\DpdIreland\src\Services;
 
 use App\Abstracts\ShippingServiceAbstract;
-use App\Http\Resources\PreAdviceResource;
 use App\Models\Order;
+use App\Models\ShippingLabel;
 use App\Modules\DpdIreland\Dpd;
 use App\Modules\DpdIreland\src\Exceptions\AuthorizationException;
 use App\Modules\DpdIreland\src\Responses\PreAdvice;
@@ -12,7 +12,8 @@ use App\Modules\PrintNode\src\Models\PrintJob;
 use App\Modules\PrintNode\src\PrintNode;
 use Exception;
 use GuzzleHttp\Exception\GuzzleException;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
 
 class NextDayShippingService extends ShippingServiceAbstract
 {
@@ -20,18 +21,26 @@ class NextDayShippingService extends ShippingServiceAbstract
      * @throws GuzzleException
      * @throws Exception
      */
-    public function ship(int $order_id): AnonymousResourceCollection
+    public function ship(int $order_id): Collection
     {
         /** @var Order $order */
         $order = Order::findOrFail($order_id);
 
         $preAdvice = $this->createPreAdviceOrFail($order);
 
+        $shippingLabel = new ShippingLabel();
+        $shippingLabel->order_id = $order_id;
+        $shippingLabel->user_id = Auth::id();
+        $shippingLabel->carrier = 'DPD Ireland';
+        $shippingLabel->service = 'next_day';
+        $shippingLabel->shipping_number = $preAdvice->trackingNumber();
+        $shippingLabel->tracking_url = 'https://dpd.ie/tracking?consignmentNumber=' . $preAdvice->trackingNumber();
+        $shippingLabel->base64_pdf_labels = base64_encode($preAdvice->labelImage());
+        $shippingLabel->save();
+
         $this->printOrFail($preAdvice);
 
-        return PreAdviceResource::collection(
-            collect()->add($preAdvice->toArray())
-        );
+        return collect()->add($shippingLabel);
     }
 
 
@@ -64,6 +73,10 @@ class NextDayShippingService extends ShippingServiceAbstract
      */
     public function printOrFail(PreAdvice $preAdvice): int
     {
+        if (! request()->user()) {
+            return -1;
+        }
+
         try {
             $printJob = new PrintJob();
             $printJob->printer_id = request()->user()->printer_id;
--- tests/Feature/Modules/DpdIreland/BasicModuleTest.php
+++ tests/Feature/Modules/DpdIreland/BasicModuleTest.php
@@ -2,17 +2,66 @@
 
 namespace Tests\Feature\Modules\DpdIreland;
 
-use App\User;
+use App\Models\Order;
+use App\Models\ShippingLabel;
+use App\Modules\DpdIreland\Dpd;
+use App\Modules\DpdIreland\src\Responses\PreAdvice;
+use App\Modules\DpdIreland\src\Services\NextDayShippingService;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Mockery;
 use Tests\TestCase;
 
 class BasicModuleTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @test */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $preAdvice = new PreAdvice(/** @lang text */
+            '<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+                <PreadviceResponse>
+                    <Status>OK</Status>
+                    <PreAdviceErrorCode></PreAdviceErrorCode>
+                    <PreAdviceErrorDetails></PreAdviceErrorDetails>
+                    <ReceivedConsignmentsNumber>1</ReceivedConsignmentsNumber>
+                    <Consignment>
+                        <RecordID>1</RecordID>
+                        <TrackingNumber>999999999</TrackingNumber>
+                        <DeliveryDepot>44</DeliveryDepot>
+                    </Consignment>
+                </PreadviceResponse>
+            '
+        );
+
+        Mockery::mock('alias:'.Dpd::class)
+            ->shouldReceive('shipOrder')
+            ->withAnyArgs()
+            ->andReturn($preAdvice);
+    }
+
+    /** @test
+     * @throws GuzzleException
+     */
     public function test_module_basic_functionality()
     {
-        $this->markAsRisky();
+        /** @var Order $order */
+        $order = factory(Order::class)->create();
+
+        $shippingService = new NextDayShippingService();
+        $shippingLabelCollection = $shippingService->ship($order->getKey());
+
+        $this->assertCount(1, $shippingLabelCollection);
+
+        /** @var ShippingLabel $label */
+        $label = $shippingLabelCollection->first();
+
+        $this->assertEquals('DPD Ireland', $label->carrier);
+        $this->assertEquals('next_day', $label->service);
+        $this->assertEquals('999999999', $label->shipping_number);
+        $this->assertEquals($order->id, $label->order_id);
+        $this->assertNotEmpty($label->base64_pdf_labels);
     }
 }
