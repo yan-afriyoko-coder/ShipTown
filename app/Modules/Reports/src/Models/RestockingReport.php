9b0bdb35669d75c631d55ede59129a34c79a2f0c
Artur Hanusek <artur@arturhanusek.com>
2022-05-17 19:19:27 +0200

    add quantity_incoming to inventory model

--- app/Modules/Reports/src/Models/RestockingReport.php
+++ app/Modules/Reports/src/Models/RestockingReport.php
@@ -16,12 +16,13 @@ class RestockingReport extends Report
            'warehouse_code'                     => 'inventory.warehouse_code',
            'product_sku'                        => 'product.sku',
            'product_name'                       => 'product.name',
-           'quantity_available'                 => 'inventory.quantity_available',
-           'restock_level'                      => 'inventory.restock_level',
            'reorder_point'                      => 'inventory.reorder_point',
+           'restock_level'                      => 'inventory.restock_level',
+           'quantity_available'                 => 'inventory.quantity_available',
+           'quantity_incoming'                  => 'inventory.quantity_incoming',
            'quantity_required'                  => 'inventory.quantity_required',
-           'inventory_source_warehouse_code'    => 'inventory_source.warehouse_code',
            'warehouse_quantity'                 => 'inventory_source.quantity_available',
+           'inventory_source_warehouse_code'    => 'inventory_source.warehouse_code',
         ];
 
         $this->baseQuery = Inventory::query()
@@ -35,6 +36,7 @@ class RestockingReport extends Report
             'reorder_point'      => 'float',
             'quantity_required'  => 'float',
             'quantity_available' => 'float',
+            'quantity_incoming'  => 'float',
             'warehouse_quantity' => 'float',
         ];
     }
