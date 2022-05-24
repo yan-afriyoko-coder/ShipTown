e40303bce9d2807b2da4646e37e7077c6b3cd799
Artur Hanusek <artur@arturhanusek.com>
2022-05-24 12:46:26 +0200

    add has tags filter

--- app/Modules/Reports/src/Models/RestockingReport.php
+++ app/Modules/Reports/src/Models/RestockingReport.php
@@ -3,6 +3,9 @@
 namespace App\Modules\Reports\src\Models;
 
 use App\Models\Inventory;
+use Illuminate\Database\Query\Builder;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\Filters\FiltersCallback;
 
 class RestockingReport extends Report
 {
@@ -12,24 +15,24 @@ class RestockingReport extends Report
 
         $this->report_name = 'Restocking Report';
 
-        $this->fields = [
-           'warehouse_code'                     => 'inventory.warehouse_code',
-           'product_sku'                        => 'product.sku',
-           'product_name'                       => 'product.name',
-           'reorder_point'                      => 'inventory.reorder_point',
-           'restock_level'                      => 'inventory.restock_level',
-           'quantity_available'                 => 'inventory.quantity_available',
-           'quantity_incoming'                  => 'inventory.quantity_incoming',
-           'quantity_required'                  => 'inventory.quantity_required',
-           'warehouse_quantity'                 => 'inventory_source.quantity_available',
-           'inventory_source_warehouse_code'    => 'inventory_source.warehouse_code',
-        ];
-
         $this->baseQuery = Inventory::query()
             ->leftJoin('inventory as inventory_source', function ($join) {
                 $join->on('inventory_source.product_id', '=', 'inventory.product_id');
             })
             ->leftJoin('products as product', 'inventory.product_id', '=', 'product.id');
+
+        $this->fields = [
+            'warehouse_code'                     => 'inventory.warehouse_code',
+            'product_sku'                        => 'product.sku',
+            'product_name'                       => 'product.name',
+            'reorder_point'                      => 'inventory.reorder_point',
+            'restock_level'                      => 'inventory.restock_level',
+            'quantity_available'                 => 'inventory.quantity_available',
+            'quantity_incoming'                  => 'inventory.quantity_incoming',
+            'quantity_required'                  => 'inventory.quantity_required',
+            'warehouse_quantity'                 => 'inventory_source.quantity_available',
+            'inventory_source_warehouse_code'    => 'inventory_source.warehouse_code',
+        ];
 
         $this->casts = [
             'restock_level'      => 'float',
@@ -39,5 +42,13 @@ class RestockingReport extends Report
             'quantity_incoming'  => 'float',
             'warehouse_quantity' => 'float',
         ];
+
+        $this->addFilter(
+            AllowedFilter::callback('has_tags', function ($query, $value) {
+                $query->whereHas('product', function ($query) use ($value) {
+                    $query->withAllTags($value);
+                });
+            })
+        );
     }
 }
