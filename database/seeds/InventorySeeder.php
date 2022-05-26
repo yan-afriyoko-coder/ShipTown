202e6ef32ecb5699ec0cbff3cb9a7779e95fba45
Artur Hanusek <artur@arturhanusek.com>
2022-05-26 15:26:06 +0200

    fix inventory seeder to ignore warehouse 999

--- database/seeds/InventorySeeder.php
+++ database/seeds/InventorySeeder.php
@@ -4,6 +4,7 @@ use App\Models\Inventory;
 use App\Models\Product;
 use App\Models\Warehouse;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class InventorySeeder extends Seeder
 {
@@ -14,26 +15,29 @@ class InventorySeeder extends Seeder
      */
     public function run()
     {
-        Warehouse::all()->each(function (Warehouse $warehouse) {
-            Product::query()
-                ->get()
-                ->each(function (Product $product) use ($warehouse) {
-                    $restock_level = rand(0, 100);
+        Warehouse::query()
+            ->where('code', '!=', DB::raw('999'))
+            ->get()
+            ->each(function (Warehouse $warehouse) {
+                Product::query()
+                    ->get()
+                    ->each(function (Product $product) use ($warehouse) {
+                        $restock_level = rand(0, 100);
 
-                    $locations = 'ABCDEFGHIJKLMNOPRSTUWXYZ';
-                    $random_location = $locations[rand(0, strlen($locations) -1)] . rand(10, 50);
+                        $locations = 'ABCDEFGHIJKLMNOPRSTUWXYZ';
+                        $random_location = $locations[rand(0, strlen($locations) -1)] . rand(10, 50);
 
-                    Inventory::where([
-                        'product_id'  => $product->id,
-                        'warehouse_id' => $warehouse->getKey(),
-                    ])->update([
-                        'quantity'          => rand(0, 100),
-                        'quantity_incoming' => rand(0, 100),
-                        'restock_level'     => $restock_level,
-                        'reorder_point'     => rand(0, $restock_level),
-                        'shelve_location'   => $random_location
-                    ]);
-                });
-        });
+                        Inventory::where([
+                            'product_id'  => $product->id,
+                            'warehouse_id' => $warehouse->getKey(),
+                        ])->update([
+                            'quantity'          => rand(0, 100),
+                            'quantity_incoming' => rand(0, 100),
+                            'restock_level'     => $restock_level,
+                            'reorder_point'     => rand(0, $restock_level),
+                            'shelve_location'   => $random_location
+                        ]);
+                    });
+            });
     }
 }
