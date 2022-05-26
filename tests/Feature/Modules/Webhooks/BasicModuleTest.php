0ce866248e1d63dc8a6ee88f40e359b75d4d83bd
Artur Hanusek <artur@arturhanusek.com>
2022-05-26 22:50:10 +0200

    fill in tests

--- tests/Feature/Modules/Webhooks/BasicModuleTest.php
+++ tests/Feature/Modules/Webhooks/BasicModuleTest.php
@@ -2,17 +2,16 @@
 
 namespace Tests\Feature\Modules\Webhooks;
 
-use App\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Modules\Webhooks\src\WebhooksServiceProviderBase;
 use Tests\TestCase;
 
 class BasicModuleTest extends TestCase
 {
-    use RefreshDatabase;
-
     /** @test */
     public function test_module_basic_functionality()
     {
-        $this->markAsRisky();
+        WebhooksServiceProviderBase::enableModule();
+
+        $this->assertTrue(true, 'Most basic test... to be continued');
     }
 }
