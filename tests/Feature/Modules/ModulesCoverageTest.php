423cb03ce1974222ad95286a0beda068b9fb32d4
Artur Hanusek <artur@arturhanusek.com>
2022-05-26 23:35:16 +0200

    refacor test

--- tests/Feature/Modules/ModulesCoverageTest.php
+++ tests/Feature/Modules/ModulesCoverageTest.php
@@ -16,19 +16,18 @@ class ModulesCoverageTest extends TestCase
      */
     public function test_if_all_modules_have_test_file()
     {
-        $modulesList = File::directories('app/Modules');
-
-        $expectedTestsList = collect($modulesList)->map(function ($moduleDirectory) {
-            $testPath = Str::replaceArray('app/Modules/', ['/tests/Feature/Modules/'], $moduleDirectory);
-
-            return app()->basePath(). $testPath . '/BasicModuleTest.php';
-        });
-
-        $expectedTestsList->each(function ($testName) {
-            $this->assertFileExists(
-                $testName,
-                $testName . ' test is missing. Run "php artisan app:generate-modules-tests"'
-            );
-        });
+        collect(File::directories('app/Modules'))
+            ->map(function ($moduleDirectory) {
+                $testFileName = app()->basePath();
+                $testFileName .= Str::replaceArray('app/Modules/', ['/tests/Feature/Modules/'], $moduleDirectory);
+                $testFileName .= '/BasicModuleTest.php';
+                return $testFileName;
+            })
+            ->each(function ($fileName) {
+                $this->assertFileExists(
+                    $fileName,
+                    $fileName . ' is missing. Run "php artisan app:generate-modules-tests"'
+                );
+            });
     }
 }
