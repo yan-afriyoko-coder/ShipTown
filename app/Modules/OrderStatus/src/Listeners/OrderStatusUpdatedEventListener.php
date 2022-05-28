774908baf6b249f8c9637a77ea846f0b9827bcb1
Artur Hanusek <artur@arturhanusek.com>
2022-05-28 17:45:52 +0200

    fix OrderStatus module to update is_active flag when updated

--- app/Modules/OrderStatus/src/Listeners/OrderStatusUpdatedEventListener.php
+++ app/Modules/OrderStatus/src/Listeners/OrderStatusUpdatedEventListener.php
@@ -3,6 +3,7 @@
 namespace App\Modules\OrderStatus\src\Listeners;
 
 use App\Events\OrderStatus\OrderStatusUpdatedEvent;
+use App\Modules\OrderStatus\src\Jobs\UpdateOrdersIsActiveJob;
 use App\Modules\OrderStatus\src\Jobs\UpdateOrdersIsOnHoldJob;
 
 class OrderStatusUpdatedEventListener
@@ -10,7 +11,7 @@ class OrderStatusUpdatedEventListener
     public function handle(OrderStatusUpdatedEvent $event)
     {
         if ($event->orderStatus->isAttributeChanged('order_active')) {
-            UpdateOrdersIsOnHoldJob::dispatch($event->orderStatus);
+            UpdateOrdersIsActiveJob::dispatch($event->orderStatus);
         }
 
         if ($event->orderStatus->isAttributeChanged('order_on_hold')) {
--- tests/Feature/Modules/OrderStatus/BasicModuleTest.php
+++ tests/Feature/Modules/OrderStatus/BasicModuleTest.php
@@ -2,13 +2,43 @@
 
 namespace Tests\Feature\Modules\OrderStatus;
 
+use App\Models\Order;
+use App\Models\OrderStatus;
 use App\Modules\OrderStatus\src\OrderStatusServiceProvider;
 use Tests\TestCase;
 
 class BasicModuleTest extends TestCase
 {
+    public function test_if_order_is_active_updates()
+    {
+        /** @var OrderStatus $orderStatus */
+        $orderStatus = factory(OrderStatus::class)->create(['order_active' => true]);
+
+        factory(Order::class)->create(['status_code' => $orderStatus->code]);
+
+        $this->assertDatabaseHas('orders', ['is_active' => true]);
+
+        $orderStatus->update(['order_active' => false]);
+
+        $this->assertDatabaseHas('orders', ['is_active' => false]);
+    }
+
+    public function test_if_order_on_hold_updates()
+    {
+        /** @var OrderStatus $orderStatus */
+        $orderStatus = factory(OrderStatus::class)->create(['order_on_hold' => true]);
+
+        factory(Order::class)->create(['status_code' => $orderStatus->code, 'is_on_hold' => true]);
+
+        $this->assertDatabaseHas('orders', ['is_on_hold' => true]);
+
+        $orderStatus->update(['order_on_hold' => false]);
+
+        $this->assertDatabaseHas('orders', ['is_on_hold' => false]);
+    }
+
     /** @test */
-    public function test_module_basic_functionality()
+    public function test_if_can_enable()
     {
         OrderStatusServiceProvider::enableModule();
 
