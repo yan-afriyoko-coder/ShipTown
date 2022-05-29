9386b9ef560052e5f9c5fc2151ccb1affc95419b
Artur Hanusek <artur@arturhanusek.com>
2022-05-30 00:52:11 +0200

    fix listener

--- app/Modules/OrderTotals/src/Listeners/HourlyEventListener.php
+++ app/Modules/OrderTotals/src/Listeners/HourlyEventListener.php
@@ -2,11 +2,12 @@
 
 namespace App\Modules\OrderTotals\src\Listeners;
 
+use App\Events\HourlyEvent;
 use App\Modules\OrderTotals\src\Jobs\EnsureCorrectTotalsJob;
 
 class HourlyEventListener
 {
-    public function handle(HourlyEventListener $event)
+    public function handle(HourlyEvent $event)
     {
         EnsureCorrectTotalsJob::dispatch();
     }
