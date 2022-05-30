320e1732a3303aeb75f3ffcd25864ed34f28a921
Artur Hanusek <artur@arturhanusek.com>
2022-05-30 17:37:39 +0200

    refactor job

--- app/Modules/OrderTotals/src/Jobs/EnsureCorrectTotalsJob.php
+++ app/Modules/OrderTotals/src/Jobs/EnsureCorrectTotalsJob.php
@@ -46,30 +46,38 @@ class EnsureCorrectTotalsJob implements ShouldQueue
 
     public function fixMissingOrWrongTotals()
     {
-//        do {
-            $records = $this->missingOrWrongTotalsQuery()
-                ->limit(10000)
-                ->get();
+        DB::statement('
+            UPDATE orders_products_totals
 
-            $records->each(function ($record) {
-                OrderProductTotal::query()
-                    ->where(['order_id' => $record->order_id])
-                    ->update([
-                        'count' => $record->count_expected ?? 0,
-                        'quantity_ordered' => $record->quantity_ordered_expected ?? 0,
-                        'quantity_split' => $record->quantity_split_expected ?? 0,
-                        'quantity_picked' => $record->quantity_picked_expected ?? 0,
-                        'quantity_skipped_picking' => $record->quantity_skipped_picking_expected ?? 0,
-                        'quantity_not_picked' => $record->quantity_not_picked_expected ?? 0,
-                        'quantity_shipped' => $record->quantity_shipped_expected ?? 0,
-                        'quantity_to_pick' => $record->quantity_to_pick_expected ?? 0,
-                        'quantity_to_ship' => $record->quantity_to_ship_expected ?? 0,
-                        'max_updated_at' => $record->max_updated_at_expected ?? 0,
-                    ]);
+            LEFT JOIN '.$this->recalculations_temp_table_name.' AS recalculations
+                ON recalculations.order_id = orders_products_totals.order_id
 
-                    Log::warning('EnsureCorrectTotalsJob - order updated', ['order_id' => $record->order_id]);
-            });
-//        } while ($records->isNotEmpty());
+            SET
+                orders_products_totals.count                    = recalculations.count_expected,
+                orders_products_totals.quantity_ordered         = recalculations.quantity_ordered_expected,
+                orders_products_totals.quantity_split           = recalculations.quantity_split_expected,
+                orders_products_totals.quantity_picked          = recalculations.quantity_picked_expected,
+                orders_products_totals.quantity_skipped_picking = recalculations.quantity_skipped_picking_expected,
+                orders_products_totals.quantity_not_picked      = recalculations.quantity_not_picked_expected,
+                orders_products_totals.quantity_shipped         = recalculations.quantity_shipped_expected,
+                orders_products_totals.quantity_to_pick         = recalculations.quantity_to_pick_expected,
+                orders_products_totals.quantity_to_ship         = recalculations.quantity_to_ship_expected,
+                orders_products_totals.max_updated_at           = recalculations.max_updated_at_expected
+
+            WHERE
+              (
+                   (orders_products_totals.count                    != recalculations.count_expected                   )
+                OR (orders_products_totals.quantity_ordered         != recalculations.quantity_ordered_expected        )
+                OR (orders_products_totals.quantity_split           != recalculations.quantity_split_expected          )
+                OR (orders_products_totals.quantity_picked          != recalculations.quantity_picked_expected         )
+                OR (orders_products_totals.quantity_skipped_picking != recalculations.quantity_skipped_picking_expected)
+                OR (orders_products_totals.quantity_not_picked      != recalculations.quantity_not_picked_expected     )
+                OR (orders_products_totals.quantity_shipped         != recalculations.quantity_shipped_expected        )
+                OR (orders_products_totals.quantity_to_pick         != recalculations.quantity_to_pick_expected        )
+                OR (orders_products_totals.quantity_to_ship         != recalculations.quantity_to_ship_expected        )
+                OR (orders_products_totals.max_updated_at           != recalculations.max_updated_at_expected          )
+              )
+            ');
     }
 
     private function prepareTempTable()
@@ -92,58 +100,5 @@ class EnsureCorrectTotalsJob implements ShouldQueue
             ->groupBy('order_id');
 
         TemporaryTable::create($this->recalculations_temp_table_name, $subQuery);
-    }
-
-    /**
-     * @return Builder
-     */
-    private function missingOrWrongTotalsQuery(): Builder
-    {
-        return DB::table('orders_products_totals')
-            ->selectRaw('
-                orders_products_totals.order_id,
-
-                orders_products_totals.count,
-                orders_products_totals.quantity_ordered,
-                orders_products_totals.quantity_split,
-                orders_products_totals.quantity_picked,
-                orders_products_totals.quantity_skipped_picking,
-                orders_products_totals.quantity_not_picked,
-                orders_products_totals.quantity_shipped,
-                orders_products_totals.quantity_to_pick,
-                orders_products_totals.quantity_to_ship,
-                orders_products_totals.max_updated_at,
-
-                recalculations.count_expected,
-                recalculations.quantity_ordered_expected,
-                recalculations.quantity_split_expected,
-                recalculations.quantity_picked_expected,
-                recalculations.quantity_skipped_picking_expected,
-                recalculations.quantity_not_picked_expected,
-                recalculations.quantity_shipped_expected,
-                recalculations.quantity_to_pick_expected,
-                recalculations.quantity_to_ship_expected,
-                recalculations.max_updated_at_expected
-            ')
-            ->leftJoin(
-                $this->recalculations_temp_table_name .' as recalculations',
-                'recalculations.order_id',
-                '=',
-                'orders_products_totals.order_id'
-            )
-            ->whereRaw('
-              (
-                   (orders_products_totals.count                    != recalculations.count_expected                   )
-                OR (orders_products_totals.quantity_ordered         != recalculations.quantity_ordered_expected        )
-                OR (orders_products_totals.quantity_split           != recalculations.quantity_split_expected          )
-                OR (orders_products_totals.quantity_picked          != recalculations.quantity_picked_expected         )
-                OR (orders_products_totals.quantity_skipped_picking != recalculations.quantity_skipped_picking_expected)
-                OR (orders_products_totals.quantity_not_picked      != recalculations.quantity_not_picked_expected     )
-                OR (orders_products_totals.quantity_shipped         != recalculations.quantity_shipped_expected        )
-                OR (orders_products_totals.quantity_to_pick         != recalculations.quantity_to_pick_expected        )
-                OR (orders_products_totals.quantity_to_ship         != recalculations.quantity_to_ship_expected        )
-                OR (orders_products_totals.max_updated_at           != recalculations.max_updated_at_expected          )
-              )
-            ');
     }
 }
---
+++ app/Modules/OrderTotals/src/Services/OrderTotalsService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Modules\OrderTotals\src\Services;
+
+class OrderTotalsService
+{
+
+}
