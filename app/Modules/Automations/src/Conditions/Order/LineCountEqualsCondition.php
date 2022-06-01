803c48c9bfbf0faf137e22479164858ebb5b98ce
Artur Hanusek <artur@arturhanusek.com>
2022-06-01 13:48:31 +0200

    remove is valid

--- app/Modules/Automations/src/Conditions/Order/LineCountEqualsCondition.php
+++ app/Modules/Automations/src/Conditions/Order/LineCountEqualsCondition.php
@@ -18,26 +18,4 @@ class LineCountEqualsCondition extends BaseOrderConditionAbstract
             $query->where('count', '=', $expected_value);
         });
     }
-
-    /**
-     * @param string $condition_value
-     * @return bool
-     */
-    public function isValid(string $condition_value): bool
-    {
-        $numericValue = intval($condition_value);
-
-        $result = is_numeric($condition_value)
-            && $this->event->order->orderTotals->product_line_count === $numericValue;
-
-        Log::debug('Automation condition', [
-            'order_number' => $this->event->order->order_number,
-            'result' => $result,
-            'class' => class_basename(self::class),
-            'expected' => $numericValue,
-            'actual' => $this->event->order->orderTotals->product_line_count,
-        ]);
-
-        return $result;
-    }
 }
