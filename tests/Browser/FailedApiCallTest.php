f566902dee79079ccbfa7cc7837a4eaebeff7e29
Artur Hanusek <artur@arturhanusek.com>
2022-06-03 14:47:29 +0200

    add Products page basic test

---
+++ tests/Browser/FailedApiCallTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\Product;
+use App\User;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Throwable;
+
+class FailedApiCallTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @throws Throwable
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            /** @var Product $product */
+            $product = factory(Product::class)->create();
+
+            $user = factory(User::class)->create();
+
+            $browser->loginAs($user)
+                ->visit('/products')
+                ->screenshot('test')
+                ->assertDontSee('No products found.')
+                ->assertSee($product->name);
+        });
+    }
+
+    /**
+     * A basic browser test example.
+     *
+     * @throws Throwable
+     *
+     * @return void
+     */
+    public function test_products_empy()
+    {
+        Product::query()->forceDelete();
+
+        $user = factory(User::class)->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                ->visit('/products')
+                ->screenshot('test')
+                ->assertSee('No products found.');
+        });
+    }
+}
