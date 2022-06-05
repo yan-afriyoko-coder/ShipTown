4d6e96efd70a0aa864db432958d4f3d608b0c51e
Artur Hanusek <artur@arturhanusek.com>
2022-06-05 14:04:45 +0200

    extend from new classes

--- app/Modules/Automations/src/Conditions/IsFullyPickedCondition.php
+++ app/Modules/Automations/src/Conditions/IsFullyPickedCondition.php
@@ -2,25 +2,10 @@
 
 namespace App\Modules\Automations\src\Conditions;
 
-use App\Modules\Automations\src\Abstracts\BaseOrderConditionAbstract;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\DB;
-
 /**
  *
  */
-class IsFullyPickedCondition extends BaseOrderConditionAbstract
+class IsFullyPickedCondition extends Order\IsFullyPickedCondition
 {
-    public static function addQueryScope(Builder $query, $expected_value): Builder
-    {
-        if ($expected_value === '') {
-            $expected_value = 'true';
-        }
 
-        $expected_value = filter_var($expected_value, FILTER_VALIDATE_BOOL);
-
-        return $query->whereHas('orderProductsTotals', function ($query) use ($expected_value) {
-            $query->where(DB::raw('(orders_products_totals.quantity_to_pick = 0)'), '=', $expected_value);
-        });
-    }
 }
--- app/Modules/Automations/src/Conditions/OrderNumberEqualsCondition.php
+++ app/Modules/Automations/src/Conditions/OrderNumberEqualsCondition.php
@@ -2,21 +2,10 @@
 
 namespace App\Modules\Automations\src\Conditions;
 
-use App\Events\Order\ActiveOrderCheckEvent;
-use App\Events\Order\OrderCreatedEvent;
-use App\Events\Order\OrderUpdatedEvent;
-use App\Modules\Automations\src\Abstracts\BaseOrderConditionAbstract;
-use Illuminate\Database\Eloquent\Builder;
-
 /**
  *
  */
-class OrderNumberEqualsCondition extends BaseOrderConditionAbstract
+class OrderNumberEqualsCondition extends Order\OrderNumberEqualsCondition
 {
-    public static function addQueryScope(Builder $query, $expected_value): Builder
-    {
-        $query->where(['order_number' => $expected_value]);
 
-        return $query;
-    }
 }
