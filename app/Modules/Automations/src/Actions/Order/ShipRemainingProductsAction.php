7bb2ccfaeca08cc403fab3ed19153dfd1e78e36c
Artur Hanusek <artur@arturhanusek.com>
2022-07-20 19:12:55 +0200

    add warehouse_id

--- app/Modules/Automations/src/Actions/Order/ShipRemainingProductsAction.php
+++ app/Modules/Automations/src/Actions/Order/ShipRemainingProductsAction.php
@@ -4,6 +4,7 @@ namespace App\Modules\Automations\src\Actions\Order;
 
 use App\Models\OrderProduct;
 use App\Models\OrderProductShipment;
+use App\Models\Warehouse;
 use App\Modules\Automations\src\Abstracts\BaseOrderActionAbstract;
 use Illuminate\Support\Facades\Log;
 
@@ -15,6 +16,9 @@ class ShipRemainingProductsAction extends BaseOrderActionAbstract
      */
     public function handle(string $options = ''): bool
     {
+        /** @var Warehouse $warehouse */
+        $warehouse = Warehouse::query()->where(['code' => $options])->first();
+
         Log::debug('Automation Action', [
             'order_number' => $this->order->order_number,
             'class' => class_basename(self::class),
@@ -23,13 +27,15 @@ class ShipRemainingProductsAction extends BaseOrderActionAbstract
 
         $order = $this->order->refresh();
 
-        $order->orderProducts()->each(function (OrderProduct $orderProduct) {
+        $order->orderProducts()->each(function (OrderProduct $orderProduct) use ($warehouse) {
 
             $orderProductShipment = new OrderProductShipment();
-            $orderProductShipment->order_id = $orderProduct->order_id;
-            $orderProductShipment->product_id = $orderProduct->product_id;
+            $orderProductShipment->sku_shipped      = $orderProduct->sku_ordered;
+            $orderProductShipment->order_id         = $orderProduct->order_id;
+            $orderProductShipment->product_id       = $orderProduct->product_id;
             $orderProductShipment->order_product_id = $orderProduct->getKey();
             $orderProductShipment->quantity_shipped = $orderProduct->quantity_to_ship;
+            $orderProductShipment->warehouse_id     = $warehouse->id;
             $orderProductShipment->save();
 
             $orderProduct->quantity_shipped += $orderProduct->quantity_to_ship;
