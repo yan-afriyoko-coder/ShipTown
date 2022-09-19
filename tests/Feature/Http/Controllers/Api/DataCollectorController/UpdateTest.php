70c3f6868c0bb0c1420dd8afa4ec7ee2b7bc0600
Artur Hanusek <arthur@youritsolutions.ie>
2022-09-19 11:39:36 +0200

    create DataCollectionController update test

---
+++ tests/Feature/Http/Controllers/Api/DataCollectorController/UpdateTest.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api\DataCollectorController;
+
+use App\Models\DataCollection;
+use App\Models\DataCollectionRecord;
+use App\Models\Inventory;
+use App\Models\Product;
+use App\Models\Warehouse;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UpdateTest extends TestCase
+{
+    /** @test */
+    public function test_transfer_in_scanned_action_call_returns_ok()
+    {
+        $randomQuantity = 10;
+
+        $user = factory(User::class)->create();
+
+        /** @var Product $product */
+        $product = factory(Product::class)->create();
+
+        /** @var Warehouse $warehouse */
+        $warehouse = factory(Warehouse::class)->create();
+
+        /** @var Inventory $inventory */
+        $inventory = Inventory::firstOrCreate([
+            'product_id' => $product->id,
+            'warehouse_id' => $warehouse->id,
+        ], []);
+
+        /** @var DataCollection $dataCollector */
+        $dataCollector = factory(DataCollection::class)->create([
+            'warehouse_id' => $warehouse->id,
+            'name' => 'Test',
+        ]);
+
+        /** @var DataCollectionRecord $dataCollectorRecord */
+        $dataCollectorRecord = factory(DataCollectionRecord::class)->create([
+            'data_collection_id' => $dataCollector->id,
+            'inventory_id' => $inventory->id,
+            'product_id' => $inventory->product_id,
+            'quantity_scanned' => $randomQuantity,
+        ]);
+
+        $response = $this->actingAs($user, 'api')->putJson(route('data-collector.update', [
+            'data_collector' => $dataCollector->getKey(),
+        ]), [
+            'action' => 'transfer_in_scanned',
+        ]);
+
+        ray($response->json());
+
+        $response->assertOk();
+
+        $this->assertEquals($randomQuantity, $inventory->refresh()->quantity);
+
+        $this->assertEquals(0, $dataCollectorRecord->refresh()->quantity_scanned);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'id'
+            ],
+        ]);
+    }
+
+    /** @test */
+    public function test_transfer_out_scanned_action_call_returns_ok()
+    {
+        $randomQuantity = 10;
+
+        $user = factory(User::class)->create();
+
+        /** @var Product $product */
+        $product = factory(Product::class)->create();
+
+        /** @var Warehouse $warehouse */
+        $warehouse = factory(Warehouse::class)->create();
+
+        /** @var Inventory $inventory */
+        $inventory = Inventory::firstOrCreate([
+            'product_id' => $product->id,
+            'warehouse_id' => $warehouse->id,
+        ], []);
+
+        /** @var DataCollection $dataCollector */
+        $dataCollector = factory(DataCollection::class)->create([
+            'warehouse_id' => $warehouse->id,
+            'name' => 'Test',
+        ]);
+
+        /** @var DataCollectionRecord $dataCollectorRecord */
+        $dataCollectorRecord = factory(DataCollectionRecord::class)->create([
+            'data_collection_id' => $dataCollector->id,
+            'inventory_id' => $inventory->id,
+            'product_id' => $inventory->product_id,
+            'quantity_scanned' => $randomQuantity,
+        ]);
+
+        $response = $this->actingAs($user, 'api')->putJson(route('data-collector.update', [
+            'data_collector' => $dataCollector->getKey(),
+        ]), [
+            'action' => 'transfer_out_scanned',
+        ]);
+
+        ray($response->json());
+
+        $response->assertOk();
+
+        $this->assertEquals($randomQuantity * -1, $inventory->refresh()->quantity);
+
+        $this->assertEquals(0, $dataCollectorRecord->refresh()->quantity_scanned);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'id'
+            ],
+        ]);
+    }
+}
