feff82a4841751c9dbfb9f37d545f02cd4a76877
Artur Hanusek <arthur@youritsolutions.ie>
2022-09-21 13:33:51 +0200

    create test

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::apiResource('csv-import', 'Api\CsvImportController')->only(['store']);
-Route::apiResource('csv-import/data-collections', 'Api\CsvImport\DataCollectionsImportController')->only(['store']);
+Route::apiResource('csv-import/data-collections', 'Api\CsvImport\DataCollectionsImportController')->names('csv-import-data-collections')->only(['store']);
 Route::apiResource('stocktake-suggestions', 'Api\StocktakeSuggestionController')->only(['index']);
 
 Route::put('print/order/{order_number}/{view}', 'Api\PrintOrderController@store');
