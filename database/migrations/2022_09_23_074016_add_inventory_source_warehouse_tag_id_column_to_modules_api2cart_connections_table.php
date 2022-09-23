fd2b4e700011bb33a863c9592785ff195163073f
Artur Hanusek <arthur@youritsolutions.ie>
2022-09-23 10:15:35 +0200

    create discrepencies views

---
+++ database/migrations/2022_09_23_074016_add_inventory_source_warehouse_tag_id_column_to_modules_api2cart_connections_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Modules\Api2cart\src\Models\Api2cartConnection;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Tags\Tag;
+
+class AddInventorySourceWarehouseTagIdColumnToModulesApi2cartConnectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('modules_api2cart_connections', function (Blueprint $table) {
+            $table->foreignId('inventory_source_warehouse_tag_id')->nullable()->after('inventory_source_warehouse_tag');
+        });
+
+        Api2cartConnection::all()
+            ->each(function (Api2cartConnection $connection) {
+                $tag = Tag::findFromString('magento_stock')->first('id');
+
+                $connection->inventory_source_warehouse_tag_id = $tag->getKey();
+                $connection->save();
+            });
+    }
+}
