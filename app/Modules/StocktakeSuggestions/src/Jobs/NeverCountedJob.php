50f19513a23f02f9c2815c9d4ca656c4e32e4ea3
Artur Hanusek <arthur@youritsolutions.ie>
2022-09-25 00:09:10 +0200

    update suggestion script

--- app/Modules/StocktakeSuggestions/src/Jobs/NeverCountedJob.php
+++ app/Modules/StocktakeSuggestions/src/Jobs/NeverCountedJob.php
@@ -26,7 +26,7 @@ class NeverCountedJob implements ShouldQueue
         StocktakeSuggestion::query()->where(['reason' => $reason])->delete();
 
         $inventory = Inventory::query()
-            ->where('quantity', '>', 0)
+            ->where('quantity', '!=', 0)
             ->whereNull('last_counted_at')
             ->get(['id'])
             ->collect();
