d6a05899adb044d707855142f36193bb43d2fb69
Artur Hanusek <arthur@youritsolutions.ie>
2022-09-25 15:04:17 +0200

    add unique index to api2cart connections table

---
+++ database/migrations/2022_09_25_125940_create_api2cart_connction_product_id_unique_index.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApi2cartConnctionProductIdUniqueIndex extends Migration
+{
+    public function up()
+    {
+        DB::statement('
+            DELETE FROM modules_api2cart_product_links WHERE api2cart_product_id IN (
+            SELECT * FROM (SELECT product_link.api2cart_product_id
+                FROM modules_api2cart_product_links as product_link
+                GROUP BY api2cart_product_id
+                HAVING count(*) > 1) as tbl
+            )
+        ');
+
+        Schema::table('modules_api2cart_connections', function (Blueprint $table) {
+            $table->unique(['connection_id', 'product_id']);
+        });
+    }
+}
