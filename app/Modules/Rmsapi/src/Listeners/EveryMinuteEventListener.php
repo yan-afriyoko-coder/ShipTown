0e0b0a3158f5932a3b7296bdfd1b66773fd5a5d1
Artur Hanusek <arthur@youritsolutions.ie>
2022-10-25 14:35:05 +0200

    refactor module

--- app/Modules/Rmsapi/src/Listeners/EveryMinuteEventListener.php
+++ app/Modules/Rmsapi/src/Listeners/EveryMinuteEventListener.php
@@ -2,14 +2,25 @@
 
 namespace App\Modules\Rmsapi\src\Listeners;
 
-use App\Modules\Rmsapi\src\Jobs\DispatchImportJobs;
+use App\Modules\Rmsapi\src\Jobs\ImportProductsJob;
+use App\Modules\Rmsapi\src\Jobs\ImportShippingsJob;
 use App\Modules\Rmsapi\src\Jobs\ProcessImportedProductRecordsJob;
+use App\Modules\Rmsapi\src\Models\RmsapiConnection;
+use Illuminate\Support\Facades\Log;
 
 class EveryMinuteEventListener
 {
     public function handle()
     {
-        DispatchImportJobs::dispatch();
+        foreach (RmsapiConnection::all() as $rmsapiConnection) {
+            ImportProductsJob::dispatch($rmsapiConnection->id);
+            ImportShippingsJob::dispatch($rmsapiConnection->id);
+
+            Log::debug('RMSAPI Sync jobs dispatched', [
+                'warehouse_code' => $rmsapiConnection->location_id,
+                'connection_id' => $rmsapiConnection->id
+            ]);
+        }
 
         ProcessImportedProductRecordsJob::dispatch();
     }
