3cd90fd7d4ce99ac4759c254adcd1f49598dd0b9
Artur Hanusek <arthur@youritsolutions.ie>
2022-10-29 17:42:35 +0200

    create modules_magento2api_connections table

--- database/migrations/2022_10_29_150728_add_columns_to_modules_magento2api_product_table.php
+++ database/migrations/2022_10_29_150728_add_columns_to_modules_magento2api_product_table.php
@@ -14,8 +14,8 @@ class AddColumnsToModulesMagento2apiProductTable extends Migration
     public function up()
     {
         Schema::table('modules_magento2api_products', function (Blueprint $table) {
-            $table->boolean('is_in_stock')->nullable();
-            $table->decimal('quantity', 20)->nullable();
+            $table->boolean('is_in_stock')->nullable()->after('product_id');
+            $table->decimal('quantity', 20)->nullable()->after('quantity');
         });
     }
 }
