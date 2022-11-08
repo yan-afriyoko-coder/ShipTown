987dfb40797664beeae9d25c58b3f1ed2df9d868
Artur Hanusek <arthur@youritsolutions.ie>
2022-11-08 18:56:31 +0100

    add test

--- tests/Feature/Modules/InventoryQuantityIncoming/BasicModuleTest.php
+++ tests/Feature/Modules/InventoryQuantityIncoming/BasicModuleTest.php
@@ -2,17 +2,46 @@
 
 namespace Tests\Feature\Modules\InventoryQuantityIncoming;
 
-use App\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\DataCollection;
+use App\Models\DataCollectionTransferIn;
+use App\Models\Inventory;
+use App\Models\Product;
+use App\Models\Warehouse;
+use App\Modules\InventoryQuantityIncoming\src\InventoryQuantityIncomingServiceProvider;
 use Tests\TestCase;
 
 class BasicModuleTest extends TestCase
 {
-    use RefreshDatabase;
-
-    /** @test */
-    public function test_module_basic_functionality()
+    public function test_basic_functionality()
     {
-        $this->markTestIncomplete('This test has not been implemented yet.');
+        InventoryQuantityIncomingServiceProvider::enableModule();
+
+        $warehouse = factory(Warehouse::class)->create();
+        $product = factory(Product::class)->create();
+
+        $inventory = Inventory::query()->where([
+            'warehouse_id' => $warehouse->getKey(),
+            'product_id' => $product->getKey(),
+        ])->first();
+
+        $inventory->update([
+            'quantity' => 10,
+            'restock_level' => 0,
+            'reorder_point' => 0,
+        ]);
+
+        /** @var DataCollection $dataCollection */
+        $dataCollection = factory(DataCollection::class)->create([
+            'type' => DataCollectionTransferIn::class,
+            'name' => 'Test',
+            'warehouse_id' => $warehouse->getKey(),
+        ]);
+
+        $dataCollection->records()->create([
+            'product_id' => $inventory->product_id,
+            'quantity_requested' => 10,
+        ]);
+
+        $this->assertEquals(10, $inventory->fresh()->quantity_incoming);
     }
 }
