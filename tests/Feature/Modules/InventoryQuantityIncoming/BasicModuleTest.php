46c1f88d21ce63ff5289ea5a1ffc82977fa202c1
Artur Hanusek <arthur@youritsolutions.ie>
2022-11-08 18:46:28 +0100

    create basic test

---
+++ tests/Feature/Modules/InventoryQuantityIncoming/BasicModuleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Modules\InventoryQuantityIncoming;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BasicModuleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function test_module_basic_functionality()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+}
