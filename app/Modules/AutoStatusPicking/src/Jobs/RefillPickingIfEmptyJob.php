c57ded168a0a8d51f09382e167c10b39fc0a404a
Artur Hanusek <arthur@youritsolutions.ie>
2022-11-29 12:43:15 +0100

    repeat multiple times to fill better

--- app/Modules/AutoStatusPicking/src/Jobs/RefillPickingIfEmptyJob.php
+++ app/Modules/AutoStatusPicking/src/Jobs/RefillPickingIfEmptyJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\AutoStatusPicking\src\Jobs;
 
+use App\Models\AutoStatusPickingConfiguration;
 use App\Models\Order;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -18,6 +19,13 @@ class RefillPickingIfEmptyJob implements ShouldQueue
     use SerializesModels;
     use IsMonitored;
 
+    private $maxAllowed;
+
+    public function __construct()
+    {
+        $this->maxAllowed = AutoStatusPickingConfiguration::query()->firstOrCreate([], [])->max_batch_size;
+    }
+
     /**
      * Execute the job.
      *
@@ -29,8 +37,14 @@ class RefillPickingIfEmptyJob implements ShouldQueue
             return;
         }
 
-        RefillOldOrdersToPickingJob::dispatchNow();
-        RefillPickingMissingStockJob::dispatchNow();
-        RefillPickingByOldestJob::dispatchNow();
+        do {
+            $countBefore = Order::query()->whereIn('status_code', ['picking'])->count();
+
+            RefillOldOrdersToPickingJob::dispatchNow();
+            RefillPickingMissingStockJob::dispatchNow();
+            RefillPickingByOldestJob::dispatchNow();
+
+            $countAfter = Order::query()->whereIn('status_code', ['picking'])->count();
+        } while (($countAfter < $this->maxAllowed) and ($countBefore !== $countAfter));
     }
 }
