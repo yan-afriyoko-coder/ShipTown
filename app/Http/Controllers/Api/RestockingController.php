91f2064742473d65315a6b9bda8e4b2c8d6c235c
Artur Hanusek <arthur@youritsolutions.ie>
2022-11-30 12:39:55 +0100

    add caching to restocking report

--- app/Http/Controllers/Api/RestockingController.php
+++ app/Http/Controllers/Api/RestockingController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Modules\Reports\src\Models\RestockingReport;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Cache;
 
 class RestockingController extends Controller
 {
@@ -17,6 +18,10 @@ class RestockingController extends Controller
             ->simplePaginate(request()->get('per_page', 10))
             ->appends(request()->query());
 
-        return JsonResource::collection($resource);
+        $anonymousResourceCollection = JsonResource::collection($resource);
+
+        Cache::put('cached_restocking_report', $anonymousResourceCollection, 60);
+
+        return $anonymousResourceCollection;
     }
 }
