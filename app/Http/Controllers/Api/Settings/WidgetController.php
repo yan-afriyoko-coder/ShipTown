24dac1f00cb912c1e859a0fbb2f0c5e12a023355
arthur@coding.cab <arthur@youritsolutions.ie>
2022-12-08 18:59:39 +0100

    fix tests

--- app/Http/Controllers/Api/Settings/WidgetController.php
+++ app/Http/Controllers/Api/Settings/WidgetController.php
@@ -13,30 +13,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
  */
 class WidgetController extends Controller
 {
-    /**
-     * @param StoreWidgetRequest $request
-     *
-     * @return JsonResource
-     */
-    public function store(StoreWidgetRequest $request)
+    public function store(StoreWidgetRequest $request): JsonResource
     {
-        $widget = new Widget();
-        $widget->fill($request->all());
-        $widget->save();
+        $widget = Widget::create($request->validated());
 
         return new JsonResource($widget);
     }
 
-    /**
-     * @param UpdateWidgetRequest $request
-     * @param Widget              $widget
-     *
-     * @return JsonResource
-     */
-    public function update(UpdateWidgetRequest $request, Widget $widget)
+    public function update(UpdateWidgetRequest $request, int $widget_id): JsonResource
     {
-        $widget->fill($request->all());
-        $widget->save();
+        $widget = Widget::query()->findOrFail($widget_id);
+
+        $widget->update($request->validated());
 
         return new JsonResource($widget);
     }
