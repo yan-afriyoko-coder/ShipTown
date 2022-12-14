255c5d926c0e1666196361bbed12456fd11f4306
arthur@coding.cab <arthur@youritsolutions.ie>
2022-12-14 01:17:38 +0100

    fix orders page test

--- tests/Browser/Routes/OrdersPageTest.php
+++ tests/Browser/Routes/OrdersPageTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser\Routes;
 
+use App\Models\Order;
 use App\User;
 use Exception;
 use Laravel\Dusk\Browser;
@@ -10,75 +11,74 @@ use Throwable;
 
 class OrdersPageTest extends DuskTestCase
 {
-    private string $uri = '';
+    private string $uri = 'orders';
 
-    public function testIncomplete()
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
     {
-        $this->markTestIncomplete('This test has not been implemented yet.');
+        if (empty($this->uri)) {
+            throw new Exception('Please set the $uri property in your test class.');
+        }
+
+        parent::setUp();
     }
 
-//    /**
-//     * @throws Exception
-//     */
-//    protected function setUp(): void
-//    {
-//        if (empty($this->uri)) {
-//            throw new Exception('Please set the $uri property in your test class.');
-//        }
-//
-//        parent::setUp();
-//    }
-//
-//    /**
-//     * @throws Throwable
-//     */
-//    public function testUserAccess()
-//    {
-//        $this->browse(function (Browser $browser) {
-//            /** @var User $user */
-//            $user = User::factory()->create();
-//            $user->assignRole('user');
-//
-//            $browser->disableFitOnFailure()
-//                ->loginAs($user)
-//                ->visit($this->uri)
-//                ->pause(300)
-//                ->assertRouteIs($this->uri)
-//                ->assertSourceMissing('snotify-error');
-//        });
-//    }
-//
-//    /**
-//     * @throws Throwable
-//     */
-//    public function testAdminAccess()
-//    {
-//        $this->browse(function (Browser $browser) {
-//            /** @var User $admin */
-//            $admin = User::factory()->create();
-//            $admin->assignRole('admin');
-//
-//            $browser->disableFitOnFailure()
-//                ->loginAs($admin)
-//                ->visit($this->uri)
-//                ->pause(300)
-//                ->assertRouteIs($this->uri)
-//                ->assertSourceMissing('snotify-error');
-//        });
-//    }
-//
-//    /**
-//     * @throws Throwable
-//     */
-//    public function testGuestAccess()
-//    {
-//        $this->browse(function (Browser $browser) {
-//            $browser->disableFitOnFailure()
-//                ->logout()
-//                ->visit($this->uri)
-//                ->assertRouteIs('login')
-//                ->assertSee('Login');
-//        });
-//    }
+    /**
+     * @throws Throwable
+     */
+    public function testUserAccess()
+    {
+        $this->browse(function (Browser $browser) {
+            /** @var User $user */
+            $user = User::factory()->create();
+            $user->assignRole('user');
+
+            /** @var Order $order */
+            $order = Order::factory()->create(['status_code' => 'paid']);
+
+            $browser->disableFitOnFailure()
+                ->loginAs($user)
+                ->visit($this->uri)
+                ->pause(300)
+                ->assertRouteIs($this->uri)
+                ->assertSourceMissing('snotify-error')
+                ->assertSee($order->order_number);
+        });
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testAdminAccess()
+    {
+        $this->browse(function (Browser $browser) {
+            /** @var User $admin */
+            $admin = User::factory()->create();
+            $admin->assignRole('admin');
+
+            $browser->disableFitOnFailure()
+                ->loginAs($admin)
+                ->visit($this->uri)
+                ->pause(300)
+                ->assertRouteIs($this->uri)
+                ->assertSourceMissing('snotify-error');
+        });
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testGuestAccess()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->disableFitOnFailure()
+                ->logout()
+                ->visit($this->uri)
+                ->assertRouteIs('login')
+                ->assertSee('Login');
+        });
+    }
 }
 
