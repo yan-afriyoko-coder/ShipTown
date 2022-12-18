5e56d205674f66c316e56f4090c80f19de788778
arthur@coding.cab <arthur@youritsolutions.ie>
2022-12-18 23:30:51 +0100

    refactoring

--- tests/Browser/Routes/StocktakingPageTest.php
+++ tests/Browser/Routes/StocktakingPageTest.php
@@ -2,9 +2,6 @@
 
 namespace Tests\Browser\Routes;
 
-use App\User;
-use Exception;
-use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use Throwable;
 
@@ -13,67 +10,13 @@ class StocktakingPageTest extends DuskTestCase
     private string $uri = 'stocktaking';
 
     /**
-     * @throws Exception
-     */
-    protected function setUp(): void
-    {
-        if (empty($this->uri)) {
-            throw new Exception('Please set the $uri property in your test class.');
-        }
-
-        parent::setUp();
-    }
-
-    /**
      * @throws Throwable
      */
-    public function testUserAccess()
+    public function testBasics()
     {
-        $this->browse(function (Browser $browser) {
-            /** @var User $user */
-            $user = User::factory()->create();
-            $user->assignRole('user');
-
-            $browser->disableFitOnFailure()
-                ->loginAs($user)
-                ->visit($this->uri)
-                ->pause(300)
-                ->assertRouteIs($this->uri)
-                ->assertSourceMissing('snotify-error');
-        });
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function testAdminAccess()
-    {
-        $this->browse(function (Browser $browser) {
-            /** @var User $admin */
-            $admin = User::factory()->create();
-            $admin->assignRole('admin');
-
-            $browser->disableFitOnFailure()
-                ->loginAs($admin)
-                ->visit($this->uri)
-                ->pause(300)
-                ->assertRouteIs($this->uri)
-                ->assertSourceMissing('snotify-error');
-        });
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function testGuestAccess()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->disableFitOnFailure()
-                ->logout()
-                ->visit($this->uri)
-                ->assertRouteIs('login')
-                ->assertSee('Login');
-        });
+        $this->basicUserAccessTest($this->uri, true);
+        $this->basicAdminAccessTest($this->uri, true);
+        $this->basicGuestAccessTest($this->uri);
     }
 }
 
