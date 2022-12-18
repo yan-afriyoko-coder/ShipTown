37e98094da40d5f59246be88af4036393299c937
arthur@coding.cab <arthur@youritsolutions.ie>
2022-12-18 23:32:56 +0100

    refactoring

--- tests/Browser/Routes/PicklistPageTest.php
+++ tests/Browser/Routes/PicklistPageTest.php
@@ -10,70 +10,16 @@ use Throwable;
 
 class PicklistPageTest extends DuskTestCase
 {
-    private string $uri = 'picklist';
-
-    /**
-     * @throws Exception
-     */
-    protected function setUp(): void
-    {
-        if (empty($this->uri)) {
-            throw new Exception('Please set the $uri property in your test class.');
-        }
-
-        parent::setUp();
-    }
+    private string $uri = '/picklist';
 
     /**
      * @throws Throwable
      */
-    public function testUserAccess()
+    public function testBasics()
     {
-        $this->browse(function (Browser $browser) {
-            /** @var User $user */
-            $user = User::factory()->create();
-            $user->assignRole('user');
-
-            $browser->disableFitOnFailure()
-                ->loginAs($user)
-                ->visit($this->uri)
-                ->pause(300)
-                ->assertRouteIs($this->uri)
-                ->assertSourceMissing('snotify-error');
-        });
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function testAdminAccess()
-    {
-        $this->browse(function (Browser $browser) {
-            /** @var User $admin */
-            $admin = User::factory()->create();
-            $admin->assignRole('admin');
-
-            $browser->disableFitOnFailure()
-                ->loginAs($admin)
-                ->visit($this->uri)
-                ->pause(300)
-                ->assertRouteIs($this->uri)
-                ->assertSourceMissing('snotify-error');
-        });
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function testGuestAccess()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->disableFitOnFailure()
-                ->logout()
-                ->visit($this->uri)
-                ->assertRouteIs('login')
-                ->assertSee('Login');
-        });
+        $this->basicUserAccessTest($this->uri, true);
+        $this->basicAdminAccessTest($this->uri, true);
+        $this->basicGuestAccessTest($this->uri);
     }
 }
 
