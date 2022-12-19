98bf56199ad2502146ac9ee4371320e850753fa2
arthur@coding.cab <arthur@youritsolutions.ie>
2022-12-19 12:07:34 +0100

    fix test

--- tests/Browser/Routes/StocktakingPageTest.php
+++ tests/Browser/Routes/StocktakingPageTest.php
@@ -2,6 +2,12 @@
 
 namespace Tests\Browser\Routes;
 
+use App\Models\Product;
+use App\Models\ProductAlias;
+use App\Models\Warehouse;
+use App\User;
+use Facebook\WebDriver\WebDriverKeys;
+use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use Throwable;
 
@@ -18,5 +24,147 @@ class StocktakingPageTest extends DuskTestCase
         $this->basicAdminAccessTest($this->uri, true);
         $this->basicGuestAccessTest($this->uri);
     }
-}
 
+    /**
+     * @throws Throwable
+     */
+    public function testSuccessfulStocktakeAction()
+    {
+        $this->browse(function (Browser $browser) {
+            $user = User::factory()->create();
+            Warehouse::factory()->create();
+
+            $browser->loginAs($user)
+                ->visit($this->uri)
+                ->pause(500)
+                ->assertSourceMissing('snotify-error')
+                ->assertFocused('@barcode-input-field');
+
+            while (rand(1, 3) !== 1) {
+                /** @var Product $product */
+                $product = Product::factory()->create();
+
+                $browser->assertFocused('@barcode-input-field')
+                    ->screenshot('StocktakingPage');
+                $browser->type('@barcode-input-field', $product->sku);
+                $browser->keys('@barcode-input-field', '{enter}');
+
+                $browser->waitFor('#quantity-request-input');
+                $browser->pause(500);
+                $browser->assertFocused('#quantity-request-input');
+                $browser->assertSee($product->sku);
+                $browser->assertSee($product->name);
+
+                $browser->driver->getKeyboard()->sendKeys(rand(0, 10000));
+                $browser->driver->getKeyboard()->sendKeys(WebDriverKeys::ENTER);
+
+                $browser->waitForText('Stocktake updated');
+                $browser->assertMissing('#quantity-request-input');
+                $browser->pause(500);
+                $browser->assertFocused('@barcode-input-field');
+            }
+        });
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testIfNegativeQuantityNotAllowed()
+    {
+        $this->browse(function (Browser $browser) {
+            Warehouse::factory()->create();
+
+            /** @var User $user */
+            $user = User::factory()->create();
+
+            /** @var Product $product */
+            $product = Product::factory()->create();
+
+            $browser->loginAs($user)
+                ->visit($this->uri)
+                ->pause(500)
+                ->assertSourceMissing('snotify-error')
+                ->assertFocused('@barcode-input-field');
+
+            $browser->driver->getKeyboard()->sendKeys($product->sku);
+            $browser->driver->getKeyboard()->sendKeys(WebDriverKeys::ENTER);
+
+            $browser->waitFor('#quantity-request-input')
+                ->pause(500)
+                ->assertSee($product->sku)
+                ->assertSee($product->name);
+
+            $browser->driver->getKeyboard()->sendKeys(-1);
+            $browser->driver->getKeyboard()->sendKeys(WebDriverKeys::ENTER);
+
+            $browser->waitForText('Minus quantity not allowed')
+                ->assertVisible('#quantity-request-input')
+                ->pause(500)
+                ->assertFocused('@quantity-request-input');
+
+            $browser->driver->getKeyboard()->sendKeys(WebDriverKeys::ESCAPE);
+        });
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testIfAliasScans()
+    {
+        $this->browse(function (Browser $browser) {
+            Warehouse::factory()->create();
+
+            /** @var User $user */
+            $user = User::factory()->create();
+
+            $browser->loginAs($user)
+                ->visit($this->uri)
+                ->pause(500)
+                ->assertSourceMissing('snotify-error')
+                ->assertFocused('@barcode-input-field');
+
+            /** @var ProductAlias $alias */
+            $alias = ProductAlias::query()->inRandomOrder()->first() ?? ProductAlias::factory()->create();
+
+            $browser->assertFocused('@barcode-input-field');
+
+            $browser->driver->getKeyboard()->sendKeys($alias->product->sku);
+            $browser->driver->getKeyboard()->sendKeys(WebDriverKeys::ENTER);
+
+            $browser->waitFor('#quantity-request-input')
+                ->pause(500)
+                ->assertFocused('#quantity-request-input')
+                ->assertSee($alias->product->sku)
+                ->assertSee($alias->product->name);
+
+            $browser->driver->getKeyboard()->sendKeys(rand(0, 10000));
+            $browser->driver->getKeyboard()->sendKeys(WebDriverKeys::ENTER);
+
+            $browser->waitForText('Stocktake updated')
+                ->assertMissing('#quantity-request-input')
+                ->pause(500)
+                ->assertFocused('@barcode-input-field');
+        });
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testIfNotifiesWhenProductNotFound()
+    {
+        $this->browse(function (Browser $browser) {
+            Warehouse::factory()->create();
+            $user = User::factory()->create();
+
+            $browser
+                ->loginAs($user)
+                ->visit($this->uri)
+                ->pause(500)
+                ->assertSourceMissing('snotify-error')
+                ->assertFocused('@barcode-input-field')
+                ->type('@barcode-input-field', 'not-existing-sku')
+                ->keys('@barcode-input-field', '{enter}')
+                ->waitForText('Product not found');
+        });
+    }
+}
