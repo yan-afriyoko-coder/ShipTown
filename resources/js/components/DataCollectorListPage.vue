6b2cf261d21b3dfa9a93943913d27d18fdf33c93
arthur@coding.cab <arthur@youritsolutions.ie>
2022-12-22 00:39:56 +0100

    change window position to avoid keyrboard bump

--- resources/js/components/DataCollectorListPage.vue
+++ resources/js/components/DataCollectorListPage.vue
@@ -45,7 +45,7 @@
             </div>
         </div>
 
-        <b-modal id="new-collection-modal" centered no-fade hide-header title="New Dats Collection" @ok="createCollectionAndRedirect" @shown="prepareNewCollectionModal">
+        <b-modal id="new-collection-modal" no-fade hide-header title="New Data Collection" @ok="createCollectionAndRedirect" @shown="prepareNewCollectionModal">
             <input id="collection_name_input" v-model="newCollectionName" type="text" @keyup.enter="createCollectionAndRedirect" class="form-control" placeholder="New Collection name">
             <hr>
             <vue-csv-import
