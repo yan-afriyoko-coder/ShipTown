2782968755282dd6b7c30e1d86f1c1c57e51b593
arthur@coding.cab <arthur@youritsolutions.ie>
2022-12-27 21:17:15 +0100

    refactring

--- routes/admin/api.php
+++ routes/admin/api.php
@@ -22,6 +22,7 @@ Route::apiResource('settings/order-statuses', Api\OrderStatusController::class, 
 Route::apiResource('settings/mail-templates', Api\Settings\MailTemplateController::class, ['as' => 'api.settings'])->only(['index', 'update']);
 Route::apiResource('settings/navigation-menu', Api\Settings\NavigationMenuController::class, ['as' => 'api.settings'])->only(['index', 'store', 'update', 'destroy']);
 Route::apiResource('settings/configurations', Api\Settings\ConfigurationController::class, ['as' => 'api.settings'])->only(['index', 'store']);
+Route::apiResource('settings/modules/api2cart/connections', Api\Settings\Module\Api2cart\Api2cartConnectionController::class, ['as' => 'api.settings.module.api2cart'])->only(['index', 'store', 'destroy']);
 
 Route::apiResource('modules/autostatus/picking/configuration', Api\Modules\AutoStatus\ConfigurationController::class, ['as' => 'modules.autostatus.picking'])->only('index', 'store');
 
@@ -30,7 +31,6 @@ Route::prefix('settings')->name('api.settings.')->group(function () {
     Route::prefix('modules')->name('module.')->group(function () {
         // api2cart
         Route::prefix('api2cart')->name('api2cart.')->group(function () {
-            Route::apiResource('connections', Api\Settings\Module\Api2cart\Api2cartConnectionController::class)->only(['index', 'store', 'destroy']);
             Route::apiResource('products', Api\Settings\Module\Api2cart\ProductsController::class)->only(['index']);
         });
 
