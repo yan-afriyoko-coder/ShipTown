4042d4e3d79227ec5afdeadc80b8f6fdcb39f23d
arthur@coding.cab <arthur@youritsolutions.ie>
2023-01-01 17:51:03 +0100

    AutoStatus Configuration controller

--- app/Http/Requests/AutoStatusConfigurationStoreRequest.php
+++ app/Http/Requests/AutoStatusConfigurationStoreRequest.php
@@ -13,7 +13,7 @@ class AutoStatusConfigurationStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return $this->user()->hasRole('admin');
     }
 
     /**
