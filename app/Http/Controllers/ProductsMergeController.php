b826c011a2e2b420bfae2844d34707ddaa3e74a0
arthur@coding.cab <arthur@youritsolutions.ie>
2023-01-12 19:37:01 +0100

    create merge products endpoint

---
+++ app/Http/Controllers/ProductsMergeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductsMergeIndexRequest;
+use App\Services\ProductService;
+
+class ProductsMergeController extends Controller
+{
+    public function index(ProductsMergeIndexRequest $request)
+    {
+        ProductService::merge($request->input('sku1'), $request->input('sku2'));
+
+        return 'ok';
+    }
+}
