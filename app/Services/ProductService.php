3415d9d852dbe9c86f700533312883007868c12e
arthur@coding.cab <arthur@youritsolutions.ie>
2023-01-12 18:13:51 +0100

    add product merge method to products service

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -2,8 +2,10 @@
 
 namespace App\Services;
 
+use App\Models\Inventory;
 use App\Models\Product;
 use App\Models\ProductAlias;
+use Barryvdh\LaravelIdeHelper\Alias;
 use Illuminate\Support\Facades\Log;
 
 /**
@@ -75,5 +77,33 @@ class ProductService
         Log::warning('Could not release quantity - SKU does not exist', ['sku' => $sku]);
 
         return false;
+    }
+
+    public static function merge(Product $product1, Product $product2)
+    {
+        $sku = $product2->sku;
+
+        // Merge product2 into product
+        $product2->update(['sku' => 'merged_to_'.$product1->sku.'_'.time()]);
+        $product2->aliases()->update(['product_id' => $product1->id]);
+
+        ProductAlias::query()->updateOrCreate([
+            'alias' => $sku,
+        ], [
+            'product_id' => $product1->id,
+        ]);
+
+        Inventory::query()
+            ->where(['product_id' => $product2->id])
+            ->where('quantity', '!=', 0)
+            ->get()
+            ->each(function (Inventory $p2_inventory) use ($product1) {
+                $p1_inventory = Inventory::query()
+                    ->where(['product_id' => $product1->id, 'warehouse_id' => $p2_inventory->warehouse_id])
+                    ->first();
+
+                InventoryService::adjustQuantity($p1_inventory, $p2_inventory->quantity, $p2_inventory->quantity);
+                InventoryService::adjustQuantity($p2_inventory, -$p2_inventory->quantity, $p2_inventory->quantity);
+            });
     }
 }
