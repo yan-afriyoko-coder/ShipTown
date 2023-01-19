bf185cf96a969ea897164c5cb9e8989c96f76b9f
arthur@coding.cab <arthur@youritsolutions.ie>
2023-01-19 01:50:55 +0100

    update link

--- resources/js/components/SharedComponents/ProductInfoCard.vue
+++ resources/js/components/SharedComponents/ProductInfoCard.vue
@@ -3,7 +3,7 @@
         <div class="text-primary h5">{{ product ? product['name'] : '&nbsp;' }}</div>
         <div>
             sku: <font-awesome-icon icon="copy" class="fa-xs btn-link" role="button" @click="copyToClipBoard((product ? product['sku'] : '') )"></font-awesome-icon>
-            <b> <a target="_blank" :href="'/products?sku=' + (product ? product['sku'] : '') " class="font-weight-bold">{{ (product ? product['sku'] : '&nbsp;') }}</a></b>
+            <b> <a target="_blank" :href="'/products?hide_nav_bar=true&sku=' + (product ? product['sku'] : '') " class="font-weight-bold">{{ (product ? product['sku'] : '&nbsp;') }}</a></b>
         </div>
         <div v-if="product">
             <template v-for="tag in product['tags']">
