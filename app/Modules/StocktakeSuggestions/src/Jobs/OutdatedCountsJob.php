0c0a5fc8ae7dacfed6b6e98bc85db555e947ef8d
arthur@coding.cab <arthur@youritsolutions.ie>
2023-01-24 23:17:04 +0100

    improve stocktake suggestions jobs

--- app/Modules/StocktakeSuggestions/src/Jobs/OutdatedCountsJob.php
+++ app/Modules/StocktakeSuggestions/src/Jobs/OutdatedCountsJob.php
@@ -75,10 +75,10 @@ class OutdatedCountsJob implements ShouldQueue
             FROM stocktake_suggestions
             INNER JOIN inventory
                 ON inventory.id = stocktake_suggestions.inventory_id
-                AND inventory.quantity = 0
 
             WHERE stocktake_suggestions.warehouse_id = ?
             AND stocktake_suggestions.reason = ?
+            AND inventory.quantity = 0
         ', [$warehouse_id, $reason]);
     }
 }
