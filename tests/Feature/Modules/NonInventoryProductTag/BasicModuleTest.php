17b4e92dea19cd2095c85b3dfe37d29fe84d72c2
arthur@coding.cab <arthur@youritsolutions.ie>
2023-01-26 12:23:46 +0100

    clear arcadia stock

--- tests/Feature/Modules/NonInventoryProductTag/BasicModuleTest.php
+++ tests/Feature/Modules/NonInventoryProductTag/BasicModuleTest.php
@@ -5,31 +5,59 @@ namespace Tests\Feature\Modules\NonInventoryProductTag;
 use App\Models\Inventory;
 use App\Models\Product;
 use App\Models\Warehouse;
+use App\Modules\NonInventoryProductTag\src\Jobs\ClearArcadiaStockJob;
 use App\Modules\NonInventoryProductTag\src\NonInventoryProductTagServiceProvider;
 use App\Services\InventoryService;
 use Tests\TestCase;
 
 class BasicModuleTest extends TestCase
 {
-    /** @test */
-    public function test_module_basic_functionality()
-    {
-        NonInventoryProductTagServiceProvider::enableModule();
+//    /** @test */
+//    public function test_module_basic_functionality()
+//    {
+//        NonInventoryProductTagServiceProvider::enableModule();
+//
+//        $warehouse = Warehouse::factory()->create();
+//        $product = Product::factory()->create();
+//
+//        $inventory = Inventory::firstOrCreate([
+//            'product_id' => $product->id,
+//            'warehouse_id' => $warehouse->id,
+//        ], []);
+//
+//        $inventory->product->attachTag('non-inventory');
+//
+//        InventoryService::adjustQuantity($inventory, 10, 'test');
+//
+//        $inventory->refresh();
+//
+//        $this->assertEquals(0, $inventory->quantity);
+//    }
 
-        $warehouse = Warehouse::factory()->create();
+    public function test_basic()
+    {
+        /** @var Product $product */
         $product = Product::factory()->create();
 
-        $inventory = Inventory::firstOrCreate([
-            'product_id' => $product->id,
-            'warehouse_id' => $warehouse->id,
-        ], []);
+        /** @var Warehouse $warehouse */
+        $warehouse = Warehouse::factory()->create();
 
-        $inventory->product->attachTag('non-inventory');
+        /** @var Inventory $inventory */
+        $inventory = Inventory::query()
+            ->where([
+                'product_id' => $product->getKey(),
+                'warehouse_id' => $warehouse->getKey(),
+            ])
+            ->first();
 
         InventoryService::adjustQuantity($inventory, 10, 'test');
 
-        $inventory->refresh();
+        $product->attachTag('ARCADIA DEAL JAN 2023', 'rms_sub_description3');
 
-        $this->assertEquals(0, $inventory->quantity);
+        ClearArcadiaStockJob::dispatch();
+
+        $inventory1 = Inventory::query()->where('quantity', '!=', 0);
+
+        $this->assertFalse($inventory1->exists());
     }
 }
