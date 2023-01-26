aa1e21cbe116c8986415cb0451ff6378eccaac7c
arthur@coding.cab <arthur@youritsolutions.ie>
2023-01-26 13:56:42 +0100

    test

--- app/Modules/NonInventoryProductTag/src/Jobs/ClearArcadiaStockJob.php
+++ app/Modules/NonInventoryProductTag/src/Jobs/ClearArcadiaStockJob.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Spatie\Tags\Tag;
 
 class ClearArcadiaStockJob implements ShouldQueue
 {
@@ -21,17 +22,17 @@ class ClearArcadiaStockJob implements ShouldQueue
 
     public function handle(): bool
     {
-        $productIds = Product::withAllTagsOfAnyType(['ARCADIA DEAL JAN 2023'])
+        $productIds = Product::withAllTags(Tag::findFromStringOfAnyType('ARCADIA DEAL JAN 2023'))
             ->where('quantity', '!=', 0)
             ->get()
             ->pluck('id');
 
-        Inventory::query()->whereIn('product_id', $productIds)
-            ->where('quantity', '!=', 0)
-            ->get()
-            ->each(function (Inventory $inventory) {
-                InventoryService::adjustQuantity($inventory, -$inventory->quantity, 'non_inventory_product_adjustment');
-            });
+//        Inventory::query()->whereIn('product_id', $productIds)
+//            ->where('quantity', '!=', 0)
+//            ->get()
+//            ->each(function (Inventory $inventory) {
+//                InventoryService::adjustQuantity($inventory, -$inventory->quantity, 'non_inventory_product_adjustment');
+//            });
 
         return true;
     }
