d915396b6185f5c6aca161b270be317e034c9adb
arthur@coding.cab <arthur@youritsolutions.ie>
2023-02-03 18:05:50 +0100

    update name when transfering to other warehouse

--- app/Http/Controllers/Api/DataCollectorActions/TransferToWarehouseController.php
+++ app/Http/Controllers/Api/DataCollectorActions/TransferToWarehouseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\DataCollectorActions;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\DataCollectionResource;
 use App\Models\DataCollection;
+use App\Models\Warehouse;
 use App\Modules\DataCollector\src\DataCollectorService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -16,8 +17,12 @@ class TransferToWarehouseController extends Controller
         $destinationDataCollection = null;
 
         DB::transaction(function () use ($request, &$destinationDataCollection) {
+            $warehouse = Warehouse::query()->findOrFail($request->get('destination_warehouse_id'));
             $sourceDataCollection = DataCollection::findOrFail($request->get('data_collector_id'));
 
+            $sourceDataCollection->update([
+                'name' => implode('', ['Transfer To ', $warehouse->code, ' - ', $sourceDataCollection->name])
+            ]);
             $sourceDataCollection->delete();
 
             $destinationDataCollection = DataCollectorService::transferScannedTo(
