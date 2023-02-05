64adbd6be9540153dab9c052fed4bffd2477e476
arthur@coding.cab <arthur@youritsolutions.ie>
2023-02-05 15:43:21 +0100

    update default migration stubs

---
+++ database/migrations/2023_02_05_144155_test3.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Test3 extends Migration
+{
+    public function up()
+    {
+        //
+    }
+}
