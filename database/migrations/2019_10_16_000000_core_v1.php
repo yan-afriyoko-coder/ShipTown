75c2345f7ac9ada8ce790d68af6f7997cc0610ac
Rizky Puji Raharja <rizky.rsj@gmail.com>
2023-02-13 00:22:03 +0700

    index modules_webhooks_pending_webhooks

--- database/migrations/2019_10_16_000000_core_v1.php
+++ database/migrations/2019_10_16_000000_core_v1.php
@@ -849,6 +849,8 @@ class CoreV1 extends Migration
             $table->timestamp('available_at')->nullable();
             $table->timestamp('published_at')->nullable();
             $table->timestamps();
+
+            $table->index(['reserved_at', 'published_at']);
         });
 
         Schema::create('modules_webhooks_configuration', function (Blueprint $table) {
