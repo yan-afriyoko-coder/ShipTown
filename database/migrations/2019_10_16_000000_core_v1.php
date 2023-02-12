50ca27d15010aec2e7ea44e220630c517fab0bb4
Rizky Puji Raharja <rizky.rsj@gmail.com>
2023-02-13 00:30:06 +0700

    create inventory_totals

--- database/migrations/2019_10_16_000000_core_v1.php
+++ database/migrations/2019_10_16_000000_core_v1.php
@@ -871,6 +871,23 @@ class CoreV1 extends Migration
             $table->unique(['key', 'key_id']);
         });
 
+        Schema::create('inventory_totals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->unique();
+            $table->decimal('quantity', 20)->default(0);
+            $table->decimal('quantity_reserved', 20)->default(0);
+            $table->decimal('quantity_incoming', 20)->default(0);
+            $table->decimal('quantity_available', 20)
+            ->storedAs('quantity - quantity_reserved')
+            ->comment('quantity - quantity_reserved');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->index('quantity');
+            $table->index('quantity_reserved');
+            $table->index('quantity_incoming');
+        });
+
         $this->installSpatiePermissions();
     }
 
