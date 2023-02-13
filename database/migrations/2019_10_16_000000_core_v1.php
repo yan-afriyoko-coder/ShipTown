c875ad4b3e4bd1e02d5cc01a74a81854838f118f
Rizky Puji Raharja <rizky.rsj@gmail.com>
2023-02-13 22:31:30 +0700

    CreateModulesRmsapiSalesImportsTable

--- database/migrations/2019_10_16_000000_core_v1.php
+++ database/migrations/2019_10_16_000000_core_v1.php
@@ -995,6 +995,15 @@ class CoreV1 extends Migration
             $table->timestamps();
         });
 
+        Schema::create('modules_rmsapi_sales_imports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('connection_id')->constrained('modules_rmsapi_connections');
+            $table->dateTime('reserved_at')->nullable();
+            $table->dateTime('processed_at')->nullable();
+            $table->json('raw_import');
+            $table->timestamps();
+        });
+
         $this->installSpatiePermissions();
     }
 
