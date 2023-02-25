bd96a74bd44ee687b665463ee468dc4a1bd3a8f6
Rizky Puji Raharja <rizky.rsj@gmail.com>
2023-02-25 18:00:47 +0700

    rename resource

--- app/Http/Controllers/Api/Modules/InventoryReservation/InventoryReservationController.php
+++ app/Http/Controllers/Api/Modules/InventoryReservation/InventoryReservationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\Modules\InventoryReservation;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\InventoryReservationsConfigurationIndexRequest;
 use App\Http\Requests\InventoryReservationsConfigurationUpdateRequest;
-use App\Http\Resources\InventoryReservationResource;
+use App\Http\Resources\InventoryReservsationsConfigurationResource;
 use App\Modules\InventoryReservations\src\Models\Configuration;
 
 class InventoryReservationController extends Controller
@@ -14,13 +14,13 @@ class InventoryReservationController extends Controller
     {
         $inventoryReservation = Configuration::first();
 
-        return new InventoryReservationResource($inventoryReservation);
+        return new InventoryReservsationsConfigurationResource($inventoryReservation);
     }
 
     public function update(InventoryReservationsConfigurationUpdateRequest $request, Configuration $inventoryReservation)
     {
         $inventoryReservation->update($request->validated());
 
-        return new InventoryReservationResource($inventoryReservation);
+        return new InventoryReservsationsConfigurationResource($inventoryReservation);
     }
 }
--- app/Http/Resources/InventoryReservsationsConfigurationResource.php
+++ app/Http/Resources/InventoryReservsationsConfigurationResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class InventoryReservationResource extends JsonResource
+class InventoryReservsationsConfigurationResource extends JsonResource
 {
     public $preserveKeys = true;
 
