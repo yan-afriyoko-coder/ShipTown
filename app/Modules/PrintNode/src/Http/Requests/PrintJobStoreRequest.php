31115ce28e3d7a0be6c9d9e9a21caf08f0217e58
Rizky Puji Raharja <rizky.rsj@gmail.com>
2023-02-28 12:50:52 +0700

    refactor PrintNode request

--- app/Modules/PrintNode/src/Http/Requests/PrintJobStoreRequest.php
+++ app/Modules/PrintNode/src/Http/Requests/PrintJobStoreRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Modules\PrintNode\src\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StorePrintJobRequest extends FormRequest
+class PrintJobStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
