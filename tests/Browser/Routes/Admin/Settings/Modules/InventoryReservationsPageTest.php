30ff1d394abf95121af054f9f5ac542ea463e080
Rizky Puji Raharja <rizky.rsj@gmail.com>
2023-03-04 10:34:34 +0700

    add InventoryReservationsPageTest

--- tests/Browser/Routes/Admin/Settings/Modules/InventoryReservationsPageTest.php
+++ tests/Browser/Routes/Admin/Settings/Modules/InventoryReservationsPageTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser\Routes\Admin\Settings\Modules;
 
+use App\Modules\InventoryReservations\src\EventServiceProviderBase as InventoryReservationsEventServiceProviderBase;
 use App\User;
 use Exception;
 use Laravel\Dusk\Browser;
@@ -12,73 +13,26 @@ class InventoryReservationsPageTest extends DuskTestCase
 {
     private string $uri = '/admin/settings/modules/inventory-reservations';
 
-    public function testIncomplete()
+    public function testBasics()
     {
-        $this->markTestIncomplete('This test has not been implemented yet.');
+        InventoryReservationsEventServiceProviderBase::enableModule();
+
+        $this->basicAdminAccessTest($this->uri, true);
+        $this->basicUserAccessTest($this->uri, false);
+        $this->basicGuestAccessTest($this->uri);
     }
 
-    // /**
-    //  * @throws Exception
-    //  */
-    // protected function setUp(): void
-    // {
-    //     if (empty($this->uri)) {
-    //         throw new Exception('Please set the $uri property in your test class.');
-    //     }
+    public function testPage()
+    {
+        InventoryReservationsEventServiceProviderBase::enableModule();
+        $user = User::factory()->create();
+        $user->assignRole('admin');
 
-    //     parent::setUp();
-    // }
-
-    // /**
-    //  * @throws Throwable
-    //  */
-    // public function testUserAccess()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         /** @var User $user */
-    //         $user = User::factory()->create();
-    //         $user->assignRole('user');
-
-    //         $browser->disableFitOnFailure()
-    //             ->loginAs($user)
-    //             ->visit($this->uri)
-    //             ->pause(300)
-    //             ->assertPathIs($this->uri)
-    //             ->assertSourceMissing('snotify-error');
-    //     });
-    // }
-
-    // /**
-    //  * @throws Throwable
-    //  */
-    // public function testAdminAccess()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         /** @var User $admin */
-    //         $admin = User::factory()->create();
-    //         $admin->assignRole('admin');
-
-    //         $browser->disableFitOnFailure()
-    //             ->loginAs($admin)
-    //             ->visit($this->uri)
-    //             ->pause(300)
-    //             ->assertPathIs($this->uri)
-    //             ->assertSourceMissing('snotify-error');
-    //     });
-    // }
-
-    // /**
-    //  * @throws Throwable
-    //  */
-    // public function testGuestAccess()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->disableFitOnFailure()
-    //             ->logout()
-    //             ->visit($this->uri)
-    //             ->assertRouteIs('login')
-    //             ->assertSee('Login');
-    //     });
-    // }
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+            ->visit($this->uri)
+            ->assertSee('Inventory Reservations');
+        });
+    }
 }
 
