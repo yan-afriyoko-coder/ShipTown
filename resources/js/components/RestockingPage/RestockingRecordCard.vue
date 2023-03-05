95a9dd7401782bb9885018816a5288a8b1bd98c0
arthur@coding.cab <arthur@youritsolutions.ie>
2023-03-05 11:15:28 +0100

    add last sold date to restocking

--- resources/js/components/RestockingPage/RestockingRecordCard.vue
+++ resources/js/components/RestockingPage/RestockingRecordCard.vue
@@ -21,13 +21,18 @@
                         <div @click="expanded = !expanded">location: <b>{{ record['warehouse_code'] }}</b></div>
                         <div @click="expanded = !expanded">price: <b>{{ record['warehouse_quantity'] }}</b></div>
                         <div @click="expanded = !expanded">warehouse stock: <b>{{ record['warehouse_quantity'] }}</b></div>
-                        <div><div @click="expanded = !expanded" class="d-inline">last movement at:</div>
-                            <strong @click="$emit('showModalMovement', record['product_sku'])" class="text-primary cursor-pointer">{{ formatDateTime(record['last_movement_at']) }}</strong>
+                        <div>
+                            <div @click="expanded = !expanded" class="d-inline">last sold at: </div>
+                            <strong @click="showInventoryMovementModal" class="text-primary cursor-pointer">{{ formatDateTime(record['last_sold_at']) }}</strong>
+                        </div>
+                        <div>
+                            <div @click="expanded = !expanded" class="d-inline">last received at:</div>
+                            <strong @click="showInventoryMovementModal" class="text-primary cursor-pointer">{{ formatDateTime(record['last_received_at']) }}</strong>
                         </div>
                         <div @click="expanded = !expanded">last counted at: <b>{{ formatDateTime(record['last_counted_at'],'D MMM HH:MM') }}</b></div>
                         <template @click="expanded = !expanded" v-if="expanded">
+                            <div @click="expanded = !expanded">last movement at: <b>{{ formatDateTime(record['last_movement_at'],'D MMM HH:MM') }}</b></div>
                             <div @click="expanded = !expanded">first received at: <b>{{ formatDateTime(record['first_received_at'],'D MMM HH:MM') }}</b></div>
-                            <div @click="expanded = !expanded">last received at: <b>{{ formatDateTime(record['last_received_at'],'D MMM HH:MM') }}</b></div>
                         </template>
                     </div>
                     <div class="col-lg-4">
@@ -269,8 +274,13 @@ export default {
 
                 this.postInventoryUpdate();
             },
+
+            showInventoryMovementModal() {
+                this.$emit('showModalMovement', this.record['product_sku'])
+            },
         },
     }
+
 </script>
 
 <style lang="scss" scoped>
