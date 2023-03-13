2c27e60f6a6ffd294034216083a298f4c591b0f0
arthur@coding.cab <arthur@youritsolutions.ie>
2023-03-13 21:54:56 +0100

    return on sync

--- app/Modules/QueueMonitor/src/Listeners/JobProcessingListener.php
+++ app/Modules/QueueMonitor/src/Listeners/JobProcessingListener.php
@@ -17,6 +17,8 @@ class JobProcessingListener
                     'dispatched_at' => now(),
                     'processing_at' => now()
                 ]);
+
+                return;
             }
 
             DB::table('modules_queue_monitor_jobs')
