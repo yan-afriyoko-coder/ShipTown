a13791990545b2a0738c8ed139e7147b8463e6fa
arthur@coding.cab <arthur@youritsolutions.ie>
2023-03-13 21:00:15 +0100

    refactoring

--- app/Modules/QueueMonitor/src/Listeners/JobProcessingListener.php
+++ app/Modules/QueueMonitor/src/Listeners/JobProcessingListener.php
@@ -2,37 +2,23 @@
 
 namespace App\Modules\QueueMonitor\src\Listeners;
 
-use App\Modules\QueueMonitor\src\Jobs\SampleJob;
-use Illuminate\Queue\Jobs\Job;
 use Illuminate\Support\Facades\DB;
 
 class JobProcessingListener
 {
     public function handle($event)
     {
-        /** @var Job $job */
-        $job = $event->job;
-
-        $recordsUpdated = DB::table('modules_queue_monitor_jobs')
-            ->where([
-                'uuid' => null,
-                'job_class' => $job->payload()['displayName'],
-            ])
-            ->update([
-                'uuid' => $job->payload()['uuid'],
-                'processing_at' => now()
-            ]);
-
-        if ($recordsUpdated > 0) {
-            return;
-        }
-
-        DB::table('modules_queue_monitor_jobs')
-            ->insert([
-                'uuid' => $job->payload()['uuid'],
-                'job_class' => $job->payload()['displayName'],
+        if ($event->job->getConnectionName() === 'sync') {
+            DB::table('modules_queue_monitor_jobs')->insert([
+                'uuid' => $event->job->payload()['uuid'],
+                'job_class' => $event->job->payload()['displayName'],
                 'dispatched_at' => now(),
                 'processing_at' => now()
             ]);
+        }
+
+        DB::table('modules_queue_monitor_jobs')
+            ->where(['uuid' => null, 'job_class' => $event->job->payload()['displayName']])
+            ->update(['uuid' => $event->job->payload()['uuid'], 'processing_at' => now()]);
     }
 }
