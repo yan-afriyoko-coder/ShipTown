2a15853bc4fc4eb3dd698c709590d1ad5a7d656e
arthur@coding.cab <arthur@youritsolutions.ie>
2023-03-14 17:42:20 +0100

    recreate queue_monitor_jobs table

--- database/migrations/2023_03_14_163515_recreate_modules_queue_monitor_jobs_table.php
+++ database/migrations/2023_03_14_163515_recreate_modules_queue_monitor_jobs_table.php
@@ -2,20 +2,27 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class AddTimingColumnsToModulesQueueMonitorJobsTable extends Migration
+class RecreateModulesQueueMonitorJobsTable extends Migration
 {
     public function up()
     {
-        Schema::table('modules_queue_monitor_jobs', function (Blueprint $table) {
+        Schema::drop('modules_queue_monitor_jobs');
+
+        Schema::create('modules_queue_monitor_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->nullable()->unique();
+            $table->string('job_class')->index();
+            $table->timestamp('dispatched_at')->default(DB::raw('CURRENT_TIMESTAMP'))->index();
+            $table->timestamp('processing_at')->nullable();
+            $table->timestamp('processed_at')->nullable();
             $table->unsignedInteger('seconds_dispatching')
-                ->nullable()
                 ->storedAs('TIMESTAMPDIFF(SECOND, dispatched_at, processing_at)')
                 ->comment('TIMESTAMPDIFF(SECOND, dispatched_at, processing_at)');
 
             $table->unsignedInteger('seconds_running')
-                ->nullable()
                 ->storedAs('TIMESTAMPDIFF(SECOND, processing_at, processed_at)')
                 ->comment('TIMESTAMPDIFF(SECOND, processing_at, processed_at)');
         });
