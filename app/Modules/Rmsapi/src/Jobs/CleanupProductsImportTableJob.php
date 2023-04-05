2f76781c9a65b4bae25404c235956954ce122606
arthur@coding.cab <arthur@youritsolutions.ie>
2023-04-05 16:40:50 +0100

    add script to cleanup

--- app/Modules/Rmsapi/src/Jobs/CleanupProductsImportTableJob.php
+++ app/Modules/Rmsapi/src/Jobs/CleanupProductsImportTableJob.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
 
 class CleanupProductsImportTableJob implements ShouldQueue
 {
@@ -23,9 +24,16 @@ class CleanupProductsImportTableJob implements ShouldQueue
             ->where('reserved_at', '<', now()->subMinutes(5))
             ->update(['reserved_at' => null]);
 
+
         RmsapiProductImport::query()
             ->where('when_processed', '<', now()->subDays(7))
             ->forceDelete();
+
+        DB::statement('
+            UPDATE inventory SET inventory.last_movement_id = (SELECT MAX(ID) FROM inventory_movements WHERE inventory_id = inventory.id)
+            WHERE inventory.last_movement_id != (SELECT MAX(ID) FROM inventory_movements WHERE inventory_id = inventory.id)
+            LIMIT 20;
+        ');
 
         return true;
     }
