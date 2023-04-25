a9505dc52bb1e2351aaa2d81a8866edbedc68853
arthur@coding.cab <arthur@youritsolutions.ie>
2023-04-25 16:46:37 +0200

    add process imported jobs to every minute jobs

--- app/Modules/Rmsapi/src/Listeners/EveryMinuteEventListener.php
+++ app/Modules/Rmsapi/src/Listeners/EveryMinuteEventListener.php
@@ -2,8 +2,10 @@
 
 namespace App\Modules\Rmsapi\src\Listeners;
 
+use App\Modules\Rmsapi\src\Jobs\ImportProductsJob;
 use App\Modules\Rmsapi\src\Jobs\ImportSalesJob;
 use App\Modules\Rmsapi\src\Jobs\ImportShippingsJob;
+use App\Modules\Rmsapi\src\Jobs\ProcessImportedProductRecordsJob;
 use App\Modules\Rmsapi\src\Jobs\ProcessImportedSalesRecordsJob;
 use App\Modules\Rmsapi\src\Jobs\UpdateImportedSalesRecordsJob;
 use App\Modules\Rmsapi\src\Models\RmsapiConnection;
@@ -16,6 +18,7 @@ class EveryMinuteEventListener
         foreach (RmsapiConnection::all() as $rmsapiConnection) {
             ImportSalesJob::dispatch($rmsapiConnection->id);
             ImportShippingsJob::dispatch($rmsapiConnection->id);
+            ImportProductsJob::dispatch($rmsapiConnection->id);
 
             Log::debug('RMSAPI Sync jobs dispatched', [
                 'warehouse_code' => $rmsapiConnection->location_id,
@@ -24,7 +27,7 @@ class EveryMinuteEventListener
         }
 
         UpdateImportedSalesRecordsJob::dispatch();
-
         ProcessImportedSalesRecordsJob::dispatch();
+        ProcessImportedProductRecordsJob::dispatch();
     }
 }
