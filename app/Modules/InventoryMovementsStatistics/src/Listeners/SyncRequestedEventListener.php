078d52a75b2c6e858ecdf58a155d172d58220520
arthur@coding.cab <arthur@youritsolutions.ie>
2023-06-28 21:35:29 +0200

    eliminate job

--- app/Modules/InventoryMovementsStatistics/src/Listeners/SyncRequestedEventListener.php
+++ app/Modules/InventoryMovementsStatistics/src/Listeners/SyncRequestedEventListener.php
@@ -4,14 +4,12 @@ namespace App\Modules\InventoryMovementsStatistics\src\Listeners;
 
 use App\Events\SyncRequestedEvent;
 use App\Modules\InventoryMovementsStatistics\src\Jobs\ClearOutdatedStatisticsJob;
-use App\Modules\InventoryMovementsStatistics\src\Jobs\EnsureCorrectLastSoldAtJob;
 use App\Modules\InventoryMovementsStatistics\src\Jobs\UpdateInventoryMovementsStatisticsTableJob;
 
 class SyncRequestedEventListener
 {
     public function handle(SyncRequestedEvent $event)
     {
-        EnsureCorrectLastSoldAtJob::dispatch();
         UpdateInventoryMovementsStatisticsTableJob::dispatch();
         ClearOutdatedStatisticsJob::dispatch();
     }
