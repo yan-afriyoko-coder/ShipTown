8be2e7e3722adba09f64a19f001c651032bcd706
arthur@coding.cab <arthur@youritsolutions.ie>
2023-06-28 19:25:55 +0200

    repopulate when module enabled

--- app/Modules/InventoryMovementsStatistics/src/Listeners/SyncRequestedEventListener.php
+++ app/Modules/InventoryMovementsStatistics/src/Listeners/SyncRequestedEventListener.php
@@ -5,14 +5,12 @@ namespace App\Modules\InventoryMovementsStatistics\src\Listeners;
 use App\Events\SyncRequestedEvent;
 use App\Modules\InventoryMovementsStatistics\src\Jobs\ClearOutdatedStatisticsJob;
 use App\Modules\InventoryMovementsStatistics\src\Jobs\EnsureCorrectLastSoldAtJob;
-use App\Modules\InventoryMovementsStatistics\src\Jobs\RepopulateLast28DaysTableJob;
 use App\Modules\InventoryMovementsStatistics\src\Jobs\UpdateInventoryMovementsStatisticsTableJob;
 
 class SyncRequestedEventListener
 {
     public function handle(SyncRequestedEvent $event)
     {
-        RepopulateLast28DaysTableJob::dispatch();
         EnsureCorrectLastSoldAtJob::dispatch();
         UpdateInventoryMovementsStatisticsTableJob::dispatch();
         ClearOutdatedStatisticsJob::dispatch();
