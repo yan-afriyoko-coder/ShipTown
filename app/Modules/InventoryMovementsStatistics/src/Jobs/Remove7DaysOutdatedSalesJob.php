babc5b340f85ff916fbd708f2b7a75e56c594e2d
arthur@coding.cab <arthur@youritsolutions.ie>
2023-07-06 15:25:30 +0200

    change temporary table names so jobs dont collide

--- app/Modules/InventoryMovementsStatistics/src/Jobs/Remove7DaysOutdatedSalesJob.php
+++ app/Modules/InventoryMovementsStatistics/src/Jobs/Remove7DaysOutdatedSalesJob.php
@@ -35,9 +35,9 @@ class Remove7DaysOutdatedSalesJob implements ShouldQueue
         ");
 
         DB::unprepared("
-            DROP TEMPORARY TABLE IF EXISTS temp_itemMovementsStatistics_3982179371;
+            DROP TEMPORARY TABLE IF EXISTS temp_itemMovementsStatistics_3982179371312321313;
 
-            CREATE TEMPORARY TABLE temp_itemMovementsStatistics_3982179371 AS (
+            CREATE TEMPORARY TABLE temp_itemMovementsStatistics_3982179371312321313 AS (
                 SELECT inventory_id,
                        sum(quantity_sold) as quantity_sold,
                        max(sold_at) as max_sold_at
@@ -49,7 +49,7 @@ class Remove7DaysOutdatedSalesJob implements ShouldQueue
 
             INSERT INTO inventory_movements_statistics (inventory_id, product_id, warehouse_code, warehouse_id, created_at, updated_at)
                 SELECT inventory.id, inventory.product_id, inventory.warehouse_code, inventory.warehouse_id, now(), now()
-                FROM temp_itemMovementsStatistics_3982179371 as tempTable
+                FROM temp_itemMovementsStatistics_3982179371312321313 as tempTable
                 LEFT JOIN inventory_movements_statistics
                     ON inventory_movements_statistics.inventory_id = tempTable.inventory_id
                 LEFT JOIN inventory
@@ -57,19 +57,19 @@ class Remove7DaysOutdatedSalesJob implements ShouldQueue
                 WHERE inventory_movements_statistics.inventory_id is null;
 
             UPDATE inventory_movements_statistics
-                RIGHT JOIN temp_itemMovementsStatistics_3982179371 as tempTable
+                RIGHT JOIN temp_itemMovementsStatistics_3982179371312321313 as tempTable
                 ON inventory_movements_statistics.inventory_id = tempTable.inventory_id
             SET
                 quantity_sold_last_7_days = IFNULL(quantity_sold_last_7_days, 0) - quantity_sold;
 
             UPDATE modules_inventory_movements_statistics_last28days_sale_movements
-            LEFT JOIN temp_itemMovementsStatistics_3982179371 as tempTable
+            LEFT JOIN temp_itemMovementsStatistics_3982179371312321313 as tempTable
                 ON tempTable.inventory_id = modules_inventory_movements_statistics_last28days_sale_movements.inventory_id
             SET included_in_7days = 0
             WHERE included_in_7days = 1
               AND modules_inventory_movements_statistics_last28days_sale_movements.sold_at <= tempTable.max_sold_at;
 
-            DROP TEMPORARY TABLE IF EXISTS temp_itemMovementsStatistics_3982179371;
+            DROP TEMPORARY TABLE IF EXISTS temp_itemMovementsStatistics_3982179371312321313;
         ");
     }
 }
