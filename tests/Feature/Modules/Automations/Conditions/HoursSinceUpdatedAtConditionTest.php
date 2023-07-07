0e9835dacf1ca6aa9a36494a895cb3ee17527619
arthur@coding.cab <arthur@youritsolutions.ie>
2023-07-07 19:08:55 +0200

    fix Hourst Since update at filer, add testing

--- tests/Feature/Modules/Automations/Conditions/HoursSinceUpdatedAtConditionTest.php
+++ tests/Feature/Modules/Automations/Conditions/HoursSinceUpdatedAtConditionTest.php
@@ -5,22 +5,23 @@ namespace Tests\Feature\Modules\Automations\Conditions;
 use App\Models\Order;
 use App\Models\OrderProduct;
 use App\Models\OrderProductTotal;
+use App\Modules\Automations\src\Conditions\Order\HoursSinceLastUpdatedAtCondition;
 use App\Modules\Automations\src\Conditions\Order\HoursSincePlacedAtCondition;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
-class HoursSincePlacedAtConditionTest extends TestCase
+class HoursSinceUpdatedAtConditionTest extends TestCase
 {
     public function testConditionTrue()
     {
         $select = DB::select('select now() as now');
 
-        Order::factory()->create(['order_placed_at' => Carbon::createFromTimeString($select[0]->now)->subHours(3)]);
+        Order::factory()->create(['updated_at' => Carbon::createFromTimeString($select[0]->now)->subHours(3)]);
 
         $query = Order::query();
 
-        HoursSincePlacedAtCondition::addQueryScope($query, '2');
+        HoursSinceLastUpdatedAtCondition::addQueryScope($query, '2');
 
         ray(Order::all()->toArray());
         ray(OrderProduct::all()->toArray());
@@ -35,11 +36,11 @@ class HoursSincePlacedAtConditionTest extends TestCase
     {
         $select = DB::select('select now() as now');
 
-        Order::factory()->create(['order_placed_at' => Carbon::createFromTimeString($select[0]->now)->subHour()]);
+        Order::factory()->create(['updated_at' => Carbon::createFromTimeString($select[0]->now)->subHour()]);
 
         $query = Order::query();
 
-        HoursSincePlacedAtCondition::addQueryScope($query, '2');
+        HoursSinceLastUpdatedAtCondition::addQueryScope($query, '2');
 
         ray(Order::all()->toArray());
 
@@ -54,7 +55,7 @@ class HoursSincePlacedAtConditionTest extends TestCase
 
         $query = Order::query();
 
-        HoursSincePlacedAtCondition::addQueryScope($query, '');
+        HoursSinceLastUpdatedAtCondition::addQueryScope($query, '');
 
         ray(Order::all()->toArray());
 
@@ -69,7 +70,7 @@ class HoursSincePlacedAtConditionTest extends TestCase
 
         $query = Order::query();
 
-        HoursSincePlacedAtCondition::addQueryScope($query, 'string');
+        HoursSinceLastUpdatedAtCondition::addQueryScope($query, 'string');
 
         ray(Order::all()->toArray());
 
