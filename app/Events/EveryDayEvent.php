66fe8585687b990f2ac9e2add6c773c1f1802167
arthur@coding.cab <arthur@youritsolutions.ie>
2023-08-09 17:54:14 +0200

    refactoring

--- app/Events/EveryDayEvent.php
+++ app/Events/EveryDayEvent.php
@@ -2,20 +2,11 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-/**
- * This event will be dispatched DAILY
- * DAILY is not guaranteed,
- * it can also be dispatched multiple times as
- * users can dispatch this manually if required
- *
- * @package App\Events
- */
 class EveryDayEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
