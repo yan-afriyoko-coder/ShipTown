b1fb6b39071fe26dc0a117c2556c74164f7e02e5
arthur@coding.cab <arthur@youritsolutions.ie>
2023-08-17 18:07:01 +0200

    fix incorrect quantity before and after

--- app/Modules/Maintenance/src/Listeners/EveryTenMinutesEventListener.php
+++ app/Modules/Maintenance/src/Listeners/EveryTenMinutesEventListener.php
@@ -3,11 +3,13 @@
 namespace App\Modules\Maintenance\src\Listeners;
 
 use App\Modules\Maintenance\src\Jobs\temp\FillPreviousMovementIdJob;
+use App\Modules\Maintenance\src\Jobs\temp\FixIncorrectQuantityBeforeAndAfterJob;
 
 class EveryTenMinutesEventListener
 {
     public function handle()
     {
         FillPreviousMovementIdJob::dispatch();
+        FixIncorrectQuantityBeforeAndAfterJob::dispatch();
     }
 }
