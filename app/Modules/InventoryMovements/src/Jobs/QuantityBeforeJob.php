8a53371ce8a38388b1202da55c9ad84e1cec0236
arthur@coding.cab <arthur@youritsolutions.ie>
2023-08-26 12:45:26 +0200

    update QuantityBeforeJob

--- app/Modules/InventoryMovements/src/Jobs/QuantityBeforeJob.php
+++ app/Modules/InventoryMovements/src/Jobs/QuantityBeforeJob.php
@@ -29,16 +29,31 @@ class QuantityBeforeJob implements ShouldQueue, ShouldBeUnique
             $recordsUpdated = DB::update('
             WITH tbl AS (
                 SELECT
+                    inventory_movements.created_at as created_at,
                     inventory_movements.id as movement_id,
-                    previous_movement.quantity_after as quantity_before_expected
+                    inventory_movements.description as description,
+                    inventory_movements.inventory_id as inventory_id,
+
+                    previous_movement.quantity_after as quantity_before_expected,
+
+                    inventory_movements.quantity_before as quantity_before_actual,
+                    previous_movement.quantity_after - inventory_movements.quantity_before as quantity_before_delta,
+                    inventory_movements.quantity_after as quantity_after_actual,
+                    CASE WHEN inventory_movements.description = "stocktake"
+                        THEN inventory_movements.quantity_after
+                        ELSE inventory_movements.quantity_after + (previous_movement.quantity_after - inventory_movements.quantity_before)
+                        END AS quantity_after_expected,
+                    inventory_movements.quantity_delta as quantity_delta_actual,
+                    CASE WHEN inventory_movements.description = "stocktake"
+                        THEN inventory_movements.quantity_delta - (previous_movement.quantity_after - inventory_movements.quantity_before)
+                        ElSE inventory_movements.quantity_delta
+                        END AS quantity_delta_expected
 
                 FROM inventory_movements
                 INNER JOIN inventory_movements as previous_movement
                  ON previous_movement.id = inventory_movements.previous_movement_id
-
-                WHERE previous_movement.quantity_after != inventory_movements.quantity_before
-                    AND inventory_movements.previous_movement_id IS NOT NULL
-                LIMIT 500
+                 AND inventory_movements.quantity_before != previous_movement.quantity_after
+                LIMIT 200
             )
 
             UPDATE inventory_movements
@@ -46,7 +61,8 @@ class QuantityBeforeJob implements ShouldQueue, ShouldBeUnique
                 tbl.movement_id = inventory_movements.id
             SET
                 inventory_movements.quantity_before = tbl.quantity_before_expected,
-                inventory_movements.updated_at = now()
+                inventory_movements.quantity_delta = tbl.quantity_delta_expected,
+                inventory_movements.quantity_after = tbl.quantity_after_expected;
             ');
             sleep(1);
             $maxRounds--;
--- app/Modules/InventoryMovements/src/Listeners/EveryTenMinutesEventListener.php
+++ app/Modules/InventoryMovements/src/Listeners/EveryTenMinutesEventListener.php
@@ -5,9 +5,7 @@ namespace App\Modules\InventoryMovements\src\Listeners;
 use App\Modules\InventoryMovements\src\Jobs\InventoryLastMovementIdJob;
 use App\Modules\InventoryMovements\src\Jobs\InventoryQuantityJob;
 use App\Modules\InventoryMovements\src\Jobs\PreviousMovementIdJob;
-use App\Modules\InventoryMovements\src\Jobs\QuantityAfterJob;
 use App\Modules\InventoryMovements\src\Jobs\QuantityBeforeJob;
-use App\Modules\InventoryMovements\src\Jobs\QuantityDeltaJob;
 
 class EveryTenMinutesEventListener
 {
@@ -15,8 +13,6 @@ class EveryTenMinutesEventListener
     {
         PreviousMovementIdJob::dispatch();
         QuantityBeforeJob::dispatch();
-        QuantityDeltaJob::dispatch();
-        QuantityAfterJob::dispatch();
         InventoryLastMovementIdJob::dispatch();
         InventoryQuantityJob::dispatch();
     }
