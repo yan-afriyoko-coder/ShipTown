66aace56802bac82a102d43cdf8b3d56fec35e34
arthur@coding.cab <arthur@youritsolutions.ie>
2023-08-28 17:10:20 +0200

    improve job

--- app/Modules/InventoryMovements/src/Jobs/QuantityBeforeJob.php
+++ app/Modules/InventoryMovements/src/Jobs/QuantityBeforeJob.php
@@ -25,11 +25,13 @@ class QuantityBeforeJob extends UniqueJob
             }
 
             $recordsUpdated = DB::update('
+                SET @min_last_movement_id = ?;
+
                 WITH tbl AS (
                     SELECT
                         inventory_movements.created_at as created_at,
                         inventory_movements.id as movement_id,
-                        inventory_movements.description as description,
+                        inventory_movements.type as type,
                         inventory_movements.inventory_id as inventory_id,
                         previous_movement.quantity_after - inventory_movements.quantity_before as quantity_before_delta
 
@@ -38,22 +40,23 @@ class QuantityBeforeJob extends UniqueJob
                      ON previous_movement.id = inventory_movements.previous_movement_id
                      AND inventory_movements.quantity_before != previous_movement.quantity_after
 
-                    WHERE inventory_movements.id >= IFNULL(?, 0)
+                    WHERE inventory_movements.id >= IFNULL(@min_last_movement_id, 0)
                     LIMIT 1
                 )
 
                 UPDATE inventory_movements
                 INNER JOIN tbl ON
-                    tbl.movement_id = inventory_movements.id
+                    tbl.inventory_id = inventory_movements.inventory_id
+                    AND tbl.movement_id <= inventory_movements.id
                 SET
                     inventory_movements.quantity_before = inventory_movements.quantity_before + tbl.quantity_before_delta,
 
-                    inventory_movements.quantity_delta = CASE WHEN inventory_movements.description = "stocktake"
+                    inventory_movements.quantity_delta = CASE WHEN inventory_movements.type = "stocktake"
                             THEN inventory_movements.quantity_delta - tbl.quantity_before_delta
                             ElSE inventory_movements.quantity_delta
                             END,
 
-                    inventory_movements.quantity_after = CASE WHEN inventory_movements.description = "stocktake"
+                    inventory_movements.quantity_after = CASE WHEN inventory_movements.type = "stocktake"
                             THEN inventory_movements.quantity_after
                             ELSE inventory_movements.quantity_after + tbl.quantity_before_delta
                             END
