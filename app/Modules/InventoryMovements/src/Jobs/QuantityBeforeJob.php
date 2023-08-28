cf87c906e5e8d7411ba25f129e175c2bd30e8555
arthur@coding.cab <arthur@youritsolutions.ie>
2023-08-28 18:37:59 +0200

    remove limit

--- app/Modules/InventoryMovements/src/Jobs/QuantityBeforeJob.php
+++ app/Modules/InventoryMovements/src/Jobs/QuantityBeforeJob.php
@@ -54,7 +54,6 @@ class QuantityBeforeJob extends UniqueJob
                             THEN inventory_movements.quantity_after
                             ELSE inventory_movements.quantity_after + tbl.quantity_before_delta
                             END
-                LIMIT 5000
             ', [$minMovementId]);
             sleep(1);
         } while ($recordsUpdated > 0);
