0b58ca1c880a070a30b6196dd3f959cee8d0211b
arthur@coding.cab <arthur@youritsolutions.ie>
2023-08-30 12:58:10 +0200

    remove limit

--- app/Modules/InventoryMovements/src/Jobs/InventoryQuantityJob.php
+++ app/Modules/InventoryMovements/src/Jobs/InventoryQuantityJob.php
@@ -13,21 +13,19 @@ class InventoryQuantityJob extends UniqueJob
 
         do {
             $recordsUpdated = DB::update('
-                UPDATE inventory
+                    UPDATE inventory
 
-                INNER JOIN inventory_movements
-                    ON inventory_movements.id = inventory.last_movement_id
-                    AND (
-                        inventory_movements.quantity_after != inventory.quantity
-                        OR inventory_movements.created_at != inventory.last_movement_at
-                    )
+                    INNER JOIN inventory_movements
+                        ON inventory_movements.id = inventory.last_movement_id
+                        AND (
+                            inventory_movements.quantity_after != inventory.quantity
+                            OR inventory_movements.created_at != inventory.last_movement_at
+                        )
 
-                SET
-                      inventory.quantity = inventory_movements.quantity_after
-                    , inventory.last_movement_at = inventory_movements.created_at
-                    , inventory.updated_at = now()
-
-                LIMIT 500
+                    SET
+                          inventory.quantity = inventory_movements.quantity_after
+                        , inventory.last_movement_at = inventory_movements.created_at
+                        , inventory.updated_at = now()
             ');
             sleep(1);
             $maxRounds--;
