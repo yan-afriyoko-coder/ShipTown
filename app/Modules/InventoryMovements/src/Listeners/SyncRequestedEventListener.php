d4c130224b831f663e76e44e001ace7bc49a3d49
arthur@coding.cab <arthur@youritsolutions.ie>
2023-08-31 03:00:00 +0200

    add job to SyncRequest

--- app/Modules/InventoryMovements/src/Listeners/SyncRequestedEventListener.php
+++ app/Modules/InventoryMovements/src/Listeners/SyncRequestedEventListener.php
@@ -2,18 +2,14 @@
 
 namespace App\Modules\InventoryMovements\src\Listeners;
 
-use App\Modules\InventoryMovements\src\Jobs\InventoryLastMovementIdJob;
-use App\Modules\InventoryMovements\src\Jobs\InventoryQuantityJob;
 use App\Modules\InventoryMovements\src\Jobs\PreviousMovementIdJob;
-use App\Modules\InventoryMovements\src\Jobs\QuantityAfterJob;
 use App\Modules\InventoryMovements\src\Jobs\QuantityBeforeJob;
-use App\Modules\InventoryMovements\src\Jobs\QuantityDeltaJob;
 
 class SyncRequestedEventListener
 {
     public function handle()
     {
-//        PreviousMovementIdJob::dispatch();
+        PreviousMovementIdJob::dispatch();
         QuantityBeforeJob::dispatch();
 //        QuantityDeltaJob::dispatch();
 //        QuantityAfterJob::dispatch();
