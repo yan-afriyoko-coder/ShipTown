e60ed4352645756cc98c5400dc94dfc72ef2154a
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-04 19:05:01 +0200

    add rounding to IsFullyPaidCondition

--- app/Modules/Automations/src/Conditions/Order/IsFullyPaidCondition.php
+++ app/Modules/Automations/src/Conditions/Order/IsFullyPaidCondition.php
@@ -21,9 +21,9 @@ class IsFullyPaidCondition extends BaseOrderConditionAbstract
 
         return $query->whereHas('orderProductsTotals', function (Builder $query) use ($expectedBoolValue) {
             $query->whereRaw('(
-                    ((total_products > 0) OR (total_paid > 0))
-                    AND ((total_paid > 0) OR (total_discounts > 0))
-                    AND ((total_paid + total_discounts) >= total_price + total_shipping)
+                    ((total_products > 0) OR (round(total_paid, 2) > 0))
+                    AND ((round(total_paid, 2) > 0) OR (total_discounts > 0))
+                    AND ((round(total_paid, 2) + total_discounts) >= total_price + total_shipping)
                 ) = ?
                 ', [$expectedBoolValue]);
         });
