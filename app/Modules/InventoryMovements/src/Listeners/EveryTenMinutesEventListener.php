c16babb4aee9c34c2d67c69422d3350925a9c5f4
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-05 20:32:37 +0200

    refactoring

--- app/Modules/InventoryMovements/src/Listeners/EveryTenMinutesEventListener.php
+++ app/Modules/InventoryMovements/src/Listeners/EveryTenMinutesEventListener.php
@@ -5,7 +5,6 @@ namespace App\Modules\InventoryMovements\src\Listeners;
 use App\Modules\InventoryMovements\src\Jobs\FirstMovementAtJob;
 use App\Modules\InventoryMovements\src\Jobs\PreviousMovementIdJob;
 use App\Modules\InventoryMovements\src\Jobs\QuantityBeforeJob;
-use App\Modules\InventoryMovements\src\Jobs\QuantityBeforeSinceLastStocktakeJob;
 use App\Modules\InventoryMovements\src\Jobs\QuantityBeforeStocktakeJob;
 
 class EveryTenMinutesEventListener
