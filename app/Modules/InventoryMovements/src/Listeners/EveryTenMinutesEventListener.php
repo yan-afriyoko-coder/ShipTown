0695c26cb0e09a5f2ac71ce47ebbf6547c97630d
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-05 23:24:31 +0200

    add job to every ten minutes schedule

--- app/Modules/InventoryMovements/src/Listeners/EveryTenMinutesEventListener.php
+++ app/Modules/InventoryMovements/src/Listeners/EveryTenMinutesEventListener.php
@@ -4,6 +4,7 @@ namespace App\Modules\InventoryMovements\src\Listeners;
 
 use App\Modules\InventoryMovements\src\Jobs\FirstMovementAtJob;
 use App\Modules\InventoryMovements\src\Jobs\PreviousMovementIdJob;
+use App\Modules\InventoryMovements\src\Jobs\QuantityBeforeBasicJob;
 use App\Modules\InventoryMovements\src\Jobs\QuantityBeforeJob;
 use App\Modules\InventoryMovements\src\Jobs\QuantityBeforeStocktakeJob;
 
@@ -11,6 +12,7 @@ class EveryTenMinutesEventListener
 {
     public function handle()
     {
+        QuantityBeforeBasicJob::dispatch();
         QuantityBeforeJob::dispatch();
         PreviousMovementIdJob::dispatch();
         QuantityBeforeStocktakeJob::dispatch();
