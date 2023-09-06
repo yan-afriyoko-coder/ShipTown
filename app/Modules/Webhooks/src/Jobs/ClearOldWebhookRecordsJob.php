fc8309a1dbb976f84e669dcc6ceb0d4673502498
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-06 14:09:13 +0200

    improve job

--- app/Modules/Webhooks/src/Jobs/ClearOldWebhookRecordsJob.php
+++ app/Modules/Webhooks/src/Jobs/ClearOldWebhookRecordsJob.php
@@ -12,11 +12,16 @@ class ClearOldWebhookRecordsJob extends UniqueJob
 {
     public function handle()
     {
+        $lastToDelete = PendingWebhook::query()
+            ->where('created_at', '<', now()->subDays(7))
+            ->latest('id')
+            ->first();
+
         do {
             $recordsUpdated = PendingWebhook::query()
-                ->where('created_at', '<', now()->subDays(7))
+                ->where('id', '<', $lastToDelete->getKey())
                 ->whereNotNull('published_at')
-                ->limit(10000)
+                ->limit(50000)
                 ->forceDelete();
             sleep(1);
         } while ($recordsUpdated > 0);
