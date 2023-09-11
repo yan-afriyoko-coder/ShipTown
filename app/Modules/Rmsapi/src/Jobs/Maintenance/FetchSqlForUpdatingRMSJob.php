baec451c653e13557d44d41af783932c3cc307af
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-11 18:13:05 +0200

    update queries

--- app/Modules/Rmsapi/src/Jobs/Maintenance/FetchSqlForUpdatingRMSJob.php
+++ app/Modules/Rmsapi/src/Jobs/Maintenance/FetchSqlForUpdatingRMSJob.php
@@ -10,26 +10,37 @@ class FetchSqlForUpdatingRMSJob extends UniqueJob
     public function handle()
     {
         DB::statement("
-            SELECT CONCAT('UPDATE Item SET Quantity=', pm_quantity, ', LastUpdated=getDate() WHERE ItemLookupCode = ''', product_sku,'''; --', warehouse_code)
+            SELECT
+                modules_rmsapi_products_quantity_comparison_view.*, inventory.updated_at as inventory_last_updated_at
+                #CONCAT('UPDATE Item SET Quantity=', pm_quantity, ', LastUpdated=getDate() WHERE ItemLookupCode = ''', product_sku,'''; --', warehouse_code)
             FROM `modules_rmsapi_products_quantity_comparison_view`
+            LEFT JOIN inventory
+              ON inventory.id = modules_rmsapi_products_quantity_comparison_view.inventory_id
+            LEFT JOIN modules_magento2api_products
             WHERE rms_quantity != pm_quantity
-            ORDER BY warehouse_code
+            ORDER BY inventory_last_updated_at, warehouse_code
             LIMIT 500
         ");
 
         DB::statement("
-            SELECT DISTINCT CONCAT('UPDATE Item SET LastUpdated=getDate() WHERE ItemLookupCode=''',products.sku,'''; --', inventory.warehouse_code)
+            SELECT
+                inventory.warehouse_code, products.sku, products.name, 'Products never synced with RMS, run query below' as whats_this
+                #DISTINCT CONCAT('UPDATE Item SET LastUpdated=getDate() WHERE ItemLookupCode=''',products.sku,'''; --', inventory.warehouse_code)
+
             FROM inventory
 
             LEFT JOIN modules_rmsapi_products_imports
-             ON modules_rmsapi_products_imports.warehouse_id = inventory.warehouse_id
-             AND modules_rmsapi_products_imports.product_id = inventory.product_id
+                ON modules_rmsapi_products_imports.warehouse_id = inventory.warehouse_id
+                AND modules_rmsapi_products_imports.product_id = inventory.product_id
 
             LEFT JOIN products
-              ON products.id = inventory.product_id
+                ON products.id = inventory.product_id
 
             WHERE modules_rmsapi_products_imports.id is null
-            and inventory.quantity != 0
+                AND inventory.quantity != 0
+                AND inventory.warehouse_code not in ('LTS')
+
+            ORDER BY inventory.warehouse_id, products.sku
 
             LIMIT 5000
         ");
