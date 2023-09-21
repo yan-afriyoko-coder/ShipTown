c6574300ea503f168e2d378ae474c8534836e26a
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-21 11:44:23 +0200

    add duration

--- app/Modules/QueueMonitor/src/Listeners/JobProcessingListener.php
+++ app/Modules/QueueMonitor/src/Listeners/JobProcessingListener.php
@@ -3,18 +3,24 @@
 namespace App\Modules\QueueMonitor\src\Listeners;
 
 use App\Modules\QueueMonitor\src\QueueMonitorServiceProvider;
+use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
 class JobProcessingListener
 {
-    public function handle($event)
+    public function handle(JobProcessing $event)
     {
+        $key = implode('-', ['job_start_time', $event->job->uuid()]);
+
+        Cache::set($key, now(), 3600);
+
         $jobName = data_get($event->job->payload(), 'displayName');
 
         if (in_array($jobName, QueueMonitorServiceProvider::$ignoredJobList)) {
             return;
         }
 
-        Log::debug('Joh processing', ['job' => $jobName]);
+        Log::debug('Job processing', ['job' => $jobName]);
     }
 }
