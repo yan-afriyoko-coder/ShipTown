c1e5d7aec69420dbcefe05f26e5f81d83f716300
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-21 13:05:25 +0200

    optimize job

--- app/Modules/InventoryMovements/src/Jobs/FirstMovementAtJob.php
+++ app/Modules/InventoryMovements/src/Jobs/FirstMovementAtJob.php
@@ -16,13 +16,12 @@ class FirstMovementAtJob extends UniqueJob
             $recordsUpdated = DB::update('
                 WITH tempTable AS (
                     SELECT DISTINCT inventory.id as inventory_id
-                    FROM  inventory
+                    FROM inventory
 
-                    INNER JOIN inventory_movements
-                        ON inventory_movements.inventory_id = inventory.id
-                        AND inventory_movements.created_at < IFNULL(inventory.first_movement_at, now())
+                    WHERE
+                        inventory.last_movement_id IS NOT NULL
+                        AND inventory.first_movement_at IS NULL
 
-                    WHERE inventory.last_movement_id IS NOT NULL
                     LIMIT 500
                 )
 
