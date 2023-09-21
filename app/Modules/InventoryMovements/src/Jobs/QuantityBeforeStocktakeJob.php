f591c2824f07ae582847cee97fce11cc915fb086
arthur@coding.cab <arthur@youritsolutions.ie>
2023-09-21 15:04:33 +0200

    rebuild jobs

--- app/Modules/InventoryMovements/src/Jobs/QuantityBeforeStocktakeJob.php
+++ app/Modules/InventoryMovements/src/Jobs/QuantityBeforeStocktakeJob.php
@@ -3,6 +3,8 @@
 namespace App\Modules\InventoryMovements\src\Jobs;
 
 use App\Abstracts\UniqueJob;
+use App\Models\InventoryMovement;
+use App\Modules\InventoryMovements\src\Models\Configuration;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
@@ -11,14 +13,37 @@ class QuantityBeforeStocktakeJob extends UniqueJob
 {
     public function handle()
     {
-        $maxRounds = 50;
+        /** @var Configuration $configuration */
+        $configuration = Configuration::query()->firstOrCreate();
+        $lastMovementId = data_get(InventoryMovement::query()->latest('id')->first('id'), 'id', 0);
+
+        $minMovementId = $configuration->quantity_before_stocktake_job_last_movement_id_checked ?? 0;
 
         do {
-            $maxRounds--;
+            $maxMovementId = min($minMovementId + 10000, $lastMovementId);
 
-            Schema::dropIfExists('tempTable');
+            do {
+                $recordsUpdated = $this->getRecordsUpdated($minMovementId, $maxMovementId);
 
-            DB::statement('
+                Log::info('Job processing', [
+                    'job' => self::class,
+                    'records_updated' => $recordsUpdated
+                ]);
+            } while ($recordsUpdated > 0);
+
+            $configuration->update(['quantity_before_stocktake_job_last_movement_id_checked' => $maxMovementId]);
+
+            $minMovementId = $maxMovementId;
+
+            usleep(400000); // 0.4 seconds
+        } while ($minMovementId < $lastMovementId);
+    }
+
+    private function getRecordsUpdated(int $minMovementId, int $maxMovementId): int
+    {
+        Schema::dropIfExists('tempTable');
+
+        DB::statement('
                 CREATE TEMPORARY TABLE tempTable AS
                     SELECT
                        inventory_movements.id as movement_id,
@@ -33,13 +58,14 @@ class QuantityBeforeStocktakeJob extends UniqueJob
                     LEFT JOIN inventory_movements as previous_movement
                       ON previous_movement.id = inventory_movements.previous_movement_id
 
-                    WHERE inventory_movements.type = "stocktake"
+                    WHERE inventory_movements.id BETWEEN ? AND ?
+                    AND inventory_movements.type = "stocktake"
                     AND previous_movement.quantity_after != inventory_movements.quantity_before
 
                     LIMIT 1000;
-            ');
+            ', [$minMovementId, $maxMovementId]);
 
-            $recordsUpdated = DB::update('
+        return DB::update('
                 UPDATE inventory_movements
                 INNER JOIN tempTable
                   ON tempTable.movement_id = inventory_movements.id
@@ -48,13 +74,5 @@ class QuantityBeforeStocktakeJob extends UniqueJob
                     inventory_movements.quantity_delta = tempTable.quantity_delta_expected,
                     inventory_movements.updated_at = NOW()
             ');
-
-            Log::info('Job processing', [
-                'job' => self::class,
-                'recordsUpdated' => $recordsUpdated
-            ]);
-
-            sleep(1);
-        } while ($recordsUpdated > 0 && $maxRounds > 0);
     }
 }
