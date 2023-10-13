1d8f330749f3a9771b5751559ece9ab08b88cd1a
arthur@coding.cab <arthur@youritsolutions.ie>
2023-10-13 19:42:29 +0200

    fix automation

--- app/Modules/Automations/src/Conditions/Order/ShippingAddressCountryCodeInCondition.php
+++ app/Modules/Automations/src/Conditions/Order/ShippingAddressCountryCodeInCondition.php
@@ -10,7 +10,7 @@ class ShippingAddressCountryCodeInCondition extends BaseOrderConditionAbstract
     public static function addQueryScope(Builder $query, $expected_value): Builder
     {
         return $query->whereHas('shippingAddress', function (Builder $query) use ($expected_value) {
-            $query->whereIn('country_code', implode(',', $expected_value));
+            $query->whereIn('country_code', explode(',', $expected_value));
         });
     }
 }
--- app/Modules/Automations/src/Conditions/Order/ShippingAddressCountryCodeNotInCondition.php
+++ app/Modules/Automations/src/Conditions/Order/ShippingAddressCountryCodeNotInCondition.php
@@ -10,7 +10,7 @@ class ShippingAddressCountryCodeNotInCondition extends BaseOrderConditionAbstrac
     public static function addQueryScope(Builder $query, $expected_value): Builder
     {
         return $query->whereHas('shippingAddress', function (Builder $query) use ($expected_value) {
-            $query->whereNotIn('country_code', implode(',', $expected_value));
+            $query->whereNotIn('country_code', explode(',', $expected_value));
         });
     }
 }
