c30f89e4e85ccd1e2c39ed2d13a9a667880f1f8c
arthur@coding.cab <arthur@youritsolutions.ie>
2023-10-19 22:05:39 +0200

    add update last_movement_id script

--- app/Modules/Rmsapi/src/Jobs/Maintenance/ResyncStockJob.php
+++ app/Modules/Rmsapi/src/Jobs/Maintenance/ResyncStockJob.php
@@ -40,19 +40,31 @@ class ResyncStockJob extends UniqueJob
             AND DATEDIFF(now(), modules_rmsapi_products_imports_updated_at) > 1;
 
 
+            WITH tbl AS (
+                SELECT
+                    inventory.id as inventory_id,
+                    max(inventory_movements.id) as last_movement_id
+
+                FROM inventory
+                INNER JOIN inventory_movements
+                  ON inventory_movements.inventory_id = inventory.id
+                  AND inventory_movements.id > IFNULL(inventory.last_movement_id, 0)
+
+                GROUP BY inventory.id
+
+                LIMIT 50000
+            )
+
             UPDATE inventory
-
+            INNER JOIN tbl
+             ON tbl.inventory_id = inventory.id
             INNER JOIN inventory_movements
-                ON inventory_movements.id = inventory.last_movement_id
-                AND (
-                    inventory_movements.quantity_after != inventory.quantity
-                    OR inventory_movements.created_at != inventory.last_movement_at
-                )
-
+             ON inventory_movements.id = tbl.last_movement_id
             SET
-                  inventory.quantity = inventory_movements.quantity_after
-                , inventory.last_movement_at = inventory_movements.created_at
-                , inventory.updated_at = now()
+                inventory.quantity = inventory_movements.quantity_after,
+                inventory.last_movement_id = tbl.last_movement_id,
+                inventory.last_movement_at = inventory_movements.created_at,
+                inventory.updated_at = now();
         ');
     }
 }
