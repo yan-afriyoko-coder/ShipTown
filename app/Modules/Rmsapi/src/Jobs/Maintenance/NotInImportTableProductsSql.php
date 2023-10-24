b08262453deb1fd28fce8b84b5410f3c1413bef4
arthur@coding.cab <arthur@youritsolutions.ie>
2023-10-24 13:48:40 +0200

    update script

--- app/Modules/Rmsapi/src/Jobs/Maintenance/NotInImportTableProductsSql.php
+++ app/Modules/Rmsapi/src/Jobs/Maintenance/NotInImportTableProductsSql.php
@@ -11,15 +11,27 @@ class NotInImportTableProductsSql extends UniqueJob
     {
         // WARNING: Use with caution! It will override the inventory
         DB::statement("
-            SELECT concat('UPDATE Item SET LastUpdated = getDate() WHERE ItemLookupCode=''', products.sku, '''')
-            FROM products
+            CREATE TEMPORARY TABLE aTemp_ProductsDuplicated AS
+                SELECT products.id as product_id, products.sku, concat('UPDATE Item SET LastUpdated = getDate() WHERE ItemLookupCode=''', products.sku, '''') as RMS_SQL
+                FROM products
 
-            LEFT JOIN modules_rmsapi_products_imports
-              ON modules_rmsapi_products_imports.product_id = products.id
+                LEFT JOIN modules_rmsapi_products_imports
+                  ON modules_rmsapi_products_imports.product_id = products.id
 
-            WHERE modules_rmsapi_products_imports.id IS NULL
+                WHERE modules_rmsapi_products_imports.id IS NULL
 
-            LIMIT 50000
+                LIMIT 50000;
+
+                SELECT * FROM products
+
+                ##UPDATE products
+
+                INNER JOIN aTemp_ProductsDuplicated
+                  ON aTemp_ProductsDuplicated.product_id = products.id
+
+                ##SET products.sku = concat('sku_removed_','id_', products.id)
+
+                LIMIT 500
         ");
     }
 }
