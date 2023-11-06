3d9e2e981116c1343e3f5ee47825b5e0eb0bbb71
arthur@coding.cab <arthur@youritsolutions.ie>
2023-11-06 15:30:49 +0100

    clarify using comments

--- database/seeders/Demo/OrderProductNotExistsSeeder.php
+++ database/seeders/Demo/OrderProductNotExistsSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders\Demo;
 
 use App\Models\Order;
+use App\Models\OrderComment;
 use App\Models\OrderProduct;
+use App\Services\OrderService;
 use Illuminate\Database\Seeder;
 
 class OrderProductNotExistsSeeder extends Seeder
@@ -11,6 +13,16 @@ class OrderProductNotExistsSeeder extends Seeder
     public function run()
     {
         $order = Order::factory()->create(['order_number' => 'T100001', 'status_code' => 'paid']);
+
+        OrderComment::create([
+            'order_id' => $order->getKey(),
+            'comment' => 'Product with SKU ordered does not exist in the system. This simulates scenario when product exists in remote system (Magento, Shopify etc) but no in our system.'
+        ]);
+
+        OrderComment::create([
+            'order_id' => $order->getKey(),
+            'comment' => 'Test order'
+        ]);
 
         OrderProduct::factory()->create([
             'order_id' => $order->getKey(),
