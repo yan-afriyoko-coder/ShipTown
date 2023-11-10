8a31d5ef1eb4b564ae8a87635e93783bdd59682f
arthur@coding.cab <arthur@youritsolutions.ie>
2023-11-10 14:26:42 +0100

    refactoring

--- app/Modules/Api2cart/src/Jobs/ProcessImportedOrdersJob.php
+++ app/Modules/Api2cart/src/Jobs/ProcessImportedOrdersJob.php
@@ -42,19 +42,24 @@ class ProcessImportedOrdersJob implements ShouldQueue
      */
     private function processOrder(Api2cartOrderImports $orderImport): void
     {
+        $data = $orderImport->raw_import;
+
+        $uuid = implode('-', ['module-api2cart-connection-id', $orderImport->connection_id, 'remote-record-id', data_get($data, 'order_id')]);
+
         $orderAttributes = [
-            'order_number' => $orderImport->raw_import['id'],
-            'total_products' => data_get($orderImport->raw_import, 'totals.subtotal'),
-            'total_shipping' => $orderImport->raw_import['totals']['shipping'] ?? 0,
-            'total_discounts' => $orderImport->raw_import['totals']['discount'] ?? 0,
-            'total' => $orderImport->raw_import['total']['total'] ?? 0,
-            'total_paid' => $orderImport->raw_import['total']['total_paid'] ?? 0,
-            'shipping_method_code' => $orderImport->shipping_method_code,
-            'shipping_method_name' => $orderImport->shipping_method_name,
-            'order_placed_at' => $orderImport->ordersCreateAt()->tz('UTC'),
-            'order_products' => $orderImport->extractOrderProducts(),
-            'shipping_address' => $orderImport->extractShippingAddressAttributes(),
-            'raw_import' => $orderImport->raw_import,
+            'custom_unique_reference_id' => $uuid,
+            'order_number'               => data_get($data, 'id'),
+            'total'                      => data_get($data, 'totals.total', 0),
+            'total_products'             => data_get($data, 'totals.subtotal'),
+            'total_shipping'             => data_get($data, 'totals.shipping', 0),
+            'total_discounts'            => data_get($data, 'totals.discount', 0),
+            'total_paid'                 => data_get($data, 'total.total_paid', 0),
+            'shipping_method_name'       => data_get($data, 'shipping_method.name', 0),
+            'shipping_method_code'       => data_get($data, 'shipping_method.additional_fields.code', 0),
+            'order_placed_at'            => $orderImport->ordersCreateAt()->tz('UTC'),
+            'order_products'             => $orderImport->extractOrderProducts(),
+            'shipping_address'           => $orderImport->extractShippingAddressAttributes(),
+            'raw_import'                 => $data,
         ];
 
         $order = OrderService::updateOrCreate($orderAttributes);
