c18b48d91d62335277bda70e9acc4e89faa7af92
arthur@coding.cab <arthur@youritsolutions.ie>
2023-11-13 10:46:38 +0100

    finish SequenceNumberJob

--- app/Modules/InventoryMovements/src/Jobs/SequenceNumberJob.php
+++ app/Modules/InventoryMovements/src/Jobs/SequenceNumberJob.php
@@ -17,41 +17,61 @@ class SequenceNumberJob extends UniqueJob
             Schema::dropIfExists('tempTable');
 
             DB::statement('
-CREATE TEMPORARY TABLE tempTable AS
-SELECT
-    id as movement_id,
-    tempTable2.inventory_id,
-    (SELECT sequence_number FROM inventory_movements as im_table WHERE im_table.inventory_id = tempTable2.inventory_id AND im_table.sequence_number IS NOT NULL ORDER BY im_table.sequence_number DESC LIMIT 1) as max_sequence_number,
-    row_number() over (partition by inventory_id order by occurred_at) as sequence_number,
-    (sum(quantity_delta) over (partition by inventory_id order by occurred_at asc, id asc)) as quantity_delta_sum
-FROM (SELECT
-          inventory_movements.*
-      FROM inventory_movements
-      WHERE sequence_number IS NULL
-      LIMIT 1000
-     ) tempTable2;
-
-# SELECT * FROM tempTable;
-
+                CREATE TEMPORARY TABLE tempTable AS
+                SELECT
+                    id as movement_id,
+                    tempTable2.inventory_id,
+                    (SELECT sequence_number FROM inventory_movements as im_table WHERE im_table.inventory_id = tempTable2.inventory_id AND im_table.sequence_number IS NOT NULL ORDER BY im_table.sequence_number DESC LIMIT 1) as max_sequence_number,
+                    row_number() over (partition by inventory_id order by occurred_at) as sequence_number,
+                    (sum(quantity_delta) over (partition by inventory_id order by occurred_at asc, id asc)) as quantity_delta_sum
+                FROM (
+                    SELECT inventory_movements.*
+                    FROM inventory_movements
+                    WHERE sequence_number IS NULL
+                    LIMIT 500
+                ) tempTable2;
             ');
 
             $recordsUpdated = DB::update('
-UPDATE inventory_movements
-    INNER JOIN tempTable
-    ON inventory_movements.id = tempTable.movement_id
+                UPDATE inventory_movements
+                    INNER JOIN tempTable
+                    ON inventory_movements.id = tempTable.movement_id
 
-    LEFT JOIN inventory_movements as last_sequenced_movement
-    ON last_sequenced_movement.inventory_id = inventory_movements.inventory_id
-        AND last_sequenced_movement.sequence_number = max_sequence_number
+                    LEFT JOIN inventory_movements as last_sequenced_movement
+                    ON last_sequenced_movement.inventory_id = inventory_movements.inventory_id
+                        AND last_sequenced_movement.sequence_number = max_sequence_number
 
-SET
-    inventory_movements.sequence_number = tempTable.sequence_number + IFNULL(tempTable.max_sequence_number, 0),
-    inventory_movements.quantity_before = IFNULL(last_sequenced_movement.quantity_after, 0) + quantity_delta_sum - inventory_movements.quantity_delta,
-    inventory_movements.quantity_after = CASE
-        WHEN inventory_movements.type = "stocktake"
-        THEN inventory_movements.quantity_after
-        ELSE IFNULL(last_sequenced_movement.quantity_after, 0) + quantity_delta_sum
-    END;
+                SET
+                    inventory_movements.sequence_number = tempTable.sequence_number + IFNULL(tempTable.max_sequence_number, 0),
+                    inventory_movements.quantity_before = IFNULL(last_sequenced_movement.quantity_after, 0) + quantity_delta_sum - inventory_movements.quantity_delta,
+                    inventory_movements.quantity_after = CASE
+                        WHEN inventory_movements.type = "stocktake"
+                        THEN inventory_movements.quantity_after
+                        ELSE IFNULL(last_sequenced_movement.quantity_after, 0) + quantity_delta_sum
+                    END;
+            ');
+
+            DB::update('
+                UPDATE inventory
+
+                SET
+                    inventory.last_sequence_number = (SELECT MAX(sequence_number) FROM inventory_movements WHERE inventory_id = inventory.id AND sequence_number > IFNULL(inventory.last_sequence_number, 0))
+
+                WHERE inventory.id IN (SELECT inventory_id FROM tempTable);
+            ');
+
+            DB::update('
+                UPDATE inventory
+
+                    INNER JOIN inventory_movements
+                    ON inventory_movements.inventory_id = inventory.id
+                        AND inventory_movements.sequence_number = inventory.last_sequence_number
+
+                SET inventory.last_movement_id = inventory_movements.id,
+                    inventory.last_movement_at = inventory_movements.occurred_at,
+                    inventory.quantity = inventory_movements.quantity_after
+
+                WHERE inventory.id IN (SELECT inventory_id FROM tempTable);
             ');
 
             Log::info('Job processing', [
