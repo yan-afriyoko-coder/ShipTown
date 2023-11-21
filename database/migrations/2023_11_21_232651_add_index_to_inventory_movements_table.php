374ad50aa1b5b4971956e55c0d6426fa5150a634
arthur@coding.cab <arthur@youritsolutions.ie>
2023-11-22 00:29:46 +0100

    add index to inventory_movements_table

---
+++ database/migrations/2023_11_21_232651_add_index_to_inventory_movements_table.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('inventory_movements', function (Blueprint $table) {
+            $table->index(['occurred_at DESC', 'sequence_number DESC','id DESC']);
+        });
+    }
+};
