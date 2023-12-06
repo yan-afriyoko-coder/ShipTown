c1145fadda9609e48c5c80b8745a1cb07ea51b53
arthur@coding.cab <arthur@youritsolutions.ie>
2023-12-06 18:13:03 +0100

    remove cache files after read

--- app/Modules/QueueMonitor/src/Listeners/JobFailedListener.php
+++ app/Modules/QueueMonitor/src/Listeners/JobFailedListener.php
@@ -4,18 +4,23 @@ namespace App\Modules\QueueMonitor\src\Listeners;
 
 use App\Modules\QueueMonitor\src\QueueMonitorServiceProvider;
 use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 
 class JobFailedListener
 {
     public function handle(JobFailed $event)
     {
-        $jobName = $event->job->resolveName();
+        $jobName = data_get($event->job->payload(), 'displayName');
 
         if (in_array($jobName, QueueMonitorServiceProvider::$ignoredJobList)) {
             return;
         }
 
-        Log::info('Job failed', ['job' => $jobName, 'exception' => $event->exception]);
+        $key = implode('-', ['job_start_time', $event->job->uuid()]);
+
+        $jobStartTime = Cache::pull($key);
+
+        Log::info('Job failed', ['job' => $jobName, 'duration' => now()->diffInSeconds($jobStartTime), 'exception' => $event->exception]);
     }
 }
