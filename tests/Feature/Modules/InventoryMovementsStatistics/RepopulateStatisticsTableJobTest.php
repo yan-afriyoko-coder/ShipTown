c872a251b5946349eb13d5a6e81d1801a4751acc
Artur Hanusek <arthur@youritsolutions.ie>
2024-01-07 17:22:45 +0100

    add date

--- tests/Feature/Modules/InventoryMovementsStatistics/RepopulateStatisticsTableJobTest.php
+++ tests/Feature/Modules/InventoryMovementsStatistics/RepopulateStatisticsTableJobTest.php
@@ -12,14 +12,14 @@ class RepopulateStatisticsTableJobTest extends TestCase
     public function testBasic()
     {
         $product1 = Product::factory()->create();
-        InventoryMovement::factory()->create(['product_id' => $product1->getKey(), 'type' => 'sale']);
-        InventoryMovement::factory()->create(['product_id' => $product1->getKey(), 'type' => 'sale']);
-        InventoryMovement::factory()->create(['product_id' => $product1->getKey(), 'type' => 'sale']);
+        InventoryMovement::factory()->create(['product_id' => $product1->getKey(), 'type' => 'sale', 'occurred_at' => now()->subDays(2)]);
+        InventoryMovement::factory()->create(['product_id' => $product1->getKey(), 'type' => 'sale', 'occurred_at' => now()->subDays(2)]);
+        InventoryMovement::factory()->create(['product_id' => $product1->getKey(), 'type' => 'sale', 'occurred_at' => now()->subDays(2)]);
 
         $product2 = Product::factory()->create();
-        InventoryMovement::factory()->create(['product_id' => $product2->getKey(), 'type' => 'sale']);
-        InventoryMovement::factory()->create(['product_id' => $product2->getKey(), 'type' => 'sale']);
-        InventoryMovement::factory()->create(['product_id' => $product2->getKey(), 'type' => 'sale']);
+        InventoryMovement::factory()->create(['product_id' => $product2->getKey(), 'type' => 'sale', 'occurred_at' => now()->subDays(2)]);
+        InventoryMovement::factory()->create(['product_id' => $product2->getKey(), 'type' => 'sale', 'occurred_at' => now()->subDays(2)]);
+        InventoryMovement::factory()->create(['product_id' => $product2->getKey(), 'type' => 'sale', 'occurred_at' => now()->subDays(2)]);
 
         RecalculateStatisticsTableJob::dispatch();
 
