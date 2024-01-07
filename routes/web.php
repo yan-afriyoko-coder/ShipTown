f70b2a69aaec8d7c76e62609c8f33d9b5ff3d170
arthur@coding.cab <arthur@youritsolutions.ie>
2024-01-07 13:32:09 +0100

    routes refactoring

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@
 |
 */
 
+use App\Http\Controllers\Admin\Settings\Modules\Webhooks\SubscriptionController;
 use App\Http\Controllers\Auth;
+use App\Http\Controllers\MailTemplatePreviewController;
 use App\Http\Controllers\Order;
 use App\Http\Controllers\Csv;
 use App\Http\Controllers\DashboardController;
@@ -22,10 +24,10 @@ use App\Http\Controllers\Reports;
 use App\Http\Controllers\ShippingLabelController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('verify', Auth\TwoFactorController::class)->only(['index', 'store']);
-
 Route::redirect('/', 'dashboard');
 Route::redirect('home', '/')->name('home');
+
+Route::resource('verify', Auth\TwoFactorController::class)->only(['index', 'store']);
 
 Route::view('quick-connect/magento', 'quick-connect.magento')->name('quick-connect.magento');
 
@@ -41,17 +43,12 @@ Route::view('stocktaking', 'stocktaking')->name('stocktaking');
 Route::view('setting-profile', 'setting-profile')->name('setting-profile');
 Route::view('data-collector', 'data-collector-list')->name('data-collector');
 Route::get('data-collector/{data_collection_id}', [DataCollectorController::class, 'index'])->name('data-collector-show');
-
-
 Route::get('shipping-labels/{shipping_label}', [ShippingLabelController::class, 'show'])->name('shipping-labels');
-
 Route::view('autopilot/packlist', 'autopilot/packlist')->name('autopilot.packlist');
-
 Route::resource('order/packsheet', Order\PacksheetController::class)->only(['show']);
 
 Route::name('reports.')->group(function () {
     Route::resource('reports/stocktake-suggestions', Reports\StocktakeSuggestionsController::class)->only('index');
-
     Route::get('reports/inventory-dashboard', [Reports\InventoryDashboardController::class, 'index'])->name('inventory-dashboard');
     Route::get('reports/picks', [Reports\PickController::class, 'index'])->name('picks');
     Route::get('reports/shipments', [Reports\ShipmentController::class, 'index'])->name('shipments');
@@ -62,26 +59,35 @@ Route::name('reports.')->group(function () {
     Route::get('reports/inventory-movements-summary', [Reports\InventoryMovementsSummaryController::class, 'index'])->name('inventory-movements-summary');
 });
 
-Route::view('settings/warehouses', 'settings/warehouses')->name('settings.warehouses');
-
 Route::get('pdf/orders/{order_number}/{template}', [PdfOrderController::class, 'show']);
 Route::get('csv/ready_order_shipments', [Csv\ReadyOrderShipmentController::class, 'index'])->name('ready_order_shipments_as_csv');
 Route::get('csv/order_shipments', [Csv\PartialOrderShipmentController::class, 'index'])->name('partial_order_shipments_as_csv');
 Route::get('csv/products/picked', [Csv\ProductsPickedInWarehouse::class, 'index'])->name('warehouse_picks.csv');
 Route::get('csv/products/shipped', [Csv\ProductsShippedFromWarehouseController::class, 'index'])->name('warehouse_shipped.csv');
 Route::get('csv/boxtop/stock', [Csv\BoxTopStockController::class, 'index'])->name('boxtop-warehouse-stock.csv');
+Route::view('settings/warehouses', 'settings/warehouses')->name('settings.warehouses');
 
 Route::middleware(['role:admin'])->group(function () {
     Route::view('settings/modules/magento2msi', 'settings/modules/magento2msi');
     Route::view('settings/modules/stocktake-suggestions', 'settings/modules/stocktake-suggestions');
-});
-
-Route::prefix('admin')->middleware(['web', 'auth', 'role:admin', 'twofactor'])->group(function () {
-    Route::view('modules/slack/config', 'modules/slack/config')->name('modules.slack.config');
-
-    // Settings
-    Route::prefix('settings')->group(function () {
-        Route::view('modules/magento-api', 'settings/magento-api')->name('settings.modules.magento-api');
-        Route::view('modules/inventory-reservations', 'settings/inventory-reservations')->name('settings.modules.inventory-reservations');
-    });
+    Route::view('admin/settings', 'settings')->name('settings');
+    Route::view('admin/activity-log', 'activity-log')->name('activity-log');
+    Route::view('admin/settings/general', 'settings/general')->name('settings.general');
+    Route::view('admin/settings/order-statuses', 'settings/order-statuses')->name('settings.order_statuses');
+    Route::view('admin/settings/printnode', 'settings/printnode')->name('settings.printnode');
+    Route::view('admin/settings/rmsapi', 'settings/rmsapi')->name('settings.rmsapi');
+    Route::view('admin/settings/dpd-ireland', 'settings/dpd-ireland')->name('settings.dpd-ireland');
+    Route::view('admin/settings/api2cart', 'settings/api2cart')->name('settings.api2cart');
+    Route::view('admin/settings/api', 'settings/api')->name('settings.api');
+    Route::view('admin/settings/users', 'settings/users')->name('settings.users');
+    Route::view('admin/settings/mail-templates', 'settings/mail-templates')->name('settings.mail_templates');
+    Route::get('admin/settings/mail-templates/{mailTemplate}/preview', [MailTemplatePreviewController::class, 'index'])->name('settings.mail_template_preview');
+    Route::view('admin/settings/navigation-menu', 'settings/navigation-menu')->name('settings.navigation_menu');
+    Route::view('admin/settings/automations', 'settings/automations')->name('settings.automations');
+    Route::view('admin/settings/modules', 'settings/modules')->name('settings.modules');
+    Route::view('admin/settings/modules/dpd-uk', 'settings/dpd-uk')->name('settings.modules.dpd-uk');
+    Route::get('admin/settings/modules/webhooks/subscriptions', [SubscriptionController::class, 'index'])->name('webhooks::subscriptions');
+    Route::view('admin/modules/slack/config', 'modules/slack/config')->name('modules.slack.config');
+    Route::view('admin/settings/modules/magento-api', 'settings/magento-api')->name('settings.modules.magento-api');
+    Route::view('admin/settings/modules/inventory-reservations', 'settings/inventory-reservations')->name('settings.modules.inventory-reservations');
 });
