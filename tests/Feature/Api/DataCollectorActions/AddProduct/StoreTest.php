4cf40e4be071709f415e72ab5be96d19e4f500c8
Artur Hanusek <arthur@youritsolutions.ie>
2024-01-19 16:10:05 +0100

    make test

---
+++ tests/Feature/Api/DataCollectorActions/AddProduct/StoreTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Api\DataCollectorActions\AddProduct;
+
+use App\Models\DataCollection;
+use App\Models\Product;
+use App\User;
+use Tests\TestCase;
+
+class StoreTest extends TestCase
+{
+    private string $uri = 'api/data-collector-actions/add-product';
+
+    /** @test */
+    public function testIfCallReturnsOk()
+    {
+        $user = User::factory()->create()->assignRole('user');
+        $dataCollection = DataCollection::factory()->create();
+        $product = Product::factory()->create();
+
+        $response = $this->actingAs($user, 'api')->postJson($this->uri, [
+            'data_collection_id' => $dataCollection->id,
+            'sku_or_alias' => $product->sku,
+            'quantity_scanned' => -50 + rand(1, 100),
+        ]);
+
+        ray($response->json());
+
+        $response->assertSuccessful();
+
+        $this->assertCount(1, $response->json('data'), 'No records returned');
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id'
+                ],
+            ],
+        ]);
+    }
+}
