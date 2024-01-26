ac5039ed1bbbe1374650c31a2553caf22f64f965
arthur@coding.cab <arthur@youritsolutions.ie>
2024-01-26 17:45:59 +0100

    refactoring

--- app/Modules/InventoryReservations/src/Jobs/UpdateInventoryQuantityReservedJob.php
+++ app/Modules/InventoryReservations/src/Jobs/UpdateInventoryQuantityReservedJob.php
@@ -2,22 +2,13 @@
 
 namespace App\Modules\InventoryReservations\src\Jobs;
 
+use App\Abstracts\UniqueJob;
 use App\Models\Inventory;
 use App\Models\OrderProduct;
 use App\Modules\InventoryReservations\src\Models\Configuration;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 
-class UpdateInventoryQuantityReservedJob implements ShouldQueue
+class UpdateInventoryQuantityReservedJob extends UniqueJob
 {
-    use Dispatchable;
-    use InteractsWithQueue;
-    use Queueable;
-    use SerializesModels;
-
     private int $product_id;
 
     public function __construct(int $product_id)
@@ -25,11 +16,6 @@ class UpdateInventoryQuantityReservedJob implements ShouldQueue
         $this->product_id = $product_id;
     }
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
     public function handle()
     {
         $newQuantityReserved = OrderProduct::where(['product_id' => $this->product_id])
