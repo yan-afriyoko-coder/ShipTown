3bb9943ce5dc234b76e9e90a9b12c1ea2e063528
arthur@coding.cab <arthur@youritsolutions.ie>
2024-01-26 13:11:31 +0100

    republish webhooks daily for RMS stock discrepencies

--- app/Modules/Rmsapi/src/Listeners/EveryDayEventListener.php
+++ app/Modules/Rmsapi/src/Listeners/EveryDayEventListener.php
@@ -3,11 +3,13 @@
 namespace App\Modules\Rmsapi\src\Listeners;
 
 use App\Modules\Rmsapi\src\Jobs\CleanupImportTablesJob;
+use App\Modules\Rmsapi\src\Jobs\Maintenance\RepublishWebhooksForDiscrepencies;
 
 class EveryDayEventListener
 {
     public function handle()
     {
         CleanupImportTablesJob::dispatch();
+        RepublishWebhooksForDiscrepencies::dispatch();
     }
 }
