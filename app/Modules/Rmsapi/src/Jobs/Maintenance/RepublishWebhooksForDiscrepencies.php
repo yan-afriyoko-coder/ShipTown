5b2bda4d1aec4d02fe1d6f2dbbcd7866c36faa02
arthur@coding.cab <arthur@youritsolutions.ie>
2024-01-31 18:18:21 +0100

    fix job

--- app/Modules/Rmsapi/src/Jobs/Maintenance/RepublishWebhooksForDiscrepencies.php
+++ app/Modules/Rmsapi/src/Jobs/Maintenance/RepublishWebhooksForDiscrepencies.php
@@ -3,6 +3,7 @@
 namespace App\Modules\Rmsapi\src\Jobs\Maintenance;
 
 use App\Abstracts\UniqueJob;
+use App\Models\Inventory;
 use Illuminate\Support\Facades\DB;
 
 class RepublishWebhooksForDiscrepencies extends UniqueJob
@@ -13,7 +14,7 @@ class RepublishWebhooksForDiscrepencies extends UniqueJob
         DB::statement('
             INSERT INTO modules_webhooks_pending_webhooks (model_class, model_id, message, created_at, updated_at)
             SELECT
-                "App\\Models\\Inventory" as model_class,
+                ? as model_class,
                 modules_rmsapi_products_quantity_comparison_view.inventory_id as model_id,
                 "{}" as message,
                 now() as created_at,
@@ -22,11 +23,11 @@ class RepublishWebhooksForDiscrepencies extends UniqueJob
 
                      LEFT JOIN modules_webhooks_pending_webhooks
                                ON modules_webhooks_pending_webhooks.model_id = modules_rmsapi_products_quantity_comparison_view.inventory_id
-                                   AND modules_webhooks_pending_webhooks.model_class = "App\\Models\\Inventory"
+                                   AND modules_webhooks_pending_webhooks.model_class = ?
                                    AND modules_webhooks_pending_webhooks.reserved_at IS NULL
 
             WHERE quantity_delta != 0
               AND modules_webhooks_pending_webhooks.id is null;
-        ');
+        ', [Inventory::class, Inventory::class]);
     }
 }
