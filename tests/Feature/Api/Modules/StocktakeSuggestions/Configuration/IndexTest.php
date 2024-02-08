bb5f41c6d02a6e8b4f0906569626db9d71574ea4
Artur Hanusek <arthur@youritsolutions.ie>
2024-02-08 21:18:10 +0100

    create tests

---
+++ tests/Feature/Api/Modules/StocktakeSuggestions/Configuration/IndexTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Api\Modules\StocktakeSuggestions\Configuration;
+
+use App\User;
+use Tests\TestCase;
+
+class IndexTest extends TestCase
+{
+    private string $uri = 'api/modules/stocktake-suggestions/configuration';
+
+    /** @test */
+    public function testIfCallReturnsOk()
+    {
+        $user = User::factory()->create()->assignRole('user');
+
+        $response = $this->actingAs($user, 'api')->postJson($this->uri, []);
+
+        ray($response->json());
+
+        $response->assertSuccessful();
+
+        $response->assertJsonStructure([
+            'data' => [
+                'id'
+            ],
+        ]);
+    }
+}
---
+++ tests/Feature/Api/Modules/StocktakeSuggestions/Configuration/StoreTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Api\Modules\StocktakeSuggestions\Configuration;
+
+use App\User;
+use Tests\TestCase;
+
+class StoreTest extends TestCase
+{
+    private string $uri = 'api/modules/stocktake-suggestions/configuration';
+
+    /** @test */
+    public function testIfCallReturnsOk()
+    {
+        $user = User::factory()->create()->assignRole('admin');
+
+        $response = $this->actingAs($user, 'api')->postJson($this->uri, []);
+
+        ray($response->json());
+
+        $response->assertSuccessful();
+
+        $response->assertJsonStructure([
+            'data' => [
+                'id'
+            ],
+        ]);
+    }
+}
