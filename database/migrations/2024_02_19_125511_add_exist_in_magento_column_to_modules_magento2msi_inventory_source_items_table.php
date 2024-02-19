617dbb9eecc90248ebffba3f1be982e80cc45400
arthur@coding.cab <arthur@youritsolutions.ie>
2024-02-19 14:20:43 +0100

    fix migration

--- database/migrations/2024_02_19_125511_add_exist_in_magento_column_to_modules_magento2msi_inventory_source_items_table.php
+++ database/migrations/2024_02_19_125511_add_exist_in_magento_column_to_modules_magento2msi_inventory_source_items_table.php
@@ -8,10 +8,16 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('modules_magento2msi_inventory_source_items', function (Blueprint $table) {
-            $table->boolean('exist_in_magento')->nullable()->after('inventory_totals_by_warehouse_tag_id');
+        if (! Schema::hasColumn('modules_magento2msi_inventory_source_items', 'exist_in_magento')) {
+            Schema::table('modules_magento2msi_inventory_source_items', function (Blueprint $table) {
+                $table->boolean('exist_in_magento')->nullable()->after('inventory_totals_by_warehouse_tag_id');
 
+            });
+        }
+
+        Schema::table('modules_magento2msi_inventory_source_items', function (Blueprint $table) {
             $table->index(['exist_in_magento'], 'exist_in_magento_index');
         });
+
     }
 };
