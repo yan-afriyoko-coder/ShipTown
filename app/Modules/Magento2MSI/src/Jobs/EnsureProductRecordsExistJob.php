bbec157d426de06933dddd83997b8b5875f671c9
arthur@coding.cab <arthur@youritsolutions.ie>
2024-02-20 13:11:12 +0100

    optimize job

--- app/Modules/Magento2MSI/src/Jobs/EnsureProductRecordsExistJob.php
+++ app/Modules/Magento2MSI/src/Jobs/EnsureProductRecordsExistJob.php
@@ -5,6 +5,7 @@ namespace App\Modules\Magento2MSI\src\Jobs;
 use App\Abstracts\UniqueJob;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Class SyncCheckFailedProductsJob.
@@ -13,7 +14,11 @@ class EnsureProductRecordsExistJob extends UniqueJob
 {
     public function handle()
     {
+        $maxId = DB::table('products')->max('id') ?? 0;
+
         do {
+            $minId = max($maxId - 1000, 0);
+
             $recordsUpdated = DB::affectingStatement("
                 INSERT INTO modules_magento2msi_inventory_source_items (connection_id, product_id, inventory_totals_by_warehouse_tag_id, sku, source_code, custom_uuid, created_at, updated_at)
                 SELECT
@@ -43,12 +48,23 @@ class EnsureProductRecordsExistJob extends UniqueJob
                 LEFT JOIN products
                     ON products.id = inventory_totals_by_warehouse_tag.product_id
 
-                WHERE modules_magento2msi_connections.enabled = 1 AND modules_magento2msi_inventory_source_items.id IS NULL AND modules_magento2msi_connections.id != 3
+                WHERE products.id BETWEEN ? AND ?
+                  AND modules_magento2msi_connections.enabled = 1
+                  AND modules_magento2msi_inventory_source_items.id IS NULL
+                  AND modules_magento2msi_connections.id != 3
 
                 LIMIT 5000
-           ", ['Available Online', Product::class]);
+           ", ['Available Online', Product::class, $minId, $maxId]);
+
+            $maxId = $minId;
+
+            Log::info('Job processing', [
+                'job' => self::class,
+                'recordsUpdated' => $recordsUpdated,
+                'maxId' => $maxId,
+            ]);
 
             usleep(100000); // 0.1 second
-        } while ($recordsUpdated > 0);
+        } while ($maxId > 0);
     }
 }
