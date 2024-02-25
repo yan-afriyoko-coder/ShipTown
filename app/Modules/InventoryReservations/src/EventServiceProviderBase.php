1cf3ef72f930a7efbf9fc7e921468d2f9318c6c7
Artur Hanusek <arthur@youritsolutions.ie>
2024-02-25 22:39:06 +0100

    refactoring

--- app/Modules/InventoryReservations/src/EventServiceProviderBase.php
+++ app/Modules/InventoryReservations/src/EventServiceProviderBase.php
@@ -3,8 +3,6 @@
 namespace App\Modules\InventoryReservations\src;
 
 use App\Events\EveryDayEvent;
-use App\Events\EveryHourEvent;
-use App\Events\Inventory\InventoryUpdatedEvent;
 use App\Events\Order\OrderUpdatedEvent;
 use App\Events\OrderProduct\OrderProductCreatedEvent;
 use App\Events\OrderProduct\OrderProductUpdatedEvent;
@@ -13,36 +11,16 @@ use App\Modules\BaseModuleServiceProvider;
 use App\Modules\InventoryReservations\src\Jobs\RecalculateQuantityReservedJob;
 use App\Modules\InventoryReservations\src\Models\Configuration;
 
-/**
- * Class EventServiceProviderBase.
- */
 class EventServiceProviderBase extends BaseModuleServiceProvider
 {
-    /**
-     * @var string
-     */
     public static string $module_name = '.CORE - Inventory Reservations';
 
-    /**
-     * @var string
-     */
     public static string $module_description = 'Reserves stock for active orders.';
 
-    /**
-     * @var string
-     */
     public static string $settings_link = '/admin/settings/modules/inventory-reservations';
 
-    /**
-     * @var bool
-     */
     public static bool $autoEnable = false;
 
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
     protected $listen = [
         OrderProductUpdatedEvent::class => [
             Listeners\OrderProductUpdatedEventListener::class,
