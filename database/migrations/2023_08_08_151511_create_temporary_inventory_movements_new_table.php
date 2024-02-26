491d95d994a0c98d20a72f1855e3555a436ca59c
arthur@coding.cab <arthur@youritsolutions.ie>
2024-02-26 12:26:24 +0100

    prevent from merging

--- database/migrations/2023_08_08_151511_create_temporary_inventory_movements_new_table.php
+++ database/migrations/2023_08_08_151511_create_temporary_inventory_movements_new_table.php
@@ -8,6 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
+        // do not merge - to be deleted
         Schema::create('inventory_movements_temp', function (Blueprint $table) {
             $table->id();
             $table->boolean('is_first_movement')->nullable();
