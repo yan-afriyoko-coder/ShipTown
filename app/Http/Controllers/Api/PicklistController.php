43317ea2f632f808aee6cafe56df3674bfe5a49f
Robert Goodliffe <robgoodliffe@gmail.com>
2024-03-04 16:01:38 +0100

    UI - Add order count to Picklist page

--- app/Http/Controllers/Api/PicklistController.php
+++ app/Http/Controllers/Api/PicklistController.php
@@ -24,7 +24,7 @@ class PicklistController extends Controller
                 'inventory_source_shelf_location',
                 DB::raw('sum(`quantity_to_pick`) as total_quantity_to_pick'),
                 DB::raw('max(`inventory_source_quantity`) as inventory_source_quantity'),
-                DB::raw("GROUP_CONCAT(id ORDER BY id SEPARATOR ',' ) AS order_product_ids"),
+                DB::raw("GROUP_CONCAT(order_id ORDER BY order_id SEPARATOR ',' ) AS order_product_ids"),
             ])
             ->groupBy([
                 'orders_products.name_ordered',
