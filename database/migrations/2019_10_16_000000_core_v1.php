1de15e018705391644c6354ce2928417766e7bbe
Robert Goodliffe <robgoodliffe@gmail.com>
2024-03-11 17:04:47 +0100

    replace modules_rmsapi_products_imports

--- database/migrations/2019_10_16_000000_core_v1.php
+++ database/migrations/2019_10_16_000000_core_v1.php
@@ -734,32 +734,56 @@ return new class extends Migration
         Schema::create('modules_rmsapi_products_imports', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('connection_id');
-            $table->foreignId('warehouse_id')->nullable();
             $table->string('warehouse_code')->nullable();
-            $table->uuid('batch_uuid')->nullable();
-            $table->timestamp('reserved_at')->nullable();
-            $table->dateTime('when_processed')->nullable();
             $table->unsignedBigInteger('product_id')->nullable();
+            $table->unsignedBigInteger('inventory_id')->nullable();
+            $table->unsignedBigInteger('warehouse_id')->nullable();
+            $table->unsignedBigInteger('rms_product_id')->nullable();
             $table->string('sku')->nullable();
+            $table->boolean('is_web_item')->nullable();
             $table->decimal('quantity_on_hand', 20)->nullable();
             $table->decimal('quantity_committed', 20)->nullable();
             $table->decimal('quantity_available', 20)->nullable();
             $table->decimal('quantity_on_order', 20)->nullable();
-            $table->json('raw_import');
+            $table->decimal('reorder_point', 20)->nullable();
+            $table->decimal('restock_level', 20)->nullable();
+            $table->decimal('price', 20)->nullable();
+            $table->decimal('cost', 20)->nullable();
+            $table->decimal('sale_price', 20)->nullable();
+            $table->timestamp('sale_price_start_date')->nullable();
+            $table->timestamp('sale_price_end_date')->nullable();
+            $table->string('department_name')->nullable();
+            $table->string('category_name')->nullable();
+            $table->string('sub_description_1')->nullable();
+            $table->string('sub_description_2')->nullable();
+            $table->string('sub_description_3')->nullable();
+            $table->string('supplier_name')->nullable();
+            $table->timestamp('reserved_at')->nullable();
+            $table->timestamp('processed_at')->nullable();
+            $table->uuid('batch_uuid')->nullable();
+            $table->json('raw_import')->nullable();
             $table->timestamps();
 
+            $table->index('connection_id');
+            $table->index('rms_product_id');
+            $table->index('reserved_at');
+            $table->index('processed_at');
+            $table->index('is_web_item');
 
-            $table->index('when_processed');
+            $table->foreign('inventory_id')
+                ->references('id')
+                ->on('inventory')
+                ->cascadeOnDelete();
 
             $table->foreign('product_id')
                 ->references('id')
                 ->on('products')
-                ->onDelete('SET NULL');
+                ->cascadeOnDelete();
 
             $table->foreign('connection_id')
                 ->references('id')
                 ->on('modules_rmsapi_connections')
-                ->onDelete('cascade');
+                ->cascadeOnDelete();
 
             $table->foreign('warehouse_id')
                 ->references('id')
