b0bcd81bb25a5455f9c87dc93bcd95d84fa44b79
Robert Goodliffe <robgoodliffe@gmail.com>
2024-03-27 22:31:41 +0100

    Warehouse code to restocking 298 (#309)
    * add warehouse location
    
    * changes
    
    * 1.0.226
    
    * changes
    
    * 1.0.227
    
    * remove old styles
    
    * 1.0.228
    
    * 1.0.229
    
    * lessened gap
    
    * 1.0.230
    
    * gap removed

--- resources/js/components/RestockingPage/RestockingRecordCard.vue
+++ resources/js/components/RestockingPage/RestockingRecordCard.vue
@@ -1,144 +1,141 @@
 <template>
-    <div class="row mb-3">
-        <div class="col ml-0 pl-0">
-            <div class="card ml-0 pl-0">
-                <div class="row card-body pt-2 pl-2">
-                    <div class="col-lg-4">
-                        <product-info-card :product="record['product']"/>
+    <div class="card ml-0 pl-0 mb-3">
+        <div class="grid-col-12 px-2 py-2 m-0 gap-none">
+            <div class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3">
+                <product-info-card :product="record['product']"/>
+            </div>
+            <div class="small col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3">
+                <div class="sd-block lg:sd-none">
+                    Warehouse: <b>{{ record['warehouse_code'] }}</b>
+                </div>
+                <div :class="{'bg-warning': record['warehouse_quantity'] <= 0 }" @click="expanded = !expanded">warehouse quantity: <b>{{ record['warehouse_quantity'] }}</b></div>
+                <div @click="expanded = !expanded">reorder point: <b>{{ record['reorder_point'] }}</b></div>
+                <div @click="expanded = !expanded">restock level: <b>{{ record['restock_level'] }}</b></div>
+                <div class="mt-1">
+                    <div @click="expanded = !expanded" class="d-inline">last sold at: </div>
+                    <strong @click="showInventoryMovementModal" class="text-primary cursor-pointer">{{ formatDateTime(record['last_sold_at']) }}</strong>
+                </div>
+                <div>
+                    <div @click="expanded = !expanded" class="d-inline">last received at:</div>
+                    <strong @click="showInventoryMovementModal" class="text-primary cursor-pointer">{{ formatDateTime(record['last_received_at']) }}</strong>
+                </div>
+                <div @click="expanded = !expanded">last counted at: <b>{{ formatDateTime(record['last_counted_at'],'D MMM HH:MM') }}</b></div>
+                <div @click="expanded = !expanded">sale price: <b>{{ pricing['sale_price'] }} ({{ formatDateTime(pricing['sale_price_start_date'], 'D MMM Y') }} - {{ formatDateTime(pricing['sale_price_end_date'], 'D MMM Y') }})</b></div>
+            </div>
+            <div class="text-lg-center sd-none lg:sd-block lg:col-span-1">
+                <p class="small text-secondary mb-0">Warehouse</p>
+                <h4>{{ record['warehouse_code'] }}</h4>
+            </div>
+            <div class="col-span-12 md:col-span-4 lg:col-span-5">
+                <div class="row-col text-right" @click="expanded = !expanded">
+                    <div class="row">
+                        <div class="col-3">
+                            <text-card label="price" :text="pricing['price']" :class="{ 'text-secondary': isOnSale }" ></text-card>
+                        </div>
+                        <div class="col-3">
+                            <text-card label="sale price" :text="pricing['sale_price']" v-if="isOnSale" class="bg-warning"></text-card>
+                        </div>
+                        <div class="col-3">
+                            <number-card label="in stock" :number="record['quantity_in_stock']" v-bind:class="{'bg-warning' : record['quantity_in_stock'] < 0 }"></number-card>
+                        </div>
+                        <div class="col-3">
+                            <number-card label="required" :number="record['quantity_required']" v-if="record['warehouse_quantity'] > 0"></number-card>
+                            <text-card class="fa-pull-right" label="required" text="N/A" v-if="Number(record['warehouse_quantity']) === 0"></text-card>
+                        </div>
                     </div>
-                    <div class="col mt-1 mb-1 small">
-                        <div :class="{'bg-warning': record['warehouse_quantity'] <= 0 }" @click="expanded = !expanded">warehouse quantity: <b>{{ record['warehouse_quantity'] }}</b></div>
-                        <div @click="expanded = !expanded">reorder point: <b>{{ record['reorder_point'] }}</b></div>
-                        <div @click="expanded = !expanded">restock level: <b>{{ record['restock_level'] }}</b></div>
-                        <div class="mt-1">
-                            <div @click="expanded = !expanded" class="d-inline">last sold at: </div>
-                            <strong @click="showInventoryMovementModal" class="text-primary cursor-pointer">{{ formatDateTime(record['last_sold_at']) }}</strong>
+                    <div class="row">
+                        <div class="col-3">
+                            <number-card label="weeks cover" :number="weeksCover"></number-card>
                         </div>
-                        <div>
-                            <div @click="expanded = !expanded" class="d-inline">last received at:</div>
-                            <strong @click="showInventoryMovementModal" class="text-primary cursor-pointer">{{ formatDateTime(record['last_received_at']) }}</strong>
+                        <div class="col-3">
                         </div>
-                        <div @click="expanded = !expanded">last counted at: <b>{{ formatDateTime(record['last_counted_at'],'D MMM HH:MM') }}</b></div>
-                        <div @click="expanded = !expanded">sale price: <b>{{ pricing['sale_price'] }} ({{ formatDateTime(pricing['sale_price_start_date'], 'D MMM Y') }} - {{ formatDateTime(pricing['sale_price_end_date'], 'D MMM Y') }})</b></div>
-
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="row-col text-right" @click="expanded = !expanded">
-                            <div class="row">
-                                <div class="col-3">
-                                    <text-card label="price" :text="pricing['price']" :class="{ 'text-secondary': isOnSale }" ></text-card>
-                                </div>
-                                <div class="col-3">
-                                    <text-card label="sale price" :text="pricing['sale_price']" v-if="isOnSale" class="bg-warning"></text-card>
-                                </div>
-                                <div class="col-3">
-                                    <number-card label="in stock" :number="record['quantity_in_stock']" v-bind:class="{'bg-warning' : record['quantity_in_stock'] < 0 }"></number-card>
-                                </div>
-                                <div class="col-3">
-                                    <number-card label="required" :number="record['quantity_required']" v-if="record['warehouse_quantity'] > 0"></number-card>
-                                    <text-card class="fa-pull-right" label="required" text="N/A" v-if="Number(record['warehouse_quantity']) === 0"></text-card>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-3">
-                                    <number-card label="weeks cover" :number="weeksCover"></number-card>
-                                </div>
-                                <div class="col-3">
-                                </div>
-                                <div class="col-3">
-                                    <number-card label="sold 7 days" :number="record['quantity_sold_last_7_days']"></number-card>
-                                </div>
-                                <div class="col-3">
-                                    <number-card label="incoming" :number="record['quantity_incoming']"></number-card>
-                                </div>
-                            </div>
-
-                            <div class="text-left d-sm-flex d-md-inline">
-                            </div>
+                        <div class="col-3">
+                            <number-card label="sold 7 days" :number="record['quantity_sold_last_7_days']"></number-card>
                         </div>
-
-                        <div @click="expanded = !expanded" class="text-center text-secondary">
-                            <font-awesome-icon v-if="expanded" icon="chevron-up" class="fa fa-xs"></font-awesome-icon>
-                            <font-awesome-icon v-if="!expanded" icon="chevron-down" class="fa fa-xs"></font-awesome-icon>
-                        </div>
-
-                        <div v-if="expanded">
-
-                            <div class="row-col text-center mt-3 small text-secondary">
-                                reorder point
-                            </div>
-
-                            <div class="row-col text-nowrap">
-                                <div class="input-group mb-3">
-                                    <button tabindex="-1" @click="minusReorderPoint" class="btn btn-danger mr-3" type="button" id="button-addon5" style="min-width: 45px">-</button>
-                                    <input tabindex="0"
-                                           @keyup="onUpdateReorderPointEvent"
-                                           v-model="newReorderPointValue"
-                                           @focus="simulateSelectAll"
-                                           type="number"
-                                           inputmode="numeric"
-                                           class="form-control text-center"
-                                           style="font-size: 24px"
-                                    >
-                                    <button tabindex="-1" @click="plusReorderPoint" class="btn btn-success ml-3" type="button" id="button-addon6" style="min-width: 45px">+</button>
-                                </div>
-                            </div>
-
-                            <div class="row-col text-center mt-3 small text-secondary">
-                                restock level
-                            </div>
-                            <div class="row-col text-nowrap">
-                                <div class="input-group mb-3">
-                                    <button tabindex="-1" @click="minusRestockLevel" class="btn btn-danger mr-3" type="button" id="button-addon3" style="min-width: 45px">-</button>
-                                    <input tabindex="0"
-                                           @keyup="onUpdateRestockLevelEvent"
-                                           v-model="newRestockLevelValue"
-                                           @focus="simulateSelectAll"
-                                           type="number"
-                                           inputmode="numeric"
-                                           class="form-control text-center"
-                                           v-bind:class="{ 'alert-danger': newRestockLevelValue < newReorderPointValue }"
-                                           style="font-size: 24px"
-                                    >
-                                    <button tabindex="-1" @click="plusRestockLevel" class="btn btn-success ml-3" type="button" id="button-addon4" style="min-width: 45px">+</button>
-                                </div>
-                            </div>
-                            <template @click="expanded = !expanded" v-if="expanded">
-                                <div @click="expanded = !expanded">last movement at: <b>{{ formatDateTime(record['last_movement_at'],'D MMM HH:MM') }}</b></div>
-                                <div @click="expanded = !expanded">first received at: <b>{{ formatDateTime(record['first_received_at'],'D MMM HH:MM') }}</b></div>
-                                <div @click="expanded = !expanded">location: <b>{{ record['warehouse_code'] }}</b></div>
-                            </template>
-                            <div class="row-col text-center align-bottom pb-2 m-0 font-weight-bold text-uppercase small text-secondary">
-                                Incoming
-                            </div>
-
-                            <div v-for="dataCollectionRecord in dataCollectorRecords" :key="dataCollectionRecord['id']">
-                                <div class="row col">
-                                    <div class="text-primary">
-                                        <a :href="'/data-collector/' + dataCollectionRecord['data_collection']['id']">
-                                            {{ dataCollectionRecord['data_collection']['name'] }}
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="row col">
-                                    <div class="flex-fill">
-                                        <a class="text-secondary small" :href="'/data-collector/' + dataCollectionRecord['data_collection']['id']">
-                                            {{ formatDateTime(dataCollectionRecord['data_collection']['created_at']) }}
-                                        </a>
-                                    </div>
-                                    <div class="">
-                                        <number-card label="requested" :number="dataCollectionRecord['quantity_requested']"></number-card>
-                                        <number-card label="outstanding" :number="dataCollectionRecord['quantity_requested'] - dataCollectionRecord['total_transferred_in']"></number-card>
-                                    </div>
-                                </div>
-                                <hr />
-                            </div>
+                        <div class="col-3">
+                            <number-card label="incoming" :number="record['quantity_incoming']"></number-card>
                         </div>
                     </div>
 
+                    <div class="text-left d-sm-flex d-md-inline">
+                    </div>
+                </div>
 
-                    <div class="row text-center align-content-center offset-lg-6 col-lg-6" v-if="expanded">
-                        <hr>
+                <div @click="expanded = !expanded" class="text-center text-secondary">
+                    <font-awesome-icon v-if="expanded" icon="chevron-up" class="fa fa-xs"></font-awesome-icon>
+                    <font-awesome-icon v-if="!expanded" icon="chevron-down" class="fa fa-xs"></font-awesome-icon>
+                </div>
+
+                <div v-if="expanded">
+
+                    <div class="row-col text-center mt-3 small text-secondary">
+                        reorder point
+                    </div>
+
+                    <div class="row-col text-nowrap">
+                        <div class="input-group mb-3">
+                            <button tabindex="-1" @click="minusReorderPoint" class="btn btn-danger mr-3" type="button" id="button-addon5" style="min-width: 45px">-</button>
+                            <input tabindex="0"
+                                   @keyup="onUpdateReorderPointEvent"
+                                   v-model="newReorderPointValue"
+                                   @focus="simulateSelectAll"
+                                   type="number"
+                                   inputmode="numeric"
+                                   class="form-control text-center"
+                                   style="font-size: 24px"
+                            >
+                            <button tabindex="-1" @click="plusReorderPoint" class="btn btn-success ml-3" type="button" id="button-addon6" style="min-width: 45px">+</button>
+                        </div>
+                    </div>
+
+                    <div class="row-col text-center mt-3 small text-secondary">
+                        restock level
+                    </div>
+                    <div class="row-col text-nowrap">
+                        <div class="input-group mb-3">
+                            <button tabindex="-1" @click="minusRestockLevel" class="btn btn-danger mr-3" type="button" id="button-addon3" style="min-width: 45px">-</button>
+                            <input tabindex="0"
+                                   @keyup="onUpdateRestockLevelEvent"
+                                   v-model="newRestockLevelValue"
+                                   @focus="simulateSelectAll"
+                                   type="number"
+                                   inputmode="numeric"
+                                   class="form-control text-center"
+                                   v-bind:class="{ 'alert-danger': newRestockLevelValue < newReorderPointValue }"
+                                   style="font-size: 24px"
+                            >
+                            <button tabindex="-1" @click="plusRestockLevel" class="btn btn-success ml-3" type="button" id="button-addon4" style="min-width: 45px">+</button>
+                        </div>
+                    </div>
+                    <template @click="expanded = !expanded" v-if="expanded">
+                        <div @click="expanded = !expanded">last movement at: <b>{{ formatDateTime(record['last_movement_at'],'D MMM HH:MM') }}</b></div>
+                        <div @click="expanded = !expanded">first received at: <b>{{ formatDateTime(record['first_received_at'],'D MMM HH:MM') }}</b></div>
+                        <div @click="expanded = !expanded">location: <b>{{ record['warehouse_code'] }}</b></div>
+                    </template>
+                    <div class="row-col text-center align-bottom pb-2 m-0 font-weight-bold text-uppercase small text-secondary">
+                        Incoming
+                    </div>
+
+                    <div v-for="dataCollectionRecord in dataCollectorRecords" :key="dataCollectionRecord['id']">
+                        <div class="row col">
+                            <div class="text-primary">
+                                <a :href="'/data-collector/' + dataCollectionRecord['data_collection']['id']">
+                                    {{ dataCollectionRecord['data_collection']['name'] }}
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row col">
+                            <div class="flex-fill">
+                                <a class="text-secondary small" :href="'/data-collector/' + dataCollectionRecord['data_collection']['id']">
+                                    {{ formatDateTime(dataCollectionRecord['data_collection']['created_at']) }}
+                                </a>
+                            </div>
+                            <div class="">
+                                <number-card label="requested" :number="dataCollectionRecord['quantity_requested']"></number-card>
+                                <number-card label="outstanding" :number="dataCollectionRecord['quantity_requested'] - dataCollectionRecord['total_transferred_in']"></number-card>
+                            </div>
+                        </div>
+                        <hr />
                     </div>
                 </div>
             </div>
@@ -355,11 +352,3 @@ export default {
     }
 
 </script>
-
-<style lang="scss" scoped>
-    //.row {
-    //    display: flex;
-    //    justify-content: center;
-    //    align-items: center;
-    //}
-</style>
