71b8128f66f6ed4c4d313729b852fa7c7d9e8f37
Artur Hanusek <arthur@youritsolutions.ie>
2024-04-03 22:59:52 +0200

    handle null billing address id (#348)
    Co-authored-by: Artur Hanusek <artur@arturhanusek.com>

--- resources/views/pdf/orders/billing_address_label.blade.php
+++ resources/views/pdf/orders/billing_address_label.blade.php
@@ -1,13 +1,13 @@
 @extends('pdf.template')
 @section('content')
-    <div>{{ $billing_address['company'] }}</div>
-    <div>{{ $billing_address['first_name'] }} {{ $billing_address['last_name'] }}</div>
-    <div>{{ $billing_address['address1'] }}</div>
-    <div>{{ $billing_address['address2'] }}</div>
-    <div>{{ $billing_address['city'] }} {{ $billing_address['postcode'] }}</div>
-    <div>{{ $billing_address['state_name'] }}</div>
-    <div style="font-weight: bold">{{ $billing_address['country_name'] }}</div>
-    <div style="font-size: xx-small">T: {{ $billing_address['phone'] }}</div>
+    <div>{{ $billing_address ? $billing_address['company'] : '' }}</div>
+    <div>{{ $billing_address ? $billing_address['first_name'] : '' }} {{ $billing_address ?  $billing_address['last_name'] : '' }}</div>
+    <div>{{ $billing_address ? $billing_address['address1'] : '' }}</div>
+    <div>{{ $billing_address ? $billing_address['address2'] : '' }}</div>
+    <div>{{ $billing_address ? $billing_address['city'] : '' }} {{ $billing_address ?  $billing_address['postcode'] : '' }}</div>
+    <div>{{ $billing_address ? $billing_address['state_name'] : '' }}</div>
+    <div style="font-weight: bold">{{ $billing_address ? $billing_address['country_name'] : '' }}</div>
+    <div style="font-size: xx-small">T: {{ $billing_address ? $billing_address['phone'] : '' }}</div>
     <br>
     <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($order_number, 'C39', 1,15) }}" alt="barcode" />
     <div style="font-size: xx-small">Order #{{ $order_number }}</div>
