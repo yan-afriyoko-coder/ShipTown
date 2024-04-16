4f74fcd3f9b61e206e3f2fa4668a548f73e809a4
Artur Hanusek <arthur@youritsolutions.ie>
2024-04-16 11:03:30 +0200

    Fix ensure product records exists job (#371)
    * fix job
    
    * fix test

--- app/Modules/Magento2MSI/src/Jobs/EnsureProductRecordsExistJob.php
+++ app/Modules/Magento2MSI/src/Jobs/EnsureProductRecordsExistJob.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Log;
  */
 class EnsureProductRecordsExistJob extends UniqueJob
 {
-    public function handle()
+    public function handle(): void
     {
         $maxId = DB::table('products')->max('id') ?? 0;
 
@@ -31,27 +31,25 @@ class EnsureProductRecordsExistJob extends UniqueJob
                         NOW() as created_at,
                         NOW() as updated_at
 
-                FROM products
+                FROM taggables as available_online_tag
+
+                LEFT JOIN products on products.id = available_online_tag.taggable_id
 
                 LEFT JOIN `modules_magento2msi_connections`
                     ON modules_magento2msi_connections.enabled = 1
 
                 INNER JOIN inventory_totals_by_warehouse_tag
                     ON inventory_totals_by_warehouse_tag.tag_id = modules_magento2msi_connections.inventory_source_warehouse_tag_id
-                    AND inventory_totals_by_warehouse_tag.product_id = products.id
+                    AND inventory_totals_by_warehouse_tag.product_id = available_online_tag.taggable_id
 
                 LEFT JOIN modules_magento2msi_inventory_source_items
                     ON modules_magento2msi_inventory_source_items.connection_id = modules_magento2msi_connections.id
-                    AND modules_magento2msi_inventory_source_items.product_id = inventory_totals_by_warehouse_tag.product_id
+                    AND modules_magento2msi_inventory_source_items.product_id = available_online_tag.taggable_id
 
-                INNER JOIN taggables as available_online_tag
-                    ON available_online_tag.tag_id IN (SELECT ID FROM `tags` WHERE JSON_EXTRACT(name, '$.en') = ?)
-                    AND available_online_tag.taggable_type = ?
-                    AND available_online_tag.taggable_id = inventory_totals_by_warehouse_tag.product_id
-
-                WHERE products.id BETWEEN ? AND ?
-                  AND modules_magento2msi_connections.enabled = 1
-                  AND modules_magento2msi_inventory_source_items.id IS NULL
+                WHERE available_online_tag.tag_id IN (SELECT ID FROM `tags` WHERE JSON_EXTRACT(name, '$.en') = ?)
+                AND available_online_tag.taggable_type = ?
+                AND available_online_tag.taggable_id BETWEEN ? AND ?
+                AND modules_magento2msi_inventory_source_items.id IS NULL
            ", ['Available Online', Product::class, $minId, $maxId]);
 
             $maxId = $minId;
