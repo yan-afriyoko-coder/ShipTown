7b7e20da818c2ae5dbae5c211b84ee8ab03e7f27
Artur Hanusek <arthur@youritsolutions.ie>
2024-04-20 01:11:34 +0200

    fix download (#412)
    * fix download
    
    * 1.0.261
    
    * run npm prod

--- app/Http/Controllers/DataCollectorController.php
+++ app/Http/Controllers/DataCollectorController.php
@@ -2,21 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\CsvStreamedResponse;
 use App\Modules\Reports\src\Models\DataCollectionReport;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\Routing\ResponseFactory;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class DataCollectorController extends Controller
 {
-    public function index(Request $request, $id): Factory|View|Response|Application|ResponseFactory
+    public function index(Request $request, $id)
     {
         if ($request->has('filename')) {
             $report = new DataCollectionReport();
-            return $report->toCsvFileDownload();
+            return CsvStreamedResponse::fromQueryBuilder($report->queryBuilder(), $request->get('filename'));
         }
 
         return view('data-collector-page', ['data_collection_id' => $id]);
