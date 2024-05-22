d730a90103dbf6d3a5056d90000db53033669564
Artur Hanusek <arthur@youritsolutions.ie>
2024-05-22 15:16:44 +0200

    update cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'myshiptown.com'],
+    'allowed_origins' => ['*', 'https://myshiptown.com'],
 
     'allowed_origins_patterns' => [],
 
