b0df3ed8d40db4754a386bcde43b9f7ae01cf1f6
Artur Hanusek <arthur@youritsolutions.ie>
2024-06-03 18:04:38 +0200

    refactoring

--- app/Modules/Telescope/src/Jobs/PruneEntriesJob.php
+++ app/Modules/Telescope/src/Jobs/PruneEntriesJob.php
@@ -2,21 +2,12 @@
 
 namespace App\Modules\Telescope\src\Jobs;
 
+use App\Abstracts\UniqueJob;
 use DB;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class PruneEntriesJob implements ShouldQueue
+class PruneEntriesJob extends UniqueJob
 {
-    use Dispatchable;
-    use InteractsWithQueue;
-    use Queueable;
-    use SerializesModels;
-
     public function handle(): bool
     {
         DB::table('telescope_entries')->where('created_at', '<', now()->subHours(config('telescope.hours')))->delete();
