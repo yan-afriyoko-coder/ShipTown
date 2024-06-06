2a05db6c0f31156421e12018330e01951919864f
Artur Hanusek <arthur@youritsolutions.ie>
2024-06-06 15:28:59 +0200

    refactroing

--- database/migrations/2019_10_16_000000_core_v1.php
+++ database/migrations/2019_10_16_000000_core_v1.php
@@ -7,59 +7,63 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     * @throws Exception
-     */
-    public function up()
+    public function up(): void
     {
-        Schema::create('oauth_auth_codes', function (Blueprint $table) {
-            $table->string('id', 100)->primary();
-            $table->foreignId('user_id')->index();
-            $table->foreignId('client_id');
-            $table->text('scopes')->nullable();
-            $table->boolean('revoked');
-            $table->dateTime('expires_at')->nullable();
-        });
+        if (!Schema::hasTable('oauth_auth_codes')) {
+            Schema::create('oauth_auth_codes', function (Blueprint $table) {
+                $table->string('id', 100)->primary();
+                $table->foreignId('user_id')->index();
+                $table->foreignId('client_id');
+                $table->text('scopes')->nullable();
+                $table->boolean('revoked');
+                $table->dateTime('expires_at')->nullable();
+            });
+        }
 
-        Schema::create('oauth_access_tokens', function (Blueprint $table) {
-            $table->string('id', 100)->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->foreignId('client_id');
-            $table->string('name')->nullable();
-            $table->text('scopes')->nullable();
-            $table->boolean('revoked');
-            $table->timestamps();
-            $table->dateTime('expires_at')->nullable();
-        });
+        if (!Schema::hasTable('oauth_access_tokens')) {
+            Schema::create('oauth_access_tokens', function (Blueprint $table) {
+                $table->string('id', 100)->primary();
+                $table->foreignId('user_id')->nullable()->index();
+                $table->foreignId('client_id');
+                $table->string('name')->nullable();
+                $table->text('scopes')->nullable();
+                $table->boolean('revoked');
+                $table->timestamps();
+                $table->dateTime('expires_at')->nullable();
+            });
+        }
 
-        Schema::create('oauth_refresh_tokens', function (Blueprint $table) {
-            $table->string('id', 100)->primary();
-            $table->string('access_token_id', 100)->index();
-            $table->boolean('revoked');
-            $table->dateTime('expires_at')->nullable();
-        });
+        if (!Schema::hasTable('oauth_refresh_tokens')) {
+            Schema::create('oauth_refresh_tokens', function (Blueprint $table) {
+                $table->string('id', 100)->primary();
+                $table->string('access_token_id', 100)->index();
+                $table->boolean('revoked');
+                $table->dateTime('expires_at')->nullable();
+            });
+        }
 
-        Schema::create('oauth_clients', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('name');
-            $table->string('secret', 100)->nullable();
-            $table->string('provider')->nullable();
-            $table->text('redirect');
-            $table->boolean('personal_access_client');
-            $table->boolean('password_client');
-            $table->boolean('revoked');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('oauth_clients')) {
+            Schema::create('oauth_clients', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('user_id')->nullable()->index();
+                $table->string('name');
+                $table->string('secret', 100)->nullable();
+                $table->string('provider')->nullable();
+                $table->text('redirect');
+                $table->boolean('personal_access_client');
+                $table->boolean('password_client');
+                $table->boolean('revoked');
+                $table->timestamps();
+            });
+        }
 
-        Schema::create('oauth_personal_access_clients', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('client_id');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('oauth_personal_access_clients')) {
+            Schema::create('oauth_personal_access_clients', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('client_id');
+                $table->timestamps();
+            });
+        }
 
         Schema::create(config('queue-monitor.table'), function (Blueprint $table) {
             $table->increments('id');
