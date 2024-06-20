e7613796284a7712278e99dfe062e66781c3b96e
Artur Hanusek <arthur@youritsolutions.ie>
2024-06-20 14:22:55 +0200

    fix migration

--- database/migrations/2024_06_20_092334_update_product_sku_index_on_inventory_totals_by_warehouse_tag_table.php
+++ database/migrations/2024_06_20_092334_update_product_sku_index_on_inventory_totals_by_warehouse_tag_table.php
@@ -8,9 +8,15 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('inventory_totals_by_warehouse_tag', function (Blueprint $table) {
-            $table->dropForeign('inventory_totals_by_warehouse_tag_product_sku_foreign');
+        try {
+            Schema::table('inventory_totals_by_warehouse_tag', function (Blueprint $table) {
+                $table->dropForeign('inventory_totals_by_warehouse_tag_product_sku_foreign');
+            });
+        } catch (Exception $e) {
+            report($e);
+        }
 
+        Schema::table('inventory_totals_by_warehouse_tag', function (Blueprint $table) {
             $table->foreign('product_sku')
                 ->references('sku')
                 ->on('products')
