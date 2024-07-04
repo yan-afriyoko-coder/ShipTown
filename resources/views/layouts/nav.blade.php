2a7750a55b81bf9c6669dada944e7666ed3956e6
Artur Hanusek <arthur@youritsolutions.ie>
2024-07-04 09:29:33 +0200

    update navigation

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,121 +1,106 @@
 @auth
     <nav class="navbar navbar-expand-md navbar-light mb-2 p-0 bg-primary" style="z-index: 1021">
-        <div class="container text-white">
-            <div class="d-block mb-0 navbar-dark bg-primary">
-                <div class="btn-group" role="group" aria-label="Basic example">
-                    <a style="width: 40px" id="dashboard_link" class="btn btn-primary" href="{{ route('dashboard') }}"><font-awesome-icon icon="chart-column" class="fa-lg"></font-awesome-icon></a>
-                    <a id="products_link" class="btn btn-primary" href="{{ route('products') }}">{{ __('Products') }}</a>
-                    <a id="orders_link" class="btn btn-primary" href="{{ route('orders') }}">{{ __('Orders') }}</a>
+        <div class="container text-white text-nowrap flex-nowrap">
+            <div class="d-flex mb-0 navbar-dark w-100 text-nowrap flex-nowrap">
+                <a id="products_link" class="btn btn-primary" href="{{ route('products') }}">{{ __('Products') }}</a>
+                <a id="orders_link" class="btn btn-primary" href="{{ route('orders') }}">{{ __('Orders') }}</a>
 
-                    @if(Auth::user()->warehouse_id)
-                        <!-- Tools -->
-                        <div class="dropdown">
-                            <a id="tools_link" class="dropdown-toggle btn btn-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ __('Tools') }}<span class="caret"></span>
-                            </a>
+                @if(Auth::user()->warehouse_id)
+                    <!-- Tools -->
+                    <div class="dropdown">
+                        <a id="tools_link" class="dropdown-toggle btn btn-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ __('Tools') }}
+                        </a>
 
-                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-md-left w-auto text-left bg-primary " aria-labelledby="navbarDropdown" >
-                                <a class="dropdown-item text-white lightHover mt-1" id="data_collector_link" href="{{ route('data-collector') }}">{{ __('Data Collector') }}</a>
-                                <a class="dropdown-item text-white lightHover mt-1" id="stocktaking_link" href="{{ route('stocktaking') }}">{{ __('Stocktaking') }}</a>
-                                <a class="dropdown-item text-white lightHover mt-1" id="restocking_link" href="{{ route('reports.restocking.index' , ['sort' => '-quantity_required', 'cache_name' => 'restocking_page']) }}">{{ __('Restocking') }}</a>
-                                <a class="dropdown-item text-white lightHover mt-1" id="inventory_movements_link" href="{{ route('reports.inventory-movements.index', ['view' => 'reports.inventory-movements']) }}">{{ __('Inventory Movements') }}</a>
-                                <a class="dropdown-item text-white lightHover mt-1 mb-1" id="shelf_label_printing" href="/tools/printer">{{ __('Label Printer') }}</a>
-                                <a class="dropdown-item text-white lightHover mt-1 mb-1" id="packlist" href="/autopilot/packlist?step=select">{{ __('Packlist') }}</a>
-                            </div>
+                        <div class="dropdown-menu dropdown-menu-left w-auto text-left bg-primary " aria-labelledby="navbarDropdown" >
+                            <a class="dropdown-item text-white lightHover mt-1" id="data_collector_link" href="{{ route('data-collector') }}">{{ __('Data Collector') }}</a>
+                            <a class="dropdown-item text-white lightHover mt-1" id="stocktaking_link" href="{{ route('stocktaking') }}">{{ __('Stocktaking') }}</a>
+                            <a class="dropdown-item text-white lightHover mt-1" id="restocking_link" href="{{ route('reports.restocking.index' , ['sort' => '-quantity_required', 'cache_name' => 'restocking_page']) }}">{{ __('Restocking') }}</a>
+                            <a class="dropdown-item text-white lightHover mt-1" id="inventory_movements_link" href="{{ route('reports.inventory-movements.index', ['view' => 'reports.inventory-movements']) }}">{{ __('Inventory Movements') }}</a>
+                            <a class="dropdown-item text-white lightHover mt-1 mb-1" id="shelf_label_printing" href="/tools/printer">{{ __('Label Printer') }}</a>
+                            <a class="dropdown-item text-white lightHover mt-1 mb-1" id="packlist" href="/autopilot/packlist?step=select">{{ __('Packlist') }}</a>
+                            <a class="dropdown-item text-white lightHover mt-1 mb-1" id="packlist" href="/picklist?step=select">{{ __('Picklist') }}</a>
                         </div>
-                    @endif
+                    </div>
+                @endif
+
+
+                <!-- Picklist Dropdown -->
+                <div class="dropdown">
+                    <a id="picklists_link" class="dropdown-toggle btn btn-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ __('Picklist') }}<span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-left bg-primary border-0" aria-labelledby="dropdown" id="dropdown">
+                        @foreach ($navigationMenuPicklist as $menu)
+                            <a id="picklistItem{{ $loop->index }}" class="dropdown-item text-white lightHover" href="{{ $menu->url }}">{{ $menu->name }}</a>
+                        @endforeach
+                    </div>
                 </div>
-            </div>
 
-            <!-- Burger menu -->
-            <button id="navToggleButton" class="btn btn-primary float-right border-0 btn-primary d-m-block d-md-none " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <font-awesome-icon icon="bars" class="fa-lg"></font-awesome-icon>
-            </button>
+                <!-- Reports Dropdown -->
+                <div class="dropdown">
+                    <a id="navbarDropdown3" class="dropdown-toggle btn btn-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{--                        <font-awesome-icon icon="chart-column" class="fa-lg"></font-awesome-icon>--}}
+                        Reports
+                    </a>
 
+                    <div class="dropdown-menu dropdown-menu-left bg-primary" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item text-white lightHover" href="{{ route('inventory-dashboard') }}">{{ __('Inventory Dashboard') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="{{ route('fulfillment-dashboard') }}">{{ __('Fulfillment Dashboard') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="{{ route('fulfillment-statistics') .'?between_dates=-7days,now' }}">{{ __('Fulfillment Statistics') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="{{ route('reports.picks.index') }}">{{ __('Order Picks') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="{{ route('reports.shipments.index') }}">{{ __('Order Shipments') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="{{ route('reports.inventory.index', ['sort' => '-quantity']) }}">{{ __('Inventory') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="{{ route('reports.inventory-transfers.index', ['filter[warehouse_code]' =>  data_get(Auth::user(), 'warehouse.code'), 'sort' => '-updated_at']) }}">{{ __('Inventory Transfers') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="{{ route('reports.inventory-movements.index') }}">{{ __('Inventory Movements') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="{{ route('reports.inventory-movements-summary.index', ['per_page' => '200', 'sort' => 'type']) }}">{{ __('Inventory Movements Summary') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="{{ route('reports.restocking.index') }}">{{ __('Restocking') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="{{ route('reports.stocktake-suggestions.index') }}">{{ __('Stocktake Suggestions') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="{{ route('reports.activity-log.index') }}">{{ __('Activity Log') }}</a>
 
-            <!-- Right Side Of Navbar -->
-            <div class="collapse navbar-collapse bg-primary" id="navbarSupportedContent" style="transition: none !important;">
-                <div class="flex-fill bg-danger row col"></div>
-                <ul class="navbar-nav mr-auto text-center text-md-left">
-                    <!-- Picklist Dropdown -->
-                    <li class="nav-item dropdown">
-                        <a id="picklists_link" class="dropdown-toggle btn btn-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ __('Picklist') }}<span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-left bg-primary border-0" aria-labelledby="navbarDropdown">
-                           @foreach ($navigationMenuPicklist as $menu)
-                                <a id="picklistItem{{ $loop->index }}" class="dropdown-item text-white lightHover" href="{{ $menu->url }}">{{ $menu->name }}</a>
+                        @if(count($navigationMenuReports) > 0)
+                            <hr v-if='{{ count($navigationMenuReports) > 0 }}' class="mb-1 mt-1">
+                            @foreach ($navigationMenuReports as $menu)
+                                <a class="dropdown-item text-white lightHover" href="{{ $menu->url }}">
+                                    {{ $menu->name }}
+                                </a>
                             @endforeach
-                        </div>
-                    </li>
+                        @endif
+                    </div>
+                </div>
 
-                    <!-- Reports Dropdown -->
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown3" class="dropdown-toggle btn btn-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ __('Reports') }}<span class="caret"></span>
-                        </a>
+                <div class="flex-fill"></div>
 
-                        <div class="dropdown-menu left bg-primary" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item text-white lightHover" href="{{ route('inventory-dashboard') }}">{{ __('Inventory Dashboard') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="{{ route('fulfillment-dashboard') }}">{{ __('Fulfillment Dashboard') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="{{ route('fulfillment-statistics') .'?between_dates=-7days,now' }}">{{ __('Fulfillment Statistics') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="{{ route('reports.picks.index') }}">{{ __('Order Picks') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="{{ route('reports.shipments.index') }}">{{ __('Order Shipments') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="{{ route('reports.inventory.index', ['sort' => '-quantity']) }}">{{ __('Inventory') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="{{ route('reports.inventory-transfers.index', ['filter[warehouse_code]' =>  data_get(Auth::user(), 'warehouse.code'), 'sort' => '-updated_at']) }}">{{ __('Inventory Transfers') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="{{ route('reports.inventory-movements.index') }}">{{ __('Inventory Movements') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="{{ route('reports.inventory-movements-summary.index', ['per_page' => '200', 'sort' => 'type']) }}">{{ __('Inventory Movements Summary') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="{{ route('reports.restocking.index') }}">{{ __('Restocking') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="{{ route('reports.stocktake-suggestions.index') }}">{{ __('Stocktake Suggestions') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="{{ route('reports.activity-log.index') }}">{{ __('Activity Log') }}</a>
-{{--                            <a class="dropdown-item text-white lightHover" href="{{ url('products-merge?sku1=45&sku2=44') }}">{{ __('products-merge') }}</a>--}}
+                <!-- Menu -->
+                <div class="dropdown dropdown-menu-right">
+                    <a style="height: 37px" id="dropdownMenu" class="btn btn-primary" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <font-awesome-icon icon="bars"></font-awesome-icon>
+                    </a>
 
-                            @if(count($navigationMenuReports) > 0)
-                                <hr v-if='{{ count($navigationMenuReports) > 0 }}' class="mb-1 mt-1">
-                                @foreach ($navigationMenuReports as $menu)
-                                    <a class="dropdown-item text-white lightHover" href="{{ $menu->url }}">
-                                        {{ $menu->name }}
-                                    </a>
-                                @endforeach
-                            @endif
-                        </div>
-                    </li>
-                </ul>
+                    <div class="dropdown-menu dropdown-menu-right w-auto text-left bg-primary" aria-labelledby="navbarDropdown" >
+                        {{-- Profile --}}
+                        <a class="dropdown-item text-white lightHover" href="{{ route('setting-profile') }}">{{ __('Profile') }}</a>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto text-center text-md-left">
-                    <li class="nav-item dropdown"> <!-- User dropdown menu -->
-                        <a id="navbarDropdown" class="nav-link text-white lightHover dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} ({{ Auth::user()->warehouse ? Auth::user()->warehouse->code : 'All Locations' }})<span class="caret"></span>
-                        </a>
+                        @hasrole('admin')
+                        <a class="dropdown-item text-white lightHover" href="{{ route('settings') }}">{{ __('Settings') }}</a>
+                        @endhasrole
 
-                        <!-- Menu Items END -->
-                        <div class="dropdown-menu dropdown-menu-right bg-primary text-center text-md-left" aria-labelledby="navbarDropdown">
-                            {{-- Profile --}}
-                            <a class="dropdown-item text-white lightHover" href="{{ route('setting-profile') }}">{{ __('Profile') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="https://ship.town/academy" target="_blank">{{ __('Academy') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="https://www.youtube.com/channel/UCl04S5dRXop1ZdZsOqY3OnA" target="_blank">{{ __('YouTube') }}</a>
+                        <a class="dropdown-item text-white lightHover" href="https://docs.google.com/spreadsheets/d/1IagdPL-ZKOz0-_Rf83ukhDKj02S-DpgLTz7LrvzWWR4/copy" target="_blank">{{ __('Shelf Labels') }}</a>
 
-                            @hasrole('admin')
-                                <a class="dropdown-item text-white lightHover" href="{{ route('settings') }}">{{ __('Settings') }}</a>
-                            @endhasrole
-
-
-                            <a class="dropdown-item text-white lightHover" href="https://ship.town/academy" target="_blank">{{ __('Academy') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="https://www.youtube.com/channel/UCl04S5dRXop1ZdZsOqY3OnA" target="_blank">{{ __('YouTube') }}</a>
-                            <a class="dropdown-item text-white lightHover" href="https://docs.google.com/spreadsheets/d/1IagdPL-ZKOz0-_Rf83ukhDKj02S-DpgLTz7LrvzWWR4/copy" target="_blank">{{ __('Shelf Labels') }}</a>
-
-                            <!-- Logout -->
-                            <a class="dropdown-item text-white lightHover" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
+                        <!-- Logout -->
+                        <a class="dropdown-item text-white lightHover" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
                                document.getElementById('logout-form').submit();">
-                                 {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                </ul>
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
