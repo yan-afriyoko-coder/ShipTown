6140d4c06422982bf1091859cd372fcd8dd23b72
Artur Hanusek <arthur@youritsolutions.ie>
2024-07-21 17:11:50 +0200

    fix test

--- tests/Browser/Routes/Autopilot/PacklistPageTest.php
+++ tests/Browser/Routes/Autopilot/PacklistPageTest.php
@@ -34,35 +34,36 @@ class PacklistPageTest extends DuskTestCase
         $user->assignRole('user');
 
         /** @var OrderStatus $orderStatus */
-        $orderStatus = OrderStatus::factory()->create(['name' => 'paid', 'order_active' => true]);
+        $orderStatus = OrderStatus::factory()->create(['name' => 'paid', 'code' => 'paid', 'order_active' => true]);
 
         /** @var Order $order */
         $order = Order::factory()->create();
-        $order->update(['order_status' => $orderStatus->code]);
 
         /** @var OrderProduct $orderProduct */
-        $orderProduct = OrderProduct::factory()->create(['order_id' => $order->getKey()]);
+        $orderProduct1 = OrderProduct::factory()->create(['order_id' => $order->getKey()]);
+        $orderProduct2 = OrderProduct::factory()->create(['order_id' => $order->getKey()]);
 
-        $orderProduct->product
+
+        $orderProduct1->product
             ->inventory($user->warehouse->code)
             ->update([
-                'quantity' => $orderProduct->quantity_ordered,
+                'quantity' => $orderProduct1->quantity_ordered,
                 'quantity_reserved' => 0,
             ]);
 
         $order->refresh();
         $order->update(['total_paid' => $order->total_order]);
-
+        $order->update(['order_status' => $orderStatus->code]);
 
         $this->browse(function (Browser $browser) use ($user, $order) {
             $browser->disableFitOnFailure();
 
-            $browser->loginAs($user);
-            $browser->visit($this->uri);
-            $browser->waitFor('@startAutopilotButton', 1);
-            $browser->click('@startAutopilotButton');
-            $browser->waitForText($order->order_number, 1);
-            $browser->assertSourceMissing('snotify-error');
+            $browser->loginAs($user)
+                ->pause($this->shortDelay)->visit($this->uri . '?step=select')
+                ->pause($this->shortDelay)->waitForText('Status: paid', 1)
+                ->pause($this->shortDelay)->clickLink('Status: paid')
+                ->pause($this->shortDelay)->waitForText($order->order_number, 1)
+                ->pause($this->shortDelay)->assertSourceMissing('snotify-error');
         });
     }
 }
