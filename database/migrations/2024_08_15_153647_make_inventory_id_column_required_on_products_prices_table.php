8201e4eb69937aae63680f69acf814aace1306c4
Artur Hanusek <arthur@youritsolutions.ie>
2024-08-15 23:23:17 +0200

    fill inventory_id

--- database/migrations/2024_08_15_153647_make_inventory_id_column_required_on_products_prices_table.php
+++ database/migrations/2024_08_15_153647_make_inventory_id_column_required_on_products_prices_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\ProductPrice;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,6 +9,18 @@ return new class extends Migration
 {
     public function up(): void
     {
+        ProductPrice::query()
+            ->where('inventory_id', null)
+            ->chunk(1000, function ($records) {
+                ProductPrice::query()
+                    ->whereIn('id', $records->pluck('id'))
+                    ->update([
+                        'inventory_id' => DB::raw('(SELECT inventory_id FROM inventory WHERE inventory.warehouse_id = warehouse_id AND inventory.product_id = product_id)'),
+                    ]);
+
+                usleep(10000); // 10ms
+            });
+
         Schema::table('products_prices', function (Blueprint $table) {
             $table->dropForeign(['inventory_id']);
         });
