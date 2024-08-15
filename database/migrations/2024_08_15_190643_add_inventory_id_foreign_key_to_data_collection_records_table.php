04bda1f6c79d2fb1a753829add6e27bb87470b6c
Artur Hanusek <arthur@youritsolutions.ie>
2024-08-15 23:40:29 +0200

    fix migration

--- database/migrations/2024_08_15_190643_add_inventory_id_foreign_key_to_data_collection_records_table.php
+++ database/migrations/2024_08_15_190643_add_inventory_id_foreign_key_to_data_collection_records_table.php
@@ -8,6 +8,14 @@ return new class extends Migration
 {
     public function up(): void
     {
+        try {
+            Schema::table('data_collection_records', function (Blueprint $table) {
+                $table->dropForeign('data_collection_records_inventory_id_foreign');
+            });
+        } catch (\Exception $e) {
+            //
+        }
+
         Schema::table('data_collection_records', function (Blueprint $table) {
             $table->foreign('inventory_id')
                 ->references('id')
