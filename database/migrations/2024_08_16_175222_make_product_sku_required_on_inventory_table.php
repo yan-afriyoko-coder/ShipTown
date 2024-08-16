2a51b26bfe50686a9769fb5c6366504c07aa563a
Artur Hanusek <arthur@youritsolutions.ie>
2024-08-16 22:23:05 +0200

    fix migration

--- database/migrations/2024_08_16_175222_make_product_sku_required_on_inventory_table.php
+++ database/migrations/2024_08_16_175222_make_product_sku_required_on_inventory_table.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    public function __construct()
+    {
+        \Illuminate\Support\Facades\DB::getDoctrineSchemaManager()
+            ->getDatabasePlatform()->registerDoctrineTypeMapping('varchar', 'string');
+    }
+
     public function up(): void
     {
         Schema::table('inventory', function (Blueprint $table) {
