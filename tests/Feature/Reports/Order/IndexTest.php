46143ca0142f8de085891b78cd6184c23eec3430
Yan Afriyoko <mail.yanafriyoko@gmail.com>
2024-08-22 23:29:07 +0700

    fix error test

---
+++ tests/Feature/Reports/Order/IndexTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature\Reports\Order;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+/**
+ *
+ */
+class IndexTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    protected string $uri = '';
+
+    protected mixed $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
+
+    /** @test */
+    public function test_if_uri_set()
+    {
+        $this->assertNotEmpty($this->uri);
+    }
+
+    /** @test */
+    public function test_guest_call()
+    {
+        $response = $this->get($this->uri);
+
+        $response->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function test_user_call()
+    {
+        $this->actingAs($this->user, 'web');
+
+        $response = $this->get($this->uri);
+
+        $response->assertForbidden();
+    }
+
+    /** @test */
+    public function test_admin_call()
+    {
+        $this->user->assignRole('admin');
+
+        $this->actingAs($this->user, 'web');
+
+        $response = $this->get($this->uri);
+
+        $response->assertSuccessful();
+    }
+}
