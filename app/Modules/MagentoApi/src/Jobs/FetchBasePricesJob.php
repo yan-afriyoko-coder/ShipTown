bf8bb4031776a29768117e0c81d98c2adad3d5d8
Artur Hanusek <arthur@youritsolutions.ie>
2024-09-07 01:53:51 +0200

    refactoring

--- app/Modules/MagentoApi/src/Jobs/FetchBasePricesJob.php
+++ app/Modules/MagentoApi/src/Jobs/FetchBasePricesJob.php
@@ -3,10 +3,10 @@
 namespace App\Modules\MagentoApi\src\Jobs;
 
 use App\Abstracts\UniqueJob;
-use App\Modules\MagentoApi\src\Exceptions\UnauthorizedException;
 use App\Modules\MagentoApi\src\Models\MagentoProduct;
 use App\Modules\MagentoApi\src\Services\MagentoService;
 use Exception;
+use Illuminate\Support\Collection;
 
 /**
  * Class SyncCheckFailedProductsJob.
@@ -15,29 +15,22 @@ class FetchBasePricesJob extends UniqueJob
 {
     public function handle(): void
     {
-        MagentoProduct::query()
-            ->with(['magentoConnection', 'product'])
-            ->whereRaw('IFNULL(exists_in_magento, 1) = 1')
-            ->whereNull('base_prices_fetched_at')
-            ->orWhereNull('base_prices_raw_import')
-            ->chunkById(100, function ($products) {
-                $result = collect($products)
-                    ->each(function (MagentoProduct $product) {
-                        try {
-                            MagentoService::fetchBasePrices($product);
-                        } catch (UnauthorizedException $exception) {
-                            report($exception);
-                            return false;
-                        } catch (Exception $exception) {
-                            report($exception);
-                            return false;
-                        }
-
-                        return true;
+        try {
+            MagentoProduct::query()
+                ->with(['magentoConnection', 'product'])
+                ->whereRaw('IFNULL(exists_in_magento, 1) = 1')
+                ->whereNull('base_prices_fetched_at')
+                ->orWhereNull('base_prices_raw_import')
+                ->chunkById(100, function (Collection $chunk) {
+                    $$chunk->each(function (MagentoProduct $product) {
+                        MagentoService::fetchBasePrices($product);
                     });
 
-                usleep(100000); // 100ms
-                return $result;
-            });
+                    usleep(100000); // 100ms
+                });
+        } catch (Exception $exception) {
+            report($exception);
+            return;
+        }
     }
 }
